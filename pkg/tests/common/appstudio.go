cfcd07385365baf9aaa3d8b7b119539a4b0cb81c
Flavius Lacatusu <flacatus@redhat.com>
2022-01-04 09:38:45 +0100

    fixes after review
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- pkg/tests/common/appstudio.go
+++ pkg/tests/common/appstudio.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/argoproj/gitops-engine/pkg/health"
@@ -9,15 +8,17 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/redhat-appstudio/e2e-tests/pkg/framework"
 	commonCtrl "github.com/redhat-appstudio/e2e-tests/pkg/utils/common"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 var (
 	// Pipelines names from https://github.com/redhat-appstudio/infra-deployments/tree/main/components/build/build-templates
-	AppStudioClusterTaskNames    = []string{"analyze-devfile", "appstudio-utils"}
+	AppStudioClusterTaskNames    = []string{"analyze-devfile", "cleanup-build-directories", "image-exists", "appstudio-utils"}
 	AppStudioComponents          = []string{"all-components-staging", "authentication", "build", "gitops", "has"}
 	AppStudioComponentsNamespace = "openshift-gitops"
 	PipelinesNamespace           = "build-templates"
+	ClusterTaskLabels            = map[string]string{"app.kubernetes.io/instance": "build"}
 )
 
 var _ = framework.CommonSuiteDescribe("Red Hat App Studio common E2E", func() {
@@ -31,22 +32,19 @@ var _ = framework.CommonSuiteDescribe("Red Hat App Studio common E2E", func() {
 				componentStatus, err := commonController.GetAppStudioComponentStatus(component, AppStudioComponentsNamespace)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(componentStatus.Health.Status).To(Equal(health.HealthStatusHealthy))
-
 			})
 		}
 	})
-	defer g.GinkgoRecover()
 
 	g.Context("ClusterTasks:", func() {
 		g.It("Check if AppStudio ClusterTasks are precreated", func() {
-			err := wait.PollImmediate(30*time.Second, 10*time.Minute, func() (done bool, err error) {
+			err := wait.PollImmediate(100*time.Millisecond, 3*time.Minute, func() (done bool, err error) {
 				for _, clusterTaskName := range AppStudioClusterTaskNames {
-					clusterTask, err := commonController.GetClusterTask(clusterTaskName, PipelinesNamespace)
-					if err != nil {
+					_, err := commonController.GetClusterTask(clusterTaskName, PipelinesNamespace)
+					if errors.IsNotFound(err) {
 						return false, nil
-					}
-					if clusterTaskName != clusterTask.Name {
-						return false, fmt.Errorf("Cluster task have an unexpected name")
+					} else if err != nil {
+						return false, err
 					}
 				}
 				return true, nil
--- pkg/utils/common/controller.go
+++ pkg/utils/common/controller.go
@@ -3,11 +3,14 @@ package common
 import (
 	"context"
 	"fmt"
+
 	app "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/redhat-appstudio/e2e-tests/pkg/client"
+	rclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // Create the struct for kubernetes clients
@@ -19,7 +22,7 @@ type SuiteController struct {
 func NewSuiteController() (*SuiteController, error) {
 	client, err := client.NewK8SClient()
 	if err != nil {
-		return nil, fmt.Errorf("Error creating client-go")
+		return nil, fmt.Errorf("Error creating client-go %v", err)
 	}
 	return &SuiteController{
 		client,
@@ -51,4 +54,13 @@ func (h *SuiteController) GetClusterTask(name string, namespace string) (*v1beta
 		return nil, err
 	}
 	return clusterTask, nil
+}
+
+// ListClusterTask return a list of ClusterTasks with a specific label selectors
+func (h *SuiteController) ListClusterTask(labelSelector map[string]string) ([]v1beta1.ClusterTask, error) {
+	clusterTasks := &v1beta1.ClusterTaskList{}
+	if err := h.KubeRest().List(context.TODO(), clusterTasks, &rclient.ListOptions{LabelSelector: labels.SelectorFromSet(labelSelector)}); err != nil {
+		return nil, err
+	}
+	return clusterTasks.Items, nil
 }
