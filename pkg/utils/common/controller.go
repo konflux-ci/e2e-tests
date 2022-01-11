9769aa19f73da6d3b3158b3ddbecb26e91b1f2a6
Flavius Lacatusu <flacatus@redhat.com>
2022-01-11 12:18:37 +0100

    Improve argo cd components status checks
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- pkg/utils/common/controller.go
+++ pkg/utils/common/controller.go
@@ -3,11 +3,15 @@ package common
 import (
 	"context"
 	"fmt"
+	"time"
 
 	app "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/argoproj/gitops-engine/pkg/health"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/redhat-appstudio/e2e-tests/pkg/client"
 	rclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -29,38 +33,54 @@ func NewSuiteController() (*SuiteController, error) {
 	}, nil
 }
 
-func (h *SuiteController) GetAppStudioComponentStatus(name string, namespace string) (*app.ApplicationStatus, error) {
+// Returns a status for a specic argo Component
+func (s *SuiteController) GetAppStudioApplicationStatus(name string, namespace string) (*app.ApplicationStatus, error) {
 	namespacedName := types.NamespacedName{
 		Name:      name,
 		Namespace: namespace,
 	}
 	application := &app.Application{}
 
-	if err := h.KubeRest().Get(context.TODO(), namespacedName, application); err != nil {
+	if err := s.KubeRest().Get(context.TODO(), namespacedName, application); err != nil {
 		return nil, err
 	}
 	return &application.Status, nil
 }
 
 // GetClusterTask return a clustertask object from cluster and if don't exist returns an error
-func (h *SuiteController) GetClusterTask(name string, namespace string) (*v1beta1.ClusterTask, error) {
+func (s *SuiteController) GetClusterTask(name string, namespace string) (*v1beta1.ClusterTask, error) {
 	namespacedName := types.NamespacedName{
 		Name:      name,
 		Namespace: namespace,
 	}
 	clusterTask := &v1beta1.ClusterTask{}
 
-	if err := h.KubeRest().Get(context.TODO(), namespacedName, clusterTask); err != nil {
+	if err := s.KubeRest().Get(context.TODO(), namespacedName, clusterTask); err != nil {
 		return nil, err
 	}
 	return clusterTask, nil
 }
 
 // ListClusterTask return a list of ClusterTasks with a specific label selectors
-func (h *SuiteController) ListClusterTask(labelSelector map[string]string) ([]v1beta1.ClusterTask, error) {
+func (s *SuiteController) ListClusterTask(labelSelector map[string]string) ([]v1beta1.ClusterTask, error) {
 	clusterTasks := &v1beta1.ClusterTaskList{}
-	if err := h.KubeRest().List(context.TODO(), clusterTasks, &rclient.ListOptions{LabelSelector: labels.SelectorFromSet(labelSelector)}); err != nil {
+	if err := s.KubeRest().List(context.TODO(), clusterTasks, &rclient.ListOptions{LabelSelector: labels.SelectorFromSet(labelSelector)}); err != nil {
 		return nil, err
 	}
 	return clusterTasks.Items, nil
+}
+
+func (s *SuiteController) WaitForArgoCDApplicationToBeReady(argoComponent string, componentNamespace string) error {
+	return wait.PollImmediate(100*time.Millisecond, 5*time.Minute, func() (done bool, err error) {
+		applicationStatus, err := s.GetAppStudioApplicationStatus(argoComponent, componentNamespace)
+		if errors.IsNotFound(err) {
+			return false, nil
+		} else if err != nil {
+			return false, err
+		} else if applicationStatus.Health.Status != health.HealthStatusHealthy {
+			return false, nil
+		}
+
+		return true, nil
+	})
 }
