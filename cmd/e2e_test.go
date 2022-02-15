48d304bc1e7cc3a21916d7d38cf05281a8b5e397
bluengo <bluengopriet@gmail.com>
2022-02-15 15:49:16 +0100

    Add secret check inside SynchronizedBeforeSuite

--- cmd/e2e_test.go
+++ cmd/e2e_test.go
@@ -1,15 +1,28 @@
 package cmd
 
 import (
+	"context"
 	"testing"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	"github.com/redhat-appstudio/e2e-tests/pkg/client"
 	_ "github.com/redhat-appstudio/e2e-tests/pkg/tests/common"
 	_ "github.com/redhat-appstudio/e2e-tests/pkg/tests/has"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = ginkgo.SynchronizedBeforeSuite(func() []byte {
+	KubeClient, err := client.NewK8SClient()
+	if err != nil {
+		panic(err)
+	}
+	secret, err := KubeClient.KubeInterface().CoreV1().Secrets("application-service").Get(context.TODO(), "has-github-token", metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+	gomega.Expect(secret).NotTo(gomega.BeNil())
+
 	return nil
 }, func(data []byte) {})
 
