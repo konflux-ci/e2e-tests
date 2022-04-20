b0c1e339ec57d76b5cdbe1738d2bf04aa7a30ca5
albarbaro <abarbaro@redhat.com>
2022-04-20 10:31:51 +0200

    fixed Readme and comments

--- pkg/framework/framework.go
+++ pkg/framework/framework.go
@@ -9,35 +9,35 @@ import (
 	"github.com/redhat-appstudio/e2e-tests/pkg/utils/tekton"
 )
 
-//Framework struct to store all controllers
+// Framework struct to store all controllers
 type Framework struct {
 	HasController    *has.SuiteController
 	CommonController *common.SuiteController
 	TektonController *tekton.SuiteController
 }
 
-//Initialize all test controllers and return them in a Framework
+// Initialize all test controllers and return them in a Framework
 func NewFramweork() (*Framework, error) {
 
-	//Initialize a common kubernetes client to be passed to the test controllers
+	// Initialize a common kubernetes client to be passed to the test controllers
 	kubeClient, err := kubeCl.NewK8SClient()
 	if err != nil {
 		return nil, fmt.Errorf("error creating client-go %v", err)
 	}
 
-	//Initialize Common controller
+	// Initialize Common controller
 	commonCtrl, err := common.NewSuiteController(kubeClient)
 	if err != nil {
 		return nil, err
 	}
 
-	//Initialize Has controller
+	// Initialize Has controller
 	hasController, err := has.NewSuiteController(kubeClient)
 	if err != nil {
 		return nil, err
 	}
 
-	//Initialize Tekton controller
+	// Initialize Tekton controller
 	tektonController, err := tekton.NewSuiteController(kubeClient)
 	if err != nil {
 		return nil, err
