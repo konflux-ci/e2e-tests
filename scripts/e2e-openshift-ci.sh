714786f0a3c4f0d4a1d620b8b90d5f601cfcc2e5
flacatus <flacatus@redhat.com>
2022-04-25 18:51:25 +0200

    Fix

--- scripts/e2e-openshift-ci.sh
+++ scripts/e2e-openshift-ci.sh
@@ -16,7 +16,7 @@ mkdir -p tmp/
 export ROOT_E2E="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 export WORKSPACE=${WORKSPACE:-${ROOT_E2E}}
 export E2E_CLONE_BRANCH="main"
-export E2E_REPO_LINK="https://github.com/redhat-appstudio/e2e-tests.git"
+export E2E_REPO_LINK=""
 
 # Example: CLONEREFS_OPTIONS={"src_root":"/go","log":"/dev/null","git_user_name":"ci-robot","git_user_email":"ci-robot@openshift.io","refs":[{"org":"redhat-appstudio","repo":"application-service","repo_link":"https://github.com/redhat-appstudio/application-service","base_ref":"main","base_sha":"75a4c79e49ab5c1a4c15d844256d1e4419da63e3","base_link":"https://github.com/redhat-appstudio/application-service/commit/75a4c79e49ab5c1a4c15d844256d1e4419da63e3","pulls":[{"number":91,"author":"flacatus","sha":"47b9fe555e27cc65c5ebfcf51c2d26a036fab235","link":"https://github.com/redhat-appstudio/application-service/pull/91","commit_link":"https://github.com/redhat-appstudio/application-service/pull/91/commits/47b9fe555e27cc65c5ebfcf51c2d26a036fab235","author_link":"https://github.com/flacatus"}]}],"fail":true}
 # Checking if CLONEREFS_OPTIONS openshift ci env exists and extract PR information to pair the PR
@@ -32,13 +32,14 @@ if [[ -n ${CLONEREFS_OPTIONS} ]]; then
     AUTHOR_E2E_BRANCH=$(curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/"${AUTHOR}"/e2e-tests/branches | jq '.[] | select(.name=="'${PR_BRANCH_REF}'")')
 
     if [ -z "${AUTHOR_E2E_BRANCH}" ]; then
+        E2E_REPO_LINK="https://github.com/redhat-appstudio/e2e-tests.git"
     else
         E2E_CLONE_BRANCH=${PR_BRANCH_REF}
         E2E_REPO_LINK="${AUTHOR_LINK}/e2e-tests.git"
     fi
 fi
 
-echo "[INFO] Cloning e2e-tests from branch ${PR_BRANCH_REF} repository ${AUTHOR_LINK}/e2e-tests"
+echo "[INFO] Cloning e2e-tests from branch ${PR_BRANCH_REF} repository ${E2E_REPO_LINK}"
 git clone -b "${E2E_CLONE_BRANCH}" "${E2E_REPO_LINK}" "$WORKSPACE"/tmp/e2e-tests
 
 cd "$WORKSPACE"/tmp/e2e-tests
