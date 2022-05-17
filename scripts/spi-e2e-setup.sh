1e3f675bc2c97a5c678b020134e28001eafd56db
flacatus <flacatus@redhat.com>
2022-05-17 13:41:14 +0200

    Add script to setup SPI for e2e tests

---
+++ scripts/spi-e2e-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
+
+echo '[INFO] Deploying SPI OAuth2 config'
+
+export OAUTH_URL='spi-oauth-route-spi-system.'$( oc get ingresses.config/cluster -o jsonpath={.spec.domain})
+export tmpfile=tmp/config.yaml
+
+# We are injecting the token manually for the e2e. No need a real secret for now
+export SPI_GITHUB_CLIENT_ID="app-client-id"
+export SPI_GITHUB_CLIENT_SECRET="app-secret"
+
+spiConfig=$(cat <<EOF
+sharedSecret: $(openssl rand -hex 20)
+serviceProviders:
+  - type: GitHub
+    clientId: $SPI_GITHUB_CLIENT_ID
+    clientSecret: $SPI_GITHUB_CLIENT_SECRET
+baseUrl: https://spi-oauth-route-spi-system.$( oc get ingresses.config/cluster -o jsonpath={.spec.domain})
+EOF
+)
+
+echo "Please go to https://github.com/settings/developers."
+echo "And register new Github OAuth application for callback https://"$OAUTH_URL"/github/callback"
+
+echo "$spiConfig" > "$tmpfile"
+oc create namespace spi-system --dry-run=client -o yaml | oc apply -f -
+oc create secret generic oauth-config \
+    --save-config --dry-run=client \
+    --from-file="$tmpfile" \
+    -n spi-system \
+    -o yaml |
+oc apply -f -
+
+rm "$tmpfile"
+
+oc rollout restart  deployment/spi-controller-manager -n spi-system
+oc rollout restart  deployment/spi-oauth-service -n spi-system
