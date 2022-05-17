9fa16b699ee042cb1d7726acd7459e3caffc8d59
flacatus <flacatus@redhat.com>
2022-05-17 17:38:14 +0200

    testing oc bin

--- .ci/oci-launch-e2e.sh
+++ .ci/oci-launch-e2e.sh
@@ -1,16 +1,33 @@
 export ROOT_E2E="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 export WORKSPACE=${WORKSPACE:-${ROOT_E2E}}
 
-oc create user user
-oc create secret generic htpass-secret --from-file=htpasswd="${WORKSPACE}"/scripts/resources/users.htpasswd -n openshift-config
-oc apply -f "${WORKSPACE}"/scripts/resources/htpasswdProvider.yaml
-oc adm policy add-cluster-role-to-user cluster-admin user
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  creationTimestamp: null
+  name: htpass-secret
+  namespace: openshift-config
+data: 
+  htpasswd: YXBwc3R1ZGlvOiQyeSQwNSREY3pLblNydExBZzF0SGhWZHpTczhPWUFURFViU1NkL2wuTWRQTDFIZWtjYWtTTE1CWTFCRw==
+EOF
+
+oc patch oauths cluster --type merge -p '
+spec:
+  identityProviders:
+    - name: htpasswd
+      mappingMethod: claim
+      type: HTPasswd
+      htpasswd:
+        fileData:
+          name: htpass-secret
+'
 
 echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
 CURRENT_TIME=$(date +%s)
 ENDTIME=$(($CURRENT_TIME + 300))
 while [ $(date +%s) -lt $ENDTIME ]; do
-    if oc login -u user -p user --insecure-skip-tls-verify=false; then
+    if oc login -u appstudio -p appstudio --insecure-skip-tls-verify=false; then
         break
     fi
     sleep 10
