b32553aa10aff0ef7c814404cfc70ffe18d50875
flacatus <flacatus@redhat.com>
2022-05-18 15:13:10 +0200

    Fix log typo

--- .ci/oci-launch-e2e.sh
+++ .ci/oci-launch-e2e.sh
@@ -1,6 +1,10 @@
 export ROOT_E2E="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 export WORKSPACE=${WORKSPACE:-${ROOT_E2E}}
 
+user=$(oc whoami)
+oc adm policy add-cluster-role-to-user cluster-admin user
+oc whoami --show-token || true
+
 cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Secret
@@ -21,21 +25,24 @@ spec:
         fileData:
           name: htpass-secret
 '
-timeout 120 bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != False ]]; do echo 'Condition (status != False) failed. Waiting 5 secs.'; sleep 5; done"
+timeout 60 bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != False ]]; do echo 'Condition (status != False) failed. Waiting 5 secs.'; sleep 5; done"
 
-timeout 180s bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != True ]]; do echo 'Condition (status != true) failed. Waiting 2sec.'; sleep 5; done"
+timeout 300 bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != True ]]; do echo 'Condition (status != true) failed. Waiting 2sec.'; sleep 5; done"
 
 oc adm policy add-cluster-role-to-user cluster-admin appstudioci
 
 echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
+CURRENT_TIME=$(date +%s)
+ENDTIME=$(($CURRENT_TIME + 300))
 
 ctx=$(oc config current-context)
 cluster=$(oc config view -ojsonpath="{.contexts[?(@.name == \"$ctx\")].context.cluster}")
 server=$(oc config view -ojsonpath="{.clusters[?(@.name == \"$cluster\")].cluster.server}")
-echo "Login against: $server"
+logger.info "Login against: $server"
 
-export name="appstudioci"
-cp "${KUBECONFIG}" "$name.kubeconfig"
-occmd="bash -c '! oc login --kubeconfig=${name}.kubeconfig --username=${name} --password=appstudioci > /dev/null'"
-timeout 180 "${occmd}"
-
+while [ $(date +%s) -lt $ENDTIME ]; do
+    if oc login --kubeconfig=/tmp/new.files --server $server --username=appstudioci --password=appstudioci --insecure-skip-tls-verify; then
+        break
+    fi
+    sleep 10
+done
