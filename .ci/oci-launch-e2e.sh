aac11043d9eeec7c9d83da8acb383a6ef32e0c3d
flacatus <flacatus@redhat.com>
2022-05-18 14:53:27 +0200

    Fix log typo

--- .ci/oci-launch-e2e.sh
+++ .ci/oci-launch-e2e.sh
@@ -21,15 +21,13 @@ spec:
         fileData:
           name: htpass-secret
 '
-#timeout 120 bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != False ]]; do echo 'Condition (status != False) failed. Waiting 5 secs.'; sleep 5; done"
+timeout 120 bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != False ]]; do echo 'Condition (status != False) failed. Waiting 5 secs.'; sleep 5; done"
 
-#timeout 180s bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != True ]]; do echo 'Condition (status != true) failed. Waiting 2sec.'; sleep 5; done"
+timeout 180s bash -x -c -- "while [[ $(oc get co authentication -o jsonpath='{.status.conditions[?(@.type=="Progressing")].status}') != True ]]; do echo 'Condition (status != true) failed. Waiting 2sec.'; sleep 5; done"
 
 oc adm policy add-cluster-role-to-user cluster-admin appstudioci
 
 echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
-CURRENT_TIME=$(date +%s)
-ENDTIME=$(($CURRENT_TIME + 300))
 
 ctx=$(oc config current-context)
 cluster=$(oc config view -ojsonpath="{.contexts[?(@.name == \"$ctx\")].context.cluster}")
