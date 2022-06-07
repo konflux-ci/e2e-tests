bbd70aeee7282b8aa572c3e2c0ba20f3bad31ab4
flacatus <flacatus@redhat.com>
2022-06-07 13:34:13 +0200

    DO NOT REVIEW

--- .ci/oci-launch-e2e.sh
+++ .ci/oci-launch-e2e.sh
@@ -7,69 +7,8 @@ set -o pipefail
 # error on unset variables
 set -u
 
-export WORKSPACE=$(dirname $(dirname $(readlink -f "$0")));
-export APPLICATION_NAMESPACE="openshift-gitops"
-export APPLICATION_NAME="all-components-staging"
+COPY
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+chmod +x ./kind
 
-# Available openshift ci environments https://docs.ci.openshift.org/docs/architecture/step-registry/#available-environment-variables
-export ARTIFACTS_DIR=${ARTIFACT_DIR:-"/tmp/appstudio"}
-
-command -v yq >/dev/null 2>&1 || { echo "yq is not installed. Aborting."; exit 1; }
-command -v kubectl >/dev/null 2>&1 || { echo "kubectl is not installed. Aborting."; exit 1; }
-
-function waitHASApplicationToBeReady() {
-    while [ "$(kubectl get applications.argoproj.io has -n openshift-gitops -o jsonpath='{.status.health.status}')" != "Healthy" ]; do
-        sleep 30s
-        echo "[INFO] Waiting for HAS to be ready."
-    done
-}
-
-function waitAppStudioToBeReady() {
-    while [ "$(kubectl get applications.argoproj.io ${APPLICATION_NAME} -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.health.status}')" != "Healthy" ] ||
-          [ "$(kubectl get applications.argoproj.io ${APPLICATION_NAME} -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.sync.status}')" != "Synced" ]; do
-        sleep 1m
-        echo "[INFO] Waiting for AppStudio to be ready."
-    done
-}
-
-function waitBuildToBeReady() {
-    while [ "$(kubectl get applications.argoproj.io build -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.health.status}')" != "Healthy" ] ||
-          [ "$(kubectl get applications.argoproj.io build -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.sync.status}')" != "Synced" ]; do
-        sleep 1m
-        echo "[INFO] Waiting for Build to be ready."
-    done
-}
-
-function waitSPIToBeReady() {
-    while [ "$(kubectl get applications.argoproj.io spi -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.health.status}')" != "Healthy" ] ||
-          [ "$(kubectl get applications.argoproj.io spi -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.sync.status}')" != "Synced" ]; do
-        sleep 1m
-        echo "[INFO] Waiting for spi to be ready."
-    done
-}
-
-function executeE2ETests() {
-    make build
-    "${WORKSPACE}"/bin/e2e-appstudio --ginkgo.junit-report="${ARTIFACTS_DIR}"/e2e-report.xml --ginkgo.progress --ginkgo.v
-}
-
-# Initiate openshift ci users
-export KUBECONFIG_TEST="/tmp/kubeconfig"
-/bin/bash "$WORKSPACE"/scripts/provision-openshift-user.sh
-
-export KUBECONFIG="${KUBECONFIG_TEST}"
-
-/bin/bash "$WORKSPACE"/scripts/install-appstudio-e2e-mode.sh install
-
-export -f waitAppStudioToBeReady
-export -f waitBuildToBeReady
-export -f waitHASApplicationToBeReady
-export -f waitSPIToBeReady
-
-# Install AppStudio Controllers and wait for HAS and other AppStudio application to be running.
-timeout --foreground 10m bash -c waitAppStudioToBeReady
-timeout --foreground 10m bash -c waitBuildToBeReady
-timeout --foreground 10m bash -c waitHASApplicationToBeReady
-timeout --foreground 10m bash -c waitSPIToBeReady
-
-executeE2ETests
+./kind create cluster
