caaebe9a2da56888cdacaa06cedef2e9bcd383b8
flacatus <flacatus@redhat.com>
2022-06-16 14:48:35 +0200

    trying kind

--- .ci/oci-launch-e2e.sh
+++ .ci/oci-launch-e2e.sh
@@ -53,25 +53,6 @@ function executeE2ETests() {
     "${WORKSPACE}"/bin/e2e-appstudio --ginkgo.junit-report="${ARTIFACTS_DIR}"/e2e-report.xml --ginkgo.progress --ginkgo.v
 }
 
-# Initiate openshift ci users
-export KUBECONFIG_TEST="/tmp/kubeconfig"
-/bin/bash "$WORKSPACE"/scripts/provision-openshift-user.sh
-
-export KUBECONFIG="${KUBECONFIG_TEST}"
-
-/bin/bash "$WORKSPACE"/scripts/install-appstudio-e2e-mode.sh install
-
-export -f waitAppStudioToBeReady
-export -f waitBuildToBeReady
-export -f waitHASApplicationToBeReady
-export -f waitSPIToBeReady
-
-# Install AppStudio Controllers and wait for HAS and other AppStudio application to be running.
-timeout --foreground 10m bash -c waitAppStudioToBeReady
-timeout --foreground 10m bash -c waitBuildToBeReady
-timeout --foreground 10m bash -c waitHASApplicationToBeReady
-timeout --foreground 10m bash -c waitSPIToBeReady
-
 executeE2ETests
 
 cat kubeconfig
