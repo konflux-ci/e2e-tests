6f4eb1399fb262c0012dd1f939762c380c514a6b
Naftaly Shprai <naftalysh@gmail.com>
2022-07-13 14:52:34 +0300

    test: Hacbs 834/create constants module in release tests folder (#140)
    * Updated README.md with the related JIRA issue
    
    * https://issues.redhat.com/browse/HACBS-834
    
    Added a constants module (const.go) in release tests folder
    
    * Extracted release failure tests related constants into release-failures.go
    
    * Removed jira ticket link from README.md
    
    Co-authored-by: Naftaly Shprai <nshprai@eredhat.com>

---
+++ tests/release/const.go
@@ -0,0 +1,20 @@
+package release
+
+import (
+	"time"
+)
+
+const (
+	snapshotName          string = "snapshot"
+	sourceReleaseLinkName string = "source-release-link"
+	targetReleaseLinkName string = "target-release-link"
+	releaseStrategyName   string = "strategy"
+	releaseName           string = "release"
+	releasePipelineName   string = "release-pipeline"
+	applicationName       string = "application"
+	releasePipelineBundle string = "quay.io/hacbs-release/demo:m5-alpine"
+	releaseStrategyPolicy string = "policy"
+
+	avgPipelineCompletionTime = 10 * time.Minute
+	defaultInterval           = 100 * time.Millisecond
+)
