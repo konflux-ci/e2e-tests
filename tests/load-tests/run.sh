482ecedc0671231324178fad09fafad24ea79102
Meer Sawood <msawood@redhat.com>
2022-08-08 17:53:23 +0530

    fix

--- tests/load-tests/run.sh
+++ tests/load-tests/run.sh
@@ -1,7 +1,7 @@
-export DOCKER_CONFIG_JSON=ewogICJhdXRocyI6IHsKICAgICJxdWF5LmlvIjogewogICAgICAiYXV0aCI6ICJiWE5oZDI5dlpEcEhhMGt3WWpsUWMwdGpialZoZGxSdEswWmxOQ3RvZG1SUmVGaHBTVGhFVG1Oa2VEWnVOVUk1YjBJNFJUZHBRUzkyTldoaGJWTTVOWEJyWlhaRFVUaDMiLAogICAgICAiZW1haWwiOiAiIgogICAgfQogIH0KfQ==
+export DOCKER_CONFIG_JSON=
 
 if [ -z ${DOCKER_CONFIG_JSON+x} ]; then echo "env DOCKER_CONFIG_JSON need to be defined"; exit 1;  else echo "DOCKER_CONFIG_JSON is set"; fi
 
-go run loadtest.go --username testuser --users 200 --batch 25 --waitpipelines
+go run loadtest.go --username testuser --users 50 --batch 10 -w && ./clear.sh
 
 
