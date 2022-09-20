fb16a521999f6e7ea0338242aa30ad2553d6ed34
Naftaly Shprai <nshprai@eredhat.com>
2022-09-20 16:16:04 +0300

    Returned const.go as it was disabled to its state as the release tests were disabled to allow infra-deployment CI process to pass

--- tests/release/const.go
+++ tests/release/const.go
@@ -1,1 +1,21 @@
 package release
+
+// import (
+// 	"time"
+// )
+
+// const (
+// 	snapshotName                   string = "snapshot"
+// 	sourceReleasePlanName          string = "source-releaseplan"
+// 	targetReleasePlanAdmissionName string = "target-releaseplanadmission"
+// 	releaseStrategyName            string = "m7-strategy"
+// 	releaseName                    string = "release"
+// 	releasePipelineName            string = "m6-release-pipeline"
+// 	applicationName                string = "application"
+// 	releasePipelineBundle          string = "quay.io/hacbs-release/m6-release-pipeline:main"
+// 	releaseStrategyPolicy          string = "m7-policy"
+// 	releaseStrategyServiceAccount  string = "m7-service-account"
+
+// 	avgPipelineCompletionTime = 10 * time.Minute
+// 	defaultInterval           = 100 * time.Millisecond
+// )
