3ee0b8444bc93d028916df2a0039a1c6b4fc082d
Kasem Alem <kasem.alem@gmail.com>
2022-09-28 16:28:29 +0300

    fix e2e-test

--- tests/release/release.go
+++ tests/release/release.go
@@ -1,24 +1,26 @@
 package release
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/redhat-appstudio/e2e-tests/pkg/constants"
 	"github.com/redhat-appstudio/e2e-tests/pkg/framework"
+	"github.com/redhat-appstudio/e2e-tests/pkg/utils"
+	"github.com/redhat-appstudio/e2e-tests/pkg/utils/tekton"
 	gitopsv1alpha1 "github.com/redhat-appstudio/managed-gitops/appstudio-shared/apis/appstudio.redhat.com/v1alpha1"
-	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
-	"k8s.io/apimachinery/pkg/api/meta"
+	corev1 "k8s.io/api/core/v1"
 	klog "k8s.io/klog/v2"
 	"knative.dev/pkg/apis"
 )
 
-var snapshotComponents = []gitopsv1alpha1.ApplicationSnapshotComponent{
-	{"component-1", "quay.io/redhat-appstudio/component1@sha256:d5e85e49c89df42b221d972f5b96c6507a8124717a6e42e83fd3caae1031d514"},
-	{"component-2", "quay.io/redhat-appstudio/component2@sha256:a01dfd18cf8ca8b68770b09a9b6af0fd7c6d1f8644c7ab97f0e06c34dfc5860e"},
-	{"component-3", "quay.io/redhat-appstudio/component3@sha256:d90a0a33e4c5a1daf5877f8dd989a570bfae4f94211a8143599245e503775b1f"},
+var myEnvironment = []gitopsv1alpha1.EnvVarPair{
+	{Name: releaseEnvironment, Value: releaseEnvironment},
+}
+var managednamespaceSecret = []corev1.ObjectReference{
+	{Name: hacbsReleaseTestsTokenSecret},
 }
 
 var _ = framework.ReleaseSuiteDescribe("test-release-service-happy-path", Label("release"), func() {
@@ -31,9 +33,12 @@ var _ = framework.ReleaseSuiteDescribe("test-release-service-happy-path", Label(
 	var managedNamespace = uuid.New().String()
 
 	BeforeAll(func() {
+		kubeController := tekton.KubeController{
+			Commonctrl: *framework.CommonController,
+			Tektonctrl: *framework.TektonController,
+		}
 		// Create the dev namespace
 		demo, err := framework.CommonController.CreateTestNamespace(devNamespace)
-		klog.Info("Demo - Error  : ", demo, err)
 		Expect(err).NotTo(HaveOccurred(), "Error when creating namespace '%s': %v", demo.Name, err)
 		klog.Info("Dev Namespace created: ", demo.Name)
 
@@ -45,32 +50,50 @@ var _ = framework.ReleaseSuiteDescribe("test-release-service-happy-path", Label(
 		// Wait until the "pipeline" SA is created and ready with secrets by the openshift-pipelines operator
 		klog.Infof("Wait until the 'pipeline' SA is created in %s namespace \n", managedNamespace)
 		Eventually(func() bool {
-			sa, err := framework.CommonController.GetServiceAccount(releaseStrategyServiceAccount, managedNamespace)
+			sa, err := framework.CommonController.GetServiceAccount("pipeline", managedNamespace)
 			return sa != nil && err == nil
 		}, 1*time.Minute, defaultInterval).Should(BeTrue(), "timed out when waiting for the \"pipeline\" SA to be created")
+
+		sourceAuthJson := utils.GetEnv(constants.QUAY_OAUTH_TOKEN_RELEASE_SOURCE)
+		destinationAuthJson := utils.GetEnv(constants.QUAY_OAUTH_TOKEN_RELEASE_DESTINATION)
+
+		_, err = framework.ReleaseController.CreateRegistryJsonSecret(redhatAppstudioUserSecret, devNamespace, sourceAuthJson, sourceKeyName)
+		Expect(err).ToNot(HaveOccurred())
+
+		_, err = framework.ReleaseController.CreateRegistryJsonSecret(hacbsReleaseTestsTokenSecret, managedNamespace, destinationAuthJson, destinationKeyName)
+		Expect(err).ToNot(HaveOccurred())
+
+		// Copy the public key from tekton-chains/signing-secrets to a new
+		// secret that contains just the public key to ensure that access
+		// to password and private key are not needed.
+		publicKey, err := kubeController.GetPublicKey("signing-secrets", constants.TEKTON_CHAINS_NS)
+		Expect(err).ToNot(HaveOccurred())
+		GinkgoWriter.Printf("Copy public key from %s/signing-secrets to a new secret\n", constants.TEKTON_CHAINS_NS)
+		Expect(kubeController.CreateOrUpdateSigningSecret(
+			publicKey, publicSecretNameAuth, managedNamespace)).To(Succeed())
 
 	})
 
 	AfterAll(func() {
-
-		// Expect(framework.HasController.DeleteAllComponentsInASpecificNamespace(devNamespace, 30*time.Second)).To(Succeed())
-		// Expect(framework.HasController.DeleteAllApplicationsInASpecificNamespace(devNamespace, 30*time.Second)).To(Succeed())
-		// Expect(framework.TektonController.DeleteAllPipelineRunsInASpecificNamespace(devNamespace)).To(Succeed())
-		// Expect(framework.CommonController.DeleteNamespace(devNamespace)).To(Succeed())
-
-		// Expect(framework.HasController.DeleteAllComponentsInASpecificNamespace(managedNamespace, 30*time.Second)).To(Succeed())
-		// Expect(framework.HasController.DeleteAllApplicationsInASpecificNamespace(managedNamespace, 30*time.Second)).To(Succeed())
-		// Expect(framework.TektonController.DeleteAllPipelineRunsInASpecificNamespace(managedNamespace)).To(Succeed())
-		// Expect(framework.CommonController.DeleteNamespace(managedNamespace)).To(Succeed())
-
 		// Delete the dev and managed namespaces with all the resources created in them
 		Expect(framework.CommonController.DeleteNamespace(devNamespace)).NotTo(HaveOccurred())
 		Expect(framework.CommonController.DeleteNamespace(managedNamespace)).NotTo(HaveOccurred())
 	})
 
 	var _ = Describe("Creation of the 'Happy path' resources", func() {
-		It("Create an ApplicationSnapshot.", func() {
-			_, err := framework.ReleaseController.CreateApplicationSnapshot(snapshotName, devNamespace, applicationName, snapshotComponents)
+
+		It("Create PVC", func() {
+			_, err := framework.TektonController.CreatePVCAccessMode(releasePvcName, managedNamespace, corev1.ReadWriteOnce)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Create Service Account", func() {
+			_, err := framework.CommonController.CreateServiceAccount(releaseStrategyServiceAccount, managedNamespace, managednamespaceSecret)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Create Environment", func() {
+			_, err := framework.GitOpsController.CreateEnvironment(releaseEnvironment, managedNamespace, gitopsv1alpha1.DeploymentStrategy_Manual, gitopsv1alpha1.EnvironmentType_POC, myEnvironment)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -80,74 +103,50 @@ var _ = framework.ReleaseSuiteDescribe("test-release-service-happy-path", Label(
 		})
 
 		It("Create ReleasePlan in dev namespace", func() {
-			AutoReleaseLabel := ""
-			_, err := framework.ReleaseController.CreateReleasePlan(sourceReleasePlanName, devNamespace, applicationName, managedNamespace, AutoReleaseLabel)
+			_, err := framework.ReleaseController.CreateReleasePlan(sourceReleasePlanName, devNamespace, applicationName, managedNamespace, "")
 			Expect(err).NotTo(HaveOccurred())
 		})
 
 		It("Create ReleasePlanAdmission in managed namespace", func() {
-			AutoReleaseLabel := ""
-			_, err := framework.ReleaseController.CreateReleasePlanAdmission(targetReleasePlanAdmissionName, devNamespace, applicationName, managedNamespace, AutoReleaseLabel, releaseStrategyName)
+			_, err := framework.ReleaseController.CreateReleasePlanAdmission(targetReleasePlanAdmissionName, devNamespace, applicationName, managedNamespace, releaseEnvironment, "", releaseStrategyName)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("Create a Release", func() {
-			_, err := framework.ReleaseController.CreateRelease(releaseName, devNamespace, snapshotName, sourceReleasePlanName)
+		It("CreateApplication", func() {
+			_, err := framework.HasController.CreateHasApplication(applicationName, devNamespace)
 			Expect(err).NotTo(HaveOccurred())
 		})
+
+		It("CreateComponent", func() {
+			_, err := framework.HasController.CreateComponent(applicationName, componentName, devNamespace, gitSourceComponentUrl, "", "", "", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
 	})
 
 	var _ = Describe("Post-release verification", func() {
-		It("A PipelineRun should have been created in the managed namespace", func() {
-			Eventually(func() error {
-				_, err := framework.ReleaseController.GetPipelineRunInNamespace(managedNamespace, releaseName, devNamespace)
 
-				return err
-			}, 1*time.Minute, defaultInterval).Should(BeNil())
-		})
-
-		It("The PipelineRun should exist and succeed", func() {
+		It("A PipelineRun should have been created in the dev namespace", func() {
 			Eventually(func() bool {
-				pipelineRun, err := framework.ReleaseController.GetPipelineRunInNamespace(managedNamespace, releaseName, devNamespace)
-
+				pipelineRun, err := framework.TektonController.GetPipelineRunInNamespace(devNamespace)
+				myPR := &pipelineRun.Items[0]
 				if pipelineRun == nil || err != nil {
 					return false
 				}
-				// Debug
-				klog.Info("**********************************************")
-				klog.Info("Pipeline in managed is : ", pipelineRun.Name)
-				klog.Info("Pipeline HasStarted : ", pipelineRun.HasStarted())
-				klog.Info("Pipeline IsDone : ", pipelineRun.IsDone())
-				klog.Info("Pipeline Status Get Condition is succeded : ", pipelineRun.Status.GetCondition(apis.ConditionSucceeded))
-				klog.Info("**********************************************")
-				return pipelineRun.HasStarted() && pipelineRun.IsDone() && pipelineRun.Status.GetCondition(apis.ConditionSucceeded).IsTrue()
+				return myPR.HasStarted() && myPR.IsDone() && myPR.Status.GetCondition(apis.ConditionSucceeded).IsTrue()
 			}, avgPipelineCompletionTime, defaultInterval).Should(BeTrue())
-		})
-
-		It("The Release should have succeeded", func() {
-			Eventually(func() bool {
-				release, err := framework.ReleaseController.GetRelease(releaseName, devNamespace)
-
-				if err != nil {
-					return false
-				}
-
-				return release.IsDone() && meta.IsStatusConditionTrue(release.Status.Conditions, "Succeeded")
-			}, avgPipelineCompletionTime, defaultInterval).Should(BeTrue())
-		})
-
-		It("The Release should reference the release PipelineRun", func() {
-			var pipelineRun *v1beta1.PipelineRun
-
-			Eventually(func() bool {
-				pipelineRun, err = framework.ReleaseController.GetPipelineRunInNamespace(managedNamespace, releaseName, devNamespace)
-
-				return pipelineRun != nil && err == nil
-			}, avgPipelineCompletionTime, defaultInterval).Should(BeTrue())
-
-			release, err := framework.ReleaseController.GetRelease(releaseName, devNamespace)
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(release.Status.ReleasePipelineRun).Should(Equal(fmt.Sprintf("%s/%s", pipelineRun.Namespace, pipelineRun.Name)))
 		})
 	})
+
+	It("A PipelineRun should have been created in the managed namespace", func() {
+		Eventually(func() bool {
+			pipelineRun, err := framework.TektonController.GetPipelineRunInNamespace(managedNamespace)
+			myPR := &pipelineRun.Items[0]
+			if pipelineRun == nil || err != nil {
+				return false
+			}
+			return myPR.HasStarted() && myPR.IsDone() && myPR.Status.GetCondition(apis.ConditionSucceeded).IsTrue()
+		}, avgPipelineCompletionTime, defaultInterval).Should(BeTrue())
+	})
+
 })
