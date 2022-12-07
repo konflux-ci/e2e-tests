f33ed5bf46ebb8febda3fbc9402136b886f72321
Naftaly Shprai <nshprai@eredhat.com>
2022-12-07 17:57:44 +0200

    Changed test name to the happy-path original name

--- tests/release/release.go
+++ tests/release/release.go
@@ -37,7 +37,7 @@ var ecPolicy = ecp.EnterpriseContractPolicySpec{
 
 var paramsReleaseStrategy = []appstudiov1alpha1.Params{}
 
-var _ = framework.ReleaseSuiteDescribe("[HACBS-1142]-test-release-service-happy-path-with-granular-time-thresholds ", Label("release", "HACBS"), func() {
+var _ = framework.ReleaseSuiteDescribe("[HACBS-1108]test-release-service-happy-path", Label("release", "HACBS"), func() {
 	defer GinkgoRecover()
 	// Initialize the tests controllers
 	framework, err := framework.NewFramework()
