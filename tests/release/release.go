5d2cf29efbe53cb3b2f0ad28786d13c112e579a1
Zoran Regvart <zoran@regvart.com>
2022-12-23 10:15:06 +0100

    Revert "fix: disable release tests"
    This reverts commit d2d5ce25d5e77e94fe401ffe21cb564487d89bdd.

--- tests/release/release.go
+++ tests/release/release.go
@@ -44,7 +44,7 @@ var ecPolicy = ecp.EnterpriseContractPolicySpec{
 
 var paramsReleaseStrategy = []appstudiov1alpha1.Params{}
 
-var _ = framework.ReleaseSuiteDescribe("[HACBS-1108]test-release-service-happy-path", Label("release", "HACBS"), Pending, func() {
+var _ = framework.ReleaseSuiteDescribe("[HACBS-1108]test-release-service-happy-path", Label("release", "HACBS"), func() {
 	defer GinkgoRecover()
 	// Initialize the tests controllers
 	framework, err := framework.NewFramework()
