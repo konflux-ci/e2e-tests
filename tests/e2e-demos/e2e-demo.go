607a1f1386b409376ff395be44c5f87a4ffebf5d
Rafaela Soares <rsoaresd@redhat.com>
2023-02-15 11:08:32 +0000

    check nil pointer

--- tests/e2e-demos/e2e-demo.go
+++ tests/e2e-demos/e2e-demo.go
@@ -199,7 +199,10 @@ var _ = framework.E2ESuiteDescribe(Label("e2e-demo"), func() {
 					Expect(err).ShouldNot(HaveOccurred())
 
 					Eventually(func() bool {
-						return fw.IntegrationController.HaveHACBSTestsSucceeded(snapshot)
+						if snapshot != nil {
+							return fw.IntegrationController.HaveHACBSTestsSucceeded(snapshot)
+						}
+						return false
 
 					}, timeout, interval).Should(BeTrue(), fmt.Sprintf("time out when trying to check if the snapshot %s is marked as successful", snapshot.Name))
 				})
