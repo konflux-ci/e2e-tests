c94efb53cc80105d7f5f5fa805284010a1603677
Flavius Lacatusu <flacatus@redhat.com>
2023-03-30 17:51:40 +0200

    fix: add quality-dashboard repo as multicomponent

--- tests/e2e-demos/multi-component.go
+++ tests/e2e-demos/multi-component.go
@@ -2,10 +2,8 @@ package e2e
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
-	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	appservice "github.com/redhat-appstudio/application-api/api/v1alpha1"
@@ -13,29 +11,24 @@ import (
 	"github.com/redhat-appstudio/e2e-tests/pkg/framework"
 	"github.com/redhat-appstudio/e2e-tests/pkg/utils"
 	"github.com/redhat-appstudio/e2e-tests/tests/e2e-demos/config"
-	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/api/core/v1"
 )
 
 const (
-	MultiComponentDemoNamespace string = "multi-comp-e2e"
+	multiComponentTestNamespace string = "multi-comp-e2e"
 )
 
-var _ = framework.E2ESuiteDescribe(Label("e2e-demo"), func() {
-	// TODO investigate failing of Component detection
-	var timeout, interval time.Duration
-
+var _ = framework.E2ESuiteDescribe(Label("e2e-demo", "multi-component"), func() {
 	defer GinkgoRecover()
 
 	var fw *framework.Framework
 	var err error
+	var namespace string
 
 	// Initialize the application struct
 	application := &appservice.Application{}
 	cdq := &appservice.ComponentDetectionQuery{}
-	componentGo := &appservice.Component{}
-	componentNode := &appservice.Component{}
-	snapshotGo := &appservice.Snapshot{}
-	snapshotNode := &appservice.Snapshot{}
+	componentList := []*appservice.Component{}
 	env := &appservice.Environment{}
 
 	var testSpecification = config.WorkflowSpec{
@@ -45,222 +38,130 @@ var _ = framework.E2ESuiteDescribe(Label("e2e-demo"), func() {
 				ApplicationName: "multi-component-application",
 				Components: []config.ComponentSpec{
 					{
-						Name:         "multi-component",
+						Name:         "mc-withdockerfile-withoutdevfile",
 						Type:         "public",
-						GitSourceUrl: "https://github.com/redhat-appstudio-qe/multi-component-example-main",
+						GitSourceUrl: "https://github.com/redhat-appstudio/quality-dashboard.git",
 					},
 				},
 			},
 		},
 	}
 
-	var compNameGo string
-	var compNameNode string
+	for _, suite := range testSpecification.Tests {
+		Describe(suite.Name, Ordered, func() {
+			BeforeAll(func() {
+				if suite.Skip {
+					Skip(fmt.Sprintf("test skipped %s", suite.Name))
+				}
 
-	var removeApplication = true
-
-	var namespace string
-
-	Describe(testSpecification.Tests[0].ApplicationName, Ordered, func() {
-		BeforeAll(func() {
-			Skip("skip tests due a issue with dockerfile detections. See jira: https://issues.redhat.com/browse/DEVHAS-266")
-			// Initialize the tests controllers
-			fw, err = framework.NewFramework(MultiComponentDemoNamespace)
-			Expect(err).NotTo(HaveOccurred())
-			namespace = fw.UserNamespace
-			// Check to see if the github token was provided
-			Expect(utils.CheckIfEnvironmentExists(constants.GITHUB_TOKEN_ENV)).Should(BeTrue(), "%s environment variable is not set", constants.GITHUB_TOKEN_ENV)
-			// Check test specification has at least one test defined
-			Expect(len(testSpecification.Tests)).To(BeNumerically(">", 0))
-		})
-
-		// Remove all resources created by the tests
-		AfterAll(func() {
-			if removeApplication {
-				Expect(fw.AsKubeDeveloper.HasController.DeleteAllComponentsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
-				Expect(fw.AsKubeAdmin.HasController.DeleteAllApplicationsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
-				Expect(fw.SandboxController.DeleteUserSignup(fw.UserName)).NotTo(BeFalse())
-			}
-		})
-
-		It("creates Red Hat AppStudio Application", func() {
-			createdApplication, err := fw.AsKubeDeveloper.HasController.CreateHasApplication(testSpecification.Tests[0].ApplicationName, namespace)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(createdApplication.Spec.DisplayName).To(Equal(testSpecification.Tests[0].ApplicationName))
-			Expect(createdApplication.Namespace).To(Equal(namespace))
-		})
-
-		It("checks Red Hat AppStudio Application health", func() {
-			Eventually(func() string {
-				application, err = fw.AsKubeDeveloper.HasController.GetHasApplication(testSpecification.Tests[0].ApplicationName, namespace)
+				// Initialize the tests controllers
+				fw, err = framework.NewFramework(utils.GetGeneratedNamespace(multiComponentTestNamespace))
 				Expect(err).NotTo(HaveOccurred())
+				namespace = fw.UserNamespace
+				Expect(namespace).NotTo(BeEmpty())
 
-				return application.Status.Devfile
-			}, 3*time.Minute, 100*time.Millisecond).Should(Not(BeEmpty()), "Error creating gitOps repository")
+				suiteConfig, _ := GinkgoConfiguration()
+				GinkgoWriter.Printf("Parallel processes: %d\n", suiteConfig.ParallelTotal)
+				GinkgoWriter.Printf("Running on namespace: %s\n", namespace)
+				GinkgoWriter.Printf("User: %s\n", fw.UserName)
 
-			Eventually(func() bool {
-				// application info should be stored even after deleting the application in application variable
-				gitOpsRepository := utils.ObtainGitOpsRepositoryName(application.Status.Devfile)
+				githubCredentials := `{"access_token":"` + utils.GetEnv(constants.GITHUB_TOKEN_ENV, "") + `"}`
 
-				return fw.AsKubeDeveloper.CommonController.Github.CheckIfRepositoryExist(gitOpsRepository)
-			}, 1*time.Minute, 1*time.Second).Should(BeTrue(), "Has controller didn't create gitops repository")
-		})
+				_ = fw.AsKubeDeveloper.SPIController.InjectManualSPIToken(namespace, fmt.Sprintf("https://github.com/%s", utils.GetEnv(constants.GITHUB_E2E_ORGANIZATION_ENV, "redhat-appstudio-qe")), githubCredentials, v1.SecretTypeBasicAuth, SPIGithubSecretName)
 
-		// Create an environment in a specific namespace
-		It("creates an environment", func() {
-			env, err = fw.AsKubeDeveloper.IntegrationController.CreateEnvironment(namespace, EnvironmentName)
-			Expect(err).NotTo(HaveOccurred())
-		})
+			})
 
-		It("creates Red Hat AppStudio ComponentDetectionQuery for Component repository", func() {
-			cdq, err := fw.AsKubeDeveloper.HasController.CreateComponentDetectionQuery(
-				testSpecification.Tests[0].Components[0].Name,
-				namespace,
-				testSpecification.Tests[0].Components[0].GitSourceUrl,
-				testSpecification.Tests[0].Components[0].GitSourceRevision,
-				testSpecification.Tests[0].Components[0].GitSourceContext,
-				"",
-				false)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(cdq.Name).To(Equal(testSpecification.Tests[0].Components[0].Name))
-		})
-
-		It("checks Red Hat AppStudio ComponentDetectionQuery status", func() {
-			// Validate that the CDQ completes successfully
-			Eventually(func() bool {
-				// application info should be stored even after deleting the application in application variable
-				cdq, err = fw.AsKubeDeveloper.HasController.GetComponentDetectionQuery(testSpecification.Tests[0].Components[0].Name, namespace)
-				return err == nil && len(cdq.Status.ComponentDetected) > 0
-			}, 1*time.Minute, 1*time.Second).Should(BeTrue(), "ComponentDetectionQuery did not complete successfully")
-
-			// Validate that the completed CDQ only has detected the two components (nodejs and go)
-			Expect(len(cdq.Status.ComponentDetected)).To(Equal(2), "Expected length of the detected Components was not 2")
-
-			// get the name of the components for future use and validate they are go and nodejs
-			for key, element := range cdq.Status.ComponentDetected {
-				if element.Language == "go" {
-					compNameGo = key
+			// Remove all resources created by the tests
+			AfterAll(func() {
+				if !CurrentSpecReport().Failed() {
+					Expect(fw.AsKubeDeveloper.HasController.DeleteAllComponentsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.AsKubeAdmin.HasController.DeleteAllApplicationsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.AsKubeAdmin.HasController.DeleteAllSnapshotEnvBindingsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.AsKubeAdmin.ReleaseController.DeleteAllSnapshotsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.AsKubeAdmin.GitOpsController.DeleteAllEnvironmentsInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.AsKubeAdmin.TektonController.DeleteAllPipelineRunsInASpecificNamespace(namespace)).To(Succeed())
+					Expect(fw.AsKubeAdmin.GitOpsController.DeleteAllGitOpsDeploymentInASpecificNamespace(namespace, 30*time.Second)).To(Succeed())
+					Expect(fw.SandboxController.DeleteUserSignup(fw.UserName)).NotTo(BeFalse())
 				}
-				if element.Language == "nodejs" {
-					compNameNode = key
+			})
+
+			// Create an application in a specific namespace
+			It(fmt.Sprintf("create application %s", suite.ApplicationName), func() {
+				GinkgoWriter.Printf("Parallel process %d\n", GinkgoParallelProcess())
+				application, err := fw.AsKubeDeveloper.HasController.CreateHasApplication(suite.ApplicationName, namespace)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(application.Spec.DisplayName).To(Equal(suite.ApplicationName))
+				Expect(application.Namespace).To(Equal(namespace))
+			})
+
+			// Check the application health and check if a devfile was generated in the status
+			It(fmt.Sprintf("checks if application %s is healthy", suite.ApplicationName), func() {
+				Eventually(func() string {
+					appstudioApp, err := fw.AsKubeDeveloper.HasController.GetHasApplication(suite.ApplicationName, namespace)
+					Expect(err).NotTo(HaveOccurred())
+					application = appstudioApp
+
+					return application.Status.Devfile
+				}, 3*time.Minute, 100*time.Millisecond).Should(Not(BeEmpty()), "Error creating gitOps repository")
+
+				Eventually(func() bool {
+					gitOpsRepository := utils.ObtainGitOpsRepositoryName(application.Status.Devfile)
+
+					return fw.AsKubeDeveloper.CommonController.Github.CheckIfRepositoryExist(gitOpsRepository)
+				}, 5*time.Minute, 1*time.Second).Should(BeTrue(), "Has controller didn't create gitops repository")
+			})
+
+			It(fmt.Sprintf("creates ComponentDetectionQuery for application %s", suite.ApplicationName), func() {
+				cdq, err = fw.AsKubeDeveloper.HasController.CreateComponentDetectionQuery(
+					suite.Components[0].Name,
+					namespace,
+					suite.Components[0].GitSourceUrl,
+					suite.Components[0].GitSourceRevision,
+					suite.Components[0].GitSourceContext,
+					"",
+					false,
+				)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(cdq.Name).To(Equal(suite.Components[0].Name))
+			})
+
+			// Create an environment in a specific namespace
+			It(fmt.Sprintf("creates environment %s", EnvironmentName), func() {
+				env, err = fw.AsKubeDeveloper.IntegrationController.CreateEnvironment(namespace, EnvironmentName)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It(fmt.Sprintf("creates multiple components in application %s", suite.ApplicationName), func() {
+				for _, component := range cdq.Status.ComponentDetected {
+					c, err := fw.AsKubeDeveloper.HasController.CreateComponentFromStub(component, component.ComponentStub.ComponentName, namespace, SPIGithubSecretName, application.Name, "")
+					Expect(err).NotTo(HaveOccurred())
+					Expect(c.Name).To(Equal(component.ComponentStub.ComponentName))
+					componentList = append(componentList, c)
 				}
-			}
+			})
 
-			_, golang := cdq.Status.ComponentDetected[compNameGo]
-			Expect(golang).To(BeTrue(), "Expect Golang component to be detected")
-			_, nodejs := cdq.Status.ComponentDetected[compNameNode]
-			Expect(nodejs).To(BeTrue(), "Expect NodeJS component to be detected")
-		})
+			It(fmt.Sprintf("waits application %s components pipelines to be finished", application.Name), func() {
+				GinkgoWriter.Print(env)
+				for _, component := range componentList {
+					if err := fw.AsKubeDeveloper.HasController.WaitForComponentPipelineToBeFinished(fw.AsKubeAdmin.CommonController, component.Name, suite.ApplicationName, namespace, ""); err != nil {
+						Fail(fmt.Sprint(err))
+					}
+				}
+			})
 
-		It("creates multiple components", func() {
-			// Create Golang component from CDQ result
-			Expect(cdq.Status.ComponentDetected[compNameGo].DevfileFound).To(BeTrue(), "DevfileFound was not set to true")
-			componentDescription := cdq.Status.ComponentDetected[compNameGo]
-			componentDescription.ComponentStub.ContainerImage = fmt.Sprintf("quay.io/%s/test-images:%s", utils.GetQuayIOOrganization(), strings.Replace(uuid.New().String(), "-", "", -1))
-			componentGo, err = fw.AsKubeDeveloper.HasController.CreateComponentFromStub(componentDescription, compNameGo, namespace, "", testSpecification.Tests[0].ApplicationName, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(componentGo.Name).To(Equal(compNameGo))
+			It(fmt.Sprintf("finds the application %s components snapshots and checks if it is marked as successfully", suite.ApplicationName), func() {
+				timeout := time.Second * 600
+				interval := time.Second * 10
 
-			// Create NodeJS component from CDQ result
-			Expect(cdq.Status.ComponentDetected[compNameNode].DevfileFound).To(BeTrue(), "DevfileFound was not set to true")
-			componentDescription = cdq.Status.ComponentDetected[compNameNode]
-			componentDescription.ComponentStub.ContainerImage = fmt.Sprintf("quay.io/%s/test-images:%s", utils.GetQuayIOOrganization(), strings.Replace(uuid.New().String(), "-", "", -1))
-			componentNode, err = fw.AsKubeDeveloper.HasController.CreateComponentFromStub(componentDescription, compNameNode, namespace, "", testSpecification.Tests[0].ApplicationName, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(componentNode.Name).To(Equal(compNameNode))
-		})
-
-		// Start to watch the pipeline until is finished
-		It("waits for all pipelines to be finished", func() {
-			err := fw.AsKubeDeveloper.HasController.WaitForComponentPipelineToBeFinished(fw.AsKubeAdmin.CommonController, compNameGo, testSpecification.Tests[0].ApplicationName, namespace, "")
-			if err != nil {
-				removeApplication = false
-				Fail(fmt.Sprint(err))
-			}
-
-			err = fw.AsKubeDeveloper.HasController.WaitForComponentPipelineToBeFinished(fw.AsKubeAdmin.CommonController, compNameNode, testSpecification.Tests[0].ApplicationName, namespace, "")
-			if err != nil {
-				removeApplication = false
-				Fail(fmt.Sprint(err))
-			}
-		})
-
-		It("finds the snapshot and checks if it is marked as successful for golang component", func() {
-			timeout = time.Second * 600
-			interval = time.Second * 10
-
-			snapshotGo, err = fw.AsKubeDeveloper.IntegrationController.GetApplicationSnapshot("", application.Name, namespace, componentGo.Name)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			Eventually(func() bool {
-				return fw.AsKubeDeveloper.IntegrationController.HaveHACBSTestsSucceeded(snapshotGo)
-
-			}, timeout, interval).Should(BeTrue(), "time out when trying to check if the snapshot is marked as successful")
-		})
-
-		It("finds the snapshot and checks if it is marked as successful for NodeJS component", func() {
-			timeout = time.Second * 600
-			interval = time.Second * 10
-
-			snapshotNode, err = fw.AsKubeDeveloper.IntegrationController.GetApplicationSnapshot("", application.Name, namespace, componentNode.Name)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			Eventually(func() bool {
-				return fw.AsKubeDeveloper.IntegrationController.HaveHACBSTestsSucceeded(snapshotNode)
-
-			}, timeout, interval).Should(BeTrue(), "time out when trying to check if the snapshot is marked as successful")
-		})
-
-		It("checks if a golang snapshot environment binding is created successfully", func() {
-			Eventually(func() bool {
-				if fw.AsKubeDeveloper.IntegrationController.HaveHACBSTestsSucceeded(snapshotGo) {
-					envbinding, err := fw.AsKubeDeveloper.IntegrationController.GetSnapshotEnvironmentBinding(application.Name, namespace, env)
+				for _, component := range componentList {
+					componentSnapshot, err := fw.AsKubeDeveloper.IntegrationController.GetApplicationSnapshot("", application.Name, namespace, component.Name)
 					Expect(err).ShouldNot(HaveOccurred())
-					GinkgoWriter.Printf("The EnvironmentBinding %s is created\n", envbinding.Name)
-					return true
-				}
 
-				snapshotGo, err = fw.AsKubeDeveloper.IntegrationController.GetApplicationSnapshot("", application.Name, namespace, compNameGo)
-				Expect(err).ShouldNot(HaveOccurred())
-				return false
-			}, timeout, interval).Should(BeTrue(), "time out when waiting for release created")
+					Eventually(func() bool {
+						return fw.AsKubeDeveloper.IntegrationController.HaveHACBSTestsSucceeded(componentSnapshot)
+					}, timeout, interval).Should(BeTrue(), "time out when trying to check if the snapshot is marked as successful")
+				}
+			})
 		})
-
-		It("checks if a Nodejs snapshot environment binding is created successfully", func() {
-			Eventually(func() bool {
-				if fw.AsKubeDeveloper.IntegrationController.HaveHACBSTestsSucceeded(snapshotGo) {
-					envbinding, err := fw.AsKubeDeveloper.IntegrationController.GetSnapshotEnvironmentBinding(application.Name, namespace, env)
-					Expect(err).ShouldNot(HaveOccurred())
-					GinkgoWriter.Printf("The EnvironmentBinding %s is created\n", envbinding.Name)
-					return true
-				}
-
-				snapshotGo, err = fw.AsKubeDeveloper.IntegrationController.GetApplicationSnapshot("", application.Name, namespace, componentNode.Name)
-				Expect(err).ShouldNot(HaveOccurred())
-				return false
-			}, timeout, interval).Should(BeTrue(), "time out when waiting for release created")
-		})
-
-		// Check components are deployed
-		It("checks if multiple components are deployed", Pending, func() {
-			Eventually(func() bool {
-				deploymentGo, err := fw.AsKubeDeveloper.CommonController.GetAppDeploymentByName(compNameGo, namespace)
-				if err != nil && !errors.IsNotFound(err) {
-					return false
-				}
-
-				deploymentNode, err := fw.AsKubeDeveloper.CommonController.GetAppDeploymentByName(compNameNode, namespace)
-				if err != nil && !errors.IsNotFound(err) {
-					return false
-				}
-
-				if deploymentGo.Status.AvailableReplicas == 1 && deploymentNode.Status.AvailableReplicas == 1 {
-					return true
-				}
-
-				return false
-			}, 15*time.Minute, 10*time.Second).Should(BeTrue(), "Component deployment didn't become ready")
-			Expect(err).NotTo(HaveOccurred())
-		})
-	})
+	}
 })
