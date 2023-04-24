0f7a5619b54df866ef2695ce2253ad3622b41b6b
Sri Vignesh <sselvan@redhat.com>
2023-04-24 13:23:05 +0530

    test: Add tests for o11y (#450)
    seperated deployments and pipelineruns tests
    moved the common script to getImagePushScript function
    added new deployment and pipelineruns tests
    
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    Co-authored-by: Flavius Lacatusu <flacatus@redhat.com>

--- pkg/utils/o11y/controller.go
+++ pkg/utils/o11y/controller.go
@@ -1,19 +1,27 @@
 package o11y
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
-	"math"
 	"os/exec"
 	"regexp"
 	"strconv"
+	"strings"
+	"time"
 
 	kubeCl "github.com/redhat-appstudio/e2e-tests/pkg/apis/kubernetes"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type SuiteController struct {
@@ -31,9 +39,9 @@ func NewSuiteController(kube *kubeCl.CustomClient) (*SuiteController, error) {
 	}, nil
 }
 
-func (h *SuiteController) convertBytesToMB(bytesValue float64) float64 {
-	megabytesValue := bytesValue / (1000 * 1000)
-	return math.Round(megabytesValue*10) / 10
+func (h *SuiteController) convertBytesToMB(valueInBytes float64) int {
+	valueInMegabytes := valueInBytes / (1000 * 1000)
+	return int(valueInMegabytes)
 }
 
 // Fetch metrics for given query
@@ -61,39 +69,131 @@ func (h *SuiteController) GetMetrics(query string) ([]MetricResult, error) {
 	return result.Data.Result, nil
 }
 
-func (h *SuiteController) GetRegexPodNameWithSize(podNameRegex string, results []MetricResult) (map[string]float64, error) {
-	podNameWithSize := make(map[string]float64)
+func (h *SuiteController) GetRegexPodNameWithResult(podNameRegex string, results []MetricResult) (map[string]string, error) {
+	podNamesWithResult := make(map[string]string)
 	regex, err := regexp.Compile(podNameRegex)
 	if err != nil {
-		return podNameWithSize, fmt.Errorf("error compiling regex pattern: %v", err)
+		return podNamesWithResult, fmt.Errorf("error compiling regex pattern: %v", err)
 	}
 
 	for _, res := range results {
 		if podName, ok := res.Metric["pod"]; ok {
 			if regex.MatchString(podName) {
 				value := res.Value[1].(string)
-				valueInBytes, err := strconv.ParseFloat(value, 64)
-				if err != nil {
-					return nil, fmt.Errorf("error parsing value for %s: %s", podName, err)
-				}
-				valueInMegabytes := h.convertBytesToMB(valueInBytes)
-				podNameWithSize[podName] = valueInMegabytes
+				podNamesWithResult[podName] = value
 			}
 		}
 	}
 
-	if len(podNameWithSize) == 0 {
+	if len(podNamesWithResult) == 0 {
 		return nil, fmt.Errorf("no pods matching the regex pattern were found")
 	}
 
-	return podNameWithSize, nil
+	return podNamesWithResult, nil
+}
+
+func (h *SuiteController) ConvertValuesToMB(podNamesWithResult map[string]string) (map[string]int, error) {
+	podNameWithMB := make(map[string]int)
+
+	for podName, value := range podNamesWithResult {
+		valueStr := value
+
+		valueInBytes, err := strconv.ParseFloat(valueStr, 64)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing value for %s: %s", podName, err)
+		}
+
+		valueInMegabytes := h.convertBytesToMB(valueInBytes)
+		podNameWithMB[podName] = int(valueInMegabytes)
+	}
+
+	return podNameWithMB, nil
+}
+
+func labelsToSelector(labelMap map[string]string) labels.Selector {
+	selector := labels.NewSelector()
+	for key, value := range labelMap {
+		req, _ := labels.NewRequirement(key, selection.Equals, []string{value})
+		selector = selector.Add(*req)
+	}
+	return selector
+}
+
+func (s *SuiteController) WaitForScriptCompletion(deployment *appsv1.Deployment, successMessage string, timeout time.Duration) error {
+	namespace := deployment.Namespace
+	deploymentName := deployment.Name
+
+	// Get the pod associated with the deployment
+	podList := &corev1.PodList{}
+	labels := deployment.Spec.Selector.MatchLabels
+	labelSelector := labelsToSelector(labels)
+	err := s.KubeRest().List(context.Background(), podList, client.InNamespace(namespace), client.MatchingLabelsSelector{Selector: labelSelector})
+	if err != nil {
+		return err
+	}
+
+	if len(podList.Items) == 0 {
+		return fmt.Errorf("no pods found for deployment %s", deploymentName)
+	}
+
+	pod := podList.Items[0]
+
+	// Wait for the success message in the pod's log output
+	podLogOpts := &corev1.PodLogOptions{}
+	req := s.KubeInterface().CoreV1().Pods(namespace).GetLogs(pod.Name, podLogOpts)
+
+	err = wait.PollImmediate(time.Second, timeout, func() (bool, error) {
+		readCloser, err := req.Stream(context.Background())
+		if err != nil {
+			return false, err
+		}
+		defer readCloser.Close()
+
+		scanner := bufio.NewScanner(readCloser)
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), successMessage) {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+
+	return err
+}
+
+func (h *SuiteController) getImagePushScript(secret, quayOrg string) string {
+	return fmt.Sprintf(`#!/bin/sh
+authFilePath="/tekton/creds-secrets/%s/.dockerconfigjson"
+destImageRef="quay.io/%s/o11y-workloads"
+# Set Permissions
+sed -i 's/^\s*short-name-mode\s*=\s*.*/short-name-mode = "disabled"/' /etc/containers/registries.conf
+echo 'root:1:4294967294' | tee -a /etc/subuid >> /etc/subgid
+# Pull Image
+echo -e "FROM quay.io/libpod/alpine:latest\nRUN dd if=/dev/urandom of=/100mbfile bs=1M count=100" > Dockerfile
+unshare -Ufp --keep-caps -r --map-users 1,1,65536 --map-groups 1,1,65536 -- buildah bud --tls-verify=false --no-cache -f ./Dockerfile -t "$destImageRef" .
+IMAGE_SHA_DIGEST=$(buildah images --digests | grep ${destImageRef} | awk '{print $4}')
+TAGGED_IMAGE_NAME="${destImageRef}:${IMAGE_SHA_DIGEST}"
+buildah tag ${destImageRef} ${TAGGED_IMAGE_NAME}
+buildah images
+buildah push --authfile "$authFilePath" --disable-compression --tls-verify=false ${TAGGED_IMAGE_NAME}
+if [ $? -eq 0 ]; then
+  # Scraping Interval Period, Pod must stay alive
+  sleep 1m
+  echo "Image push completed"
+else
+  echo "Image push failed"
+  exit 1
+fi`, secret, quayOrg)
 }
 
 func (h *SuiteController) QuayImagePushPipelineRun(quayOrg, secret, namespace string) (*v1beta1.PipelineRun, error) {
 	pipelineRun := &v1beta1.PipelineRun{
 		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: "pipeline-egress-",
+			GenerateName: "pipelinerun-egress-",
 			Namespace:    namespace,
+			Labels: map[string]string{
+				"pipelines.appstudio.openshift.io/type": "test",
+			},
 		},
 		Spec: v1beta1.PipelineRunSpec{
 			PipelineSpec: &v1beta1.PipelineSpec{
@@ -110,24 +210,7 @@ func (h *SuiteController) QuayImagePushPipelineRun(quayOrg, secret, namespace st
 											{Name: "BUILDAH_FORMAT", Value: "oci"},
 											{Name: "STORAGE_DRIVER", Value: "vfs"},
 										},
-										Script: fmt.Sprintf(`#!/bin/sh
-												authFilePath="/tekton/creds-secrets/%s/.dockerconfigjson"
-												destImageRef="quay.io/%s/o11y-workloads"
-												# Set Permissions
-												sed -i 's/^\s*short-name-mode\s*=\s*.*/short-name-mode = "disabled"/' /etc/containers/registries.conf
-												# Setting new namespace to run buildah - 2^32-2
-												echo 'root:1:4294967294' | tee -a /etc/subuid >> /etc/subgid
-												# Pull Image
-												echo -e "FROM quay.io/libpod/alpine:latest\nRUN dd if=/dev/urandom of=/100mbfile bs=1M count=100" > Dockerfile
-												unshare -Ufp --keep-caps -r --map-users 1,1,65536 --map-groups 1,1,65536 -- buildah bud --tls-verify=false --no-cache -f ./Dockerfile -t "$destImageRef" .
-												IMAGE_SHA_DIGEST=$(buildah images --digests | grep ${destImageRef} | awk '{print $4}')
-												TAGGED_IMAGE_NAME="${destImageRef}:${IMAGE_SHA_DIGEST}"
-												buildah tag ${destImageRef} ${TAGGED_IMAGE_NAME}
-												buildah images
-												buildah push --authfile "$authFilePath" --disable-compression --tls-verify=false ${TAGGED_IMAGE_NAME}
-												echo "Successfully pushed Image"
-												# Scraping Interval Period, Pod must stay alive:
-												sleep 1m`, secret, quayOrg),
+										Script: h.getImagePushScript(secret, quayOrg),
 										SecurityContext: &corev1.SecurityContext{
 											RunAsUser: pointer.Int64(0),
 											Capabilities: &corev1.Capabilities{
@@ -151,4 +234,180 @@ func (h *SuiteController) QuayImagePushPipelineRun(quayOrg, secret, namespace st
 	}
 
 	return pipelineRun, nil
+}
+
+func (h *SuiteController) VCPUMinutesPipelineRun(namespace string) (*v1beta1.PipelineRun, error) {
+	pipelineRun := &v1beta1.PipelineRun{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "pipelinerun-vcpu-",
+			Namespace:    namespace,
+			Labels: map[string]string{
+				"pipelines.appstudio.openshift.io/type": "test",
+			},
+		},
+		Spec: v1beta1.PipelineRunSpec{
+			PipelineSpec: &v1beta1.PipelineSpec{
+				Tasks: []v1beta1.PipelineTask{
+					{
+						Name: "vcpu-minutes",
+						TaskSpec: &v1beta1.EmbeddedTask{
+							TaskSpec: v1beta1.TaskSpec{
+								Steps: []v1beta1.Step{
+									{
+										Name:   "resource-constraint",
+										Image:  "registry.access.redhat.com/ubi9/ubi-micro",
+										Script: "#!/usr/bin/env bash\nsleep 1m\necho 'vCPU Deployment Completed'\n",
+										Resources: corev1.ResourceRequirements{
+											Requests: corev1.ResourceList{
+												corev1.ResourceMemory: resource.MustParse("200Mi"),
+												corev1.ResourceCPU:    resource.MustParse("200m"),
+											},
+											Limits: corev1.ResourceList{
+												corev1.ResourceMemory: resource.MustParse("200Mi"),
+												corev1.ResourceCPU:    resource.MustParse("200m"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.KubeRest().Create(context.Background(), pipelineRun); err != nil {
+		return nil, err
+	}
+
+	return pipelineRun, nil
+}
+
+func (h *SuiteController) QuayImagePushDeployment(quayOrg, secret, namespace string) (*appsv1.Deployment, error) {
+	Deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment-egress",
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "deployment-egress",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "deployment-egress",
+					},
+				},
+				Spec: corev1.PodSpec{
+					ImagePullSecrets: []corev1.LocalObjectReference{
+						{
+							Name: secret,
+						},
+					},
+					ServiceAccountName: "pipeline",
+					Containers: []corev1.Container{
+						{
+							Name:  "quay-image-push-container",
+							Image: "quay.io/redhat-appstudio/buildah:v1.28",
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "docker-config",
+									MountPath: "/tekton/creds-secrets/o11y-tests-token/",
+									ReadOnly:  true,
+								},
+							},
+							Env: []corev1.EnvVar{
+								{Name: "BUILDAH_FORMAT", Value: "oci"},
+								{Name: "STORAGE_DRIVER", Value: "vfs"},
+							},
+							Command: []string{"/bin/sh", "-c"},
+							Args:    []string{h.getImagePushScript(secret, quayOrg)},
+							SecurityContext: &corev1.SecurityContext{
+								RunAsUser: pointer.Int64(0),
+								Capabilities: &corev1.Capabilities{
+									Add: []corev1.Capability{
+										"SETFCAP",
+									},
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "docker-config",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: secret,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.KubeRest().Create(context.Background(), Deployment); err != nil {
+		return &appsv1.Deployment{}, err
+	}
+
+	return Deployment, nil
+}
+
+func (h *SuiteController) VCPUMinutesDeployment(namespace string) (*appsv1.Deployment, error) {
+	Deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment-vcpu",
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "deployment-vcpu",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "deployment-vcpu",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "vcpu-minutes",
+							Image: "registry.access.redhat.com/ubi9/ubi-micro",
+							Command: []string{
+								"/bin/bash",
+								"-c",
+								"sleep 1m ; echo 'vCPU Deployment Completed'",
+							},
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("200Mi"),
+									corev1.ResourceCPU:    resource.MustParse("200m"),
+								},
+								Limits: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("200Mi"),
+									corev1.ResourceCPU:    resource.MustParse("200m"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.KubeRest().Create(context.Background(), Deployment); err != nil {
+		return nil, err
+	}
+
+	return Deployment, nil
 }
