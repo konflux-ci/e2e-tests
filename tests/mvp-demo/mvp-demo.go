92744fdb4bf3c0b850882b4834bea33cc84a1a8e
David Moreno García <david.mogar@gmail.com>
2023-04-28 08:57:21 +0200

    feat(HACBS-1777): adapt test to run for latest Release refactor (#461)
    The Release service code has been refactor and includes some breaking
    changes that had to be addressed within the E2E tests. This commit
    introduce those changes.
    
    Signed-off-by: David Moreno García <damoreno@redhat.com>

--- tests/mvp-demo/mvp-demo.go
+++ tests/mvp-demo/mvp-demo.go
@@ -270,7 +270,7 @@ var _ = framework.MvpDemoSuiteDescribe("MVP Demo tests", Label("mvp-demo"), func
 					GinkgoWriter.Printf("failed to get Release CR in '%s' namespace: %+v\n", managedNamespace, err)
 					return false
 				}
-				return release.HasStarted()
+				return release.IsReleasing()
 			}, customResourceUpdateTimeout, defaultPollingInterval).Should(BeTrue())
 		})
 
@@ -292,7 +292,7 @@ var _ = framework.MvpDemoSuiteDescribe("MVP Demo tests", Label("mvp-demo"), func
 					GinkgoWriter.Printf("failed to get Release CR in '%s' namespace: %+v\n", managedNamespace, err)
 					return false
 				}
-				return release.IsDone() && !release.HasSucceeded()
+				return release.HasReleaseFinished() && !release.IsReleased()
 			}, customResourceUpdateTimeout, defaultPollingInterval).Should(BeTrue())
 		})
 
@@ -454,7 +454,7 @@ var _ = framework.MvpDemoSuiteDescribe("MVP Demo tests", Label("mvp-demo"), func
 					GinkgoWriter.Printf("failed to get Release CR in '%s' namespace: %+v\n", managedNamespace, err)
 					return false
 				}
-				return release.HasStarted()
+				return release.IsReleasing()
 			}, customResourceUpdateTimeout, defaultPollingInterval).Should(BeTrue())
 		})
 
@@ -474,7 +474,7 @@ var _ = framework.MvpDemoSuiteDescribe("MVP Demo tests", Label("mvp-demo"), func
 					GinkgoWriter.Printf("failed to get Release CR in '%s' namespace: %+v\n", managedNamespace, err)
 					return false
 				}
-				return release.IsDone() && release.HasSucceeded()
+				return release.IsReleased()
 			}, customResourceUpdateTimeout, defaultPollingInterval).Should(BeTrue())
 		})
 
