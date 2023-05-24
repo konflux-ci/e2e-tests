463fd3b50f7af3173c94cad7389c7ff0da40104b
Pavel Macík <pavel.macik@gmail.com>
2023-05-24 14:34:09 +0200

    feat(load-test): Include more detailed information about errors occurred during test execution (#497)
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- tests/load-tests/ci-scripts/load-test.sh
+++ tests/load-tests/ci-scripts/load-test.sh
@@ -9,7 +9,10 @@ source "/usr/local/ci-secrets/redhat-appstudio-load-test/load-test-scenario.${1:
 
 pushd "${2:-./tests/load-tests}"
 
-export DOCKER_CONFIG_JSON=$QUAY_TOKEN
+export DOCKER_CONFIG_JSON QUAY_E2E_ORGANIZATION MY_GITHUB_ORG GITHUB_TOKEN
+DOCKER_CONFIG_JSON=$QUAY_TOKEN
+QUAY_E2E_ORGANIZATION=$(cat /usr/local/ci-secrets/redhat-appstudio-load-test/quay-org)
+MY_GITHUB_ORG=$(cat /usr/local/ci-secrets/redhat-appstudio-load-test/github-org)
 
 rate_limits_csv=./gh-rate-limits-remaining.csv
 
