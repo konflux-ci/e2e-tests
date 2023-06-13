daec5a209de5d01aa9832f8d7074630bc5d43d8e
Pavel Macík <pavel.macik@gmail.com>
2023-06-13 11:28:51 +0200

    feat(RHTAP-979): Add possibility in load test to tweak tekton performance (#572)
    * feat(load-test): Add metrics per concurrency to max-concurrency results.
    
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>
    
    * feat(RHTAP-979): Add possibility in load test to tweak tekton performance
    
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>
    
    ---------
    
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- tests/load-tests/run-max-concurrency.sh
+++ tests/load-tests/run-max-concurrency.sh
@@ -32,16 +32,31 @@ else
     threshold=${THRESHOLD:-300}
     echo '{"maxThreads": '"$maxThreads"', "threshold": '"$threshold"', "maxConcurrencyReached": 0}' | jq >"$output"
     for t in $(seq 1 "${MAX_THREADS:-10}"); do
-        oc get usersignups.toolchain.dev.openshift.com -A -o name | grep "$USER_PREFIX" | xargs oc delete -n toolchain-host-operator
-        while true; do
-            echo "Waiting until all namespaces with '$USER_PREFIX' prefix are gone..."
-            oc get ns | grep "$USER_PREFIX" >/dev/null 2>&1 || break 1
-            sleep 5s
+        echo "Deleting resources from previous steps"
+        for res in pipelineruns.tekton.dev components.appstudio.redhat.com componentdetectionqueries.appstudio.redhat.com snapshotenvironmentbindings.appstudio.redhat.com applications.appstudio.redhat.com; do
+            echo -e " * $res"
+            oc get "$res" -A -o json | jq -rc '.items[] | select(.metadata.namespace | startswith("'"$USER_PREFIX"'"))| "oc delete '"$res"' " + .metadata.name + " -n " + .metadata.namespace + ";"' | bash -s
         done
+        oc get usersignups.toolchain.dev.openshift.com -A -o name | grep "$USER_PREFIX" | xargs oc delete -n toolchain-host-operator
+        attempts=60
+        attempt=1
+        sleep="5s"
+        while [ "$attempt" -le "$attempts" ]; do
+            echo " * Waiting $sleep until all namespaces with '$USER_PREFIX' prefix are gone [attempt $attempt/$attempts]"
+            oc get ns | grep -E "^$USER_PREFIX" >/dev/null 2>&1 || break 1
+            sleep "$sleep"
+            attempt=$((attempt + 1))
+        done
+        if [ "$attempt" -le "$attempts" ]; then
+            echo " * All the namespaces with '$USER_PREFIX' are gone!"
+        else
+            echo " * WARNING: Timeout waiting for namespaces with '$USER_PREFIX' to be gone. The following namespaces still exist:"
+            oc get ns | grep -E "^$USER_PREFIX"
+        fi
         load_test "$t"
         index=$(printf "%04d" "$t")
-        mv -f load-tests.json "load-tests.max-concurrency.$index.json"
-        mv -f load-tests.log "load-tests.max-concurrency.$index.log"
+        cp -vf load-tests.json "load-tests.max-concurrency.$index.json"
+        cp -vf load-tests.log "load-tests.max-concurrency.$index.log"
         pipelineRunThresholdExceeded=$(jq -rc ".runPipelineSucceededTimeMax > $threshold" load-tests.json)
         pipelineRunKPI=$(jq -rc ".runPipelineSucceededTimeMax" load-tests.json)
         if [ "$pipelineRunThresholdExceeded" = "true" ]; then
