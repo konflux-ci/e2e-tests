e6cb854348c01cd367736a8f0d83e95a9b02a312
abarbaro <abarbaro@redhat.com>
2023-06-19 21:05:31 +0200

    fix typos

--- pkg/utils/common/deployment.go
+++ pkg/utils/common/deployment.go
@@ -2,11 +2,9 @@ package common
 
 import (
 	"context"
-	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
