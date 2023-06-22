51d90a8927b5436a63607a703011b41418318578
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2023-06-22 23:52:48 +0530

    Add Happy Path coverage for namespace-backed envs
    * Created a new test file to store tests for ns-backed envs (RHTAP-244)
    * Added Happy path scenario for RHTAP-244 inside this new file
    * Moved a negative scenario related to RHTAP-244 into this new file
    
    Signed-off-by: Dheeraj <djodha@redhat.com>

--- pkg/utils/gitops/deploymenttargetclasses.go
+++ pkg/utils/gitops/deploymenttargetclasses.go
@@ -6,6 +6,7 @@ import (
 
 	appservice "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // HaveAvailableDeploymentTargetClassExist attempts to find a DeploymentTargetClass with appstudioApi.Provisioner_Devsandbox as provisioner.
@@ -24,4 +25,37 @@ func (g *GitopsController) HaveAvailableDeploymentTargetClassExist() (*appservic
 	}
 
 	return nil, nil
+}
+
+// CreateDeploymentTargetClass creates a DeploymentTargetClass with a "appstudio.redhat.com/devsandbox" provisioner
+func (g *GitopsController) CreateDeploymentTargetClass() (*appservice.DeploymentTargetClass, error) {
+	dtcls := &appservice.DeploymentTargetClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "test-sandbox-class",
+			Annotations: map[string]string{},
+		},
+		Spec: appservice.DeploymentTargetClassSpec{
+			Provisioner:   appservice.Provisioner_Devsandbox,
+			ReclaimPolicy: "Retain",
+		},
+	}
+
+	err := g.KubeRest().Create(context.TODO(), dtcls)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred when creating the DeploymentTargetClass: %+v", err)
+	}
+	return dtcls, nil
+}
+
+func (g *GitopsController) DeleteDeploymentTargetClass() (error) {
+	dtcls := appservice.DeploymentTargetClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-sandbox-class",
+		},
+	}
+	if err := g.KubeRest().Delete(context.TODO(), &dtcls); err != nil {
+		return fmt.Errorf("error occurred when deleting the DeploymentTargetClass: %+v", err)
+	}
+
+	return nil
 }
