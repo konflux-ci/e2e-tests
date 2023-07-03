f67b18c6cf0bf2b9eef4e743ed029774387ae8c6
Pavel Sturc <psturc@redhat.com>
2023-07-03 10:23:40 +0200

    fix: improve (error) logging (#558)
    * fix: improve (error) logging
    
    * fix: address comments

--- tests/build/build_templates.go
+++ tests/build/build_templates.go
@@ -19,11 +19,9 @@ import (
 	"github.com/redhat-appstudio/e2e-tests/pkg/utils/tekton"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/util/rand"
-	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "HACBS"), func() {
+var _ = framework.BuildSuiteDescribe("Build templates E2E test", Label("build", "HACBS"), func() {
 	var f *framework.Framework
 	var err error
 
@@ -32,8 +30,6 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 
 		var applicationName, componentName, testNamespace string
 		var kubeadminClient *framework.ControllerHub
-		pipelineCreatedRetryInterval := time.Second * 5
-		pipelineCreatedTimeout := time.Minute * 5
 
 		BeforeAll(func() {
 			if os.Getenv("APP_SUFFIX") != "" {
@@ -64,7 +60,7 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 				Eventually(func() bool {
 					_, err := kubeadminClient.HasController.GetApplication(applicationName, testNamespace)
 					return errors.IsNotFound(err)
-				}, time.Minute*5, time.Second*1).Should(BeTrue(), "timed out when waiting for the app %s to be deleted in %s namespace", applicationName, testNamespace)
+				}, time.Minute*5, time.Second*1).Should(BeTrue(), fmt.Sprintf("timed out when waiting for the app %s to be deleted in %s namespace", applicationName, testNamespace))
 			}
 			app, err := kubeadminClient.HasController.CreateApplication(applicationName, testNamespace)
 			Expect(err).NotTo(HaveOccurred())
@@ -108,36 +104,33 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 			i := i
 			gitUrl := gitUrl
 			It(fmt.Sprintf("triggers PipelineRun for component with source URL %s", gitUrl), Label(buildTemplatesTestLabel), func() {
-				timeout := time.Minute * 25
-				interval := time.Second * 1
+				timeout := time.Minute * 5
 
-				Eventually(func() bool {
+				Eventually(func() error {
 					pipelineRun, err := kubeadminClient.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
 					if err != nil {
-						GinkgoWriter.Println("PipelineRun has not been created yet")
-						return false
+						GinkgoWriter.Printf("PipelineRun has not been created yet for Component %s/%s\n", testNamespace, componentNames[i])
+						return err
 					}
-					return pipelineRun.HasStarted()
-				}, timeout, interval).Should(BeTrue(), "timed out when waiting for the %s PipelineRun to start", componentNames[i])
+					if !pipelineRun.HasStarted() {
+						return fmt.Errorf("pipelinerun %s/%s has not started yet", pipelineRun.GetNamespace(), pipelineRun.GetName())
+					}
+					return nil
+				}, timeout, constants.PipelineRunPollingInterval).Should(Succeed(), fmt.Sprintf("timed out when waiting for the PipelineRun to start for the Component %s/%s", testNamespace, componentNames[i]))
 			})
-		}
 
-		for i, gitUrl := range componentUrls {
-			i := i
-			gitUrl := gitUrl
-
-			It(fmt.Sprintf("should eventually finish successfully for component with source URL %s", gitUrl), Label(buildTemplatesTestLabel), func() {
+			It(fmt.Sprintf("should eventually finish successfully for component with Git source URL %s", gitUrl), Label(buildTemplatesTestLabel), func() {
 				component, err := kubeadminClient.HasController.GetComponent(componentNames[i], testNamespace)
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(kubeadminClient.HasController.WaitForComponentPipelineToBeFinished(component, "", 2)).To(Succeed())
 			})
 
-			It("should ensure SBOM is shown", Label(buildTemplatesTestLabel), func() {
-				pipelineRun, err := kubeadminClient.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
+			It(fmt.Sprintf("should ensure SBOM is shown for component with Git source URL %s", gitUrl), Label(buildTemplatesTestLabel), func() {
+				pr, err := kubeadminClient.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
 				Expect(err).ShouldNot(HaveOccurred())
-				Expect(pipelineRun).ToNot(BeNil(), "component pipelinerun not found")
+				Expect(pr).ToNot(BeNil(), fmt.Sprintf("PipelineRun for the component %s/%s not found", testNamespace, componentNames[i]))
 
-				logs, err := kubeadminClient.TektonController.GetTaskRunLogs(pipelineRun.Name, "show-sbom", testNamespace)
+				logs, err := kubeadminClient.TektonController.GetTaskRunLogs(pr.GetName(), "show-sbom", testNamespace)
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(logs).To(HaveLen(1))
 				var sbomTaskLog string
@@ -147,15 +140,15 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 
 				sbom := &build.SbomCyclonedx{}
 				err = json.Unmarshal([]byte(sbomTaskLog), sbom)
-				Expect(err).NotTo(HaveOccurred(), "failed to parse SBOM from show-sbom task output")
+				Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed to parse SBOM from show-sbom task output from %s/%s PipelineRun", pr.GetNamespace(), pr.GetName()))
 				Expect(sbom.BomFormat).ToNot(BeEmpty())
 				Expect(sbom.SpecVersion).ToNot(BeEmpty())
 				Expect(len(sbom.Components)).To(BeNumerically(">=", 1))
 			})
 
-			When("Pipeline Results are stored", Label("pipeline"), func() {
+			When(fmt.Sprintf("Pipeline Results are stored for component with Git source URL %s", gitUrl), Label("pipeline"), func() {
 				var resultClient *pipeline.ResultClient
-				var pipelineRun *v1beta1.PipelineRun
+				var pr *v1beta1.PipelineRun
 
 				BeforeAll(func() {
 					// create the proxyplugin for tekton-results
@@ -165,60 +158,58 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 					regProxyUrl := fmt.Sprintf("%s/plugins/tekton-results", f.ProxyUrl)
 					resultClient = pipeline.NewClient(regProxyUrl, f.UserToken)
 
-					err = wait.Poll(pipelineCreatedRetryInterval, pipelineCreatedTimeout, func() (done bool, err error) {
-						pipelineRun, err = f.AsKubeDeveloper.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
-						if err != nil {
-							time.Sleep(time.Millisecond * time.Duration(rand.IntnRange(10, 200)))
-							return false, fmt.Errorf("deletion of PipelineRun has been timedout: %v", err)
-						}
-						return true, nil
-					})
+					pr, err = f.AsKubeDeveloper.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
 					Expect(err).ShouldNot(HaveOccurred())
 				})
 
 				It("should have Pipeline Records", func() {
-					records, err := resultClient.GetRecords(testNamespace, string(pipelineRun.GetUID()))
+					records, err := resultClient.GetRecords(testNamespace, string(pr.GetUID()))
 					// temporary logs due to RHTAPBUGS-213
-					GinkgoWriter.Printf("records for PipelineRun %s:\n%s\n", pipelineRun.Name, records)
-					Expect(err).NotTo(HaveOccurred(), "got error getting records for PipelineRun %s: %v", pipelineRun.Name, err)
-					Expect(len(records.Record)).NotTo(BeZero(), "No records found for PipelineRun %s", pipelineRun.Name)
+					GinkgoWriter.Printf("records for PipelineRun %s:\n%s\n", pr.Name, records)
+					Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("got error getting records for PipelineRun %s: %v", pr.Name, err))
+					Expect(len(records.Record)).NotTo(BeZero(), fmt.Sprintf("No records found for PipelineRun %s", pr.Name))
 				})
 
 				It("should have Pipeline Logs", func() {
 					// Verify if result is stored in Database
 					// temporary logs due to RHTAPBUGS-213
-					logs, err := resultClient.GetLogs(testNamespace, string(pipelineRun.GetUID()))
-					GinkgoWriter.Printf("logs for PipelineRun %s:\n%s\n", pipelineRun.Name, logs)
-					Expect(err).NotTo(HaveOccurred(), "got error getting logs for PipelineRun %s: %v", pipelineRun.Name, err)
+					logs, err := resultClient.GetLogs(testNamespace, string(pr.GetUID()))
+					GinkgoWriter.Printf("logs for PipelineRun %s:\n%s\n", pr.GetName(), logs)
+					Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("got error getting logs for PipelineRun %s: %v", pr.Name, err))
 
 					timeout := time.Minute * 2
 					interval := time.Second * 10
 					// temporary timeout  due to RHTAPBUGS-213
-					Eventually(func() (bool, error) {
+					Eventually(func() error {
 						// temporary logs due to RHTAPBUGS-213
-						logs, err = resultClient.GetLogs(testNamespace, string(pipelineRun.GetUID()))
-						GinkgoWriter.Printf("logs for PipelineRun %s:\n%s\n", pipelineRun.Name, logs)
-						Expect(err).NotTo(HaveOccurred(), "got error getting logs for PipelineRun %s: %v", pipelineRun.Name, err)
+						logs, err = resultClient.GetLogs(testNamespace, string(pr.GetUID()))
+						if err != nil {
+							return fmt.Errorf("failed to get logs for PipelineRun %s: %v", pr.Name, err)
+						}
+						GinkgoWriter.Printf("logs for PipelineRun %s:\n%s\n", pr.Name, logs)
 
-						return len(logs.Record) != 0, err
-					}, timeout, interval).Should(BeTrue(), fmt.Sprintf("timed out when getting logs for PipelineRun %s", pipelineRun.Name))
+						if len(logs.Record) == 0 {
+							return fmt.Errorf("logs for PipelineRun %s/%s are empty", pr.GetNamespace(), pr.GetName())
+						}
+						return nil
+					}, timeout, interval).Should(Succeed(), fmt.Sprintf("timed out when getting logs for PipelineRun %s/%s", pr.GetNamespace(), pr.GetName()))
 
 					// Verify if result is stored in S3
 					// temporary logs due to RHTAPBUGS-213
 					log, err := resultClient.GetLogByName(logs.Record[0].Name)
 					GinkgoWriter.Printf("log for record %s:\n%s\n", logs.Record[0].Name, log)
-					Expect(err).NotTo(HaveOccurred(), "got error getting log '%s' for PipelineRun %s: %v", logs.Record[0].Name, pipelineRun.Name, err)
-					Expect(len(log)).NotTo(BeZero(), "no log content '%s' found for PipelineRun %s", logs.Record[0].Name, pipelineRun.Name)
+					Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("got error getting log '%s' for PipelineRun %s: %v", logs.Record[0].Name, pr.GetName(), err))
+					Expect(len(log)).NotTo(BeZero(), fmt.Sprintf("no log content '%s' found for PipelineRun %s", logs.Record[0].Name, pr.GetName()))
 				})
 			})
 
-			It("should validate tekton taskrun test results", Label(buildTemplatesTestLabel), func() {
-				pipelineRun, err := kubeadminClient.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
+			It(fmt.Sprintf("should validate tekton taskrun test results for component with Git source URL %s", gitUrl), Label(buildTemplatesTestLabel), func() {
+				pr, err := kubeadminClient.HasController.GetComponentPipelineRun(componentNames[i], applicationName, testNamespace, "")
 				Expect(err).ShouldNot(HaveOccurred())
-				Expect(build.ValidateBuildPipelineTestResults(pipelineRun, kubeadminClient.CommonController.KubeRest())).To(Succeed())
+				Expect(build.ValidateBuildPipelineTestResults(pr, kubeadminClient.CommonController.KubeRest())).To(Succeed())
 			})
 
-			When("the container image is created and pushed to container registry", Label("sbom", "slow"), func() {
+			When(fmt.Sprintf("the container image for component with Git source URL %s is created and pushed to container registry", gitUrl), Label("sbom", "slow"), func() {
 				var outputImage string
 				var kubeController tekton.KubeController
 				BeforeAll(func() {
@@ -238,7 +229,7 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 						Namespace:  testNamespace,
 					}
 				})
-				It("verify-enterprice-contract check should pass", Label(buildTemplatesTestLabel), func() {
+				It("verify-enterprise-contract check should pass", Label(buildTemplatesTestLabel), func() {
 					Skip("Skip until RHTAP bug is solved: https://issues.redhat.com/browse/RHTAPBUGS-352")
 					cm, err := kubeController.Commonctrl.GetConfigMap("ec-defaults", "enterprise-contract-service")
 					Expect(err).ToNot(HaveOccurred())
@@ -300,7 +291,6 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build", "
 					))
 				})
 				It("contains non-empty sbom files", Label(buildTemplatesTestLabel), func() {
-
 					purl, cyclonedx, err := build.GetParsedSbomFilesContentFromImage(outputImage)
 					Expect(err).NotTo(HaveOccurred())
 
