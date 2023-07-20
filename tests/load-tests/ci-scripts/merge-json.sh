b2859724011dbc43156a57b7ece4e80e524c429a
Meer Sawood <msawood@redhat.com>
2023-07-20 20:46:33 +0530

    github action to run test

---
+++ tests/load-tests/ci-scripts/merge-json.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to check if jq is installed and install it if not (for Ubuntu-based systems)
+check_and_install_jq() {
+    if ! command -v jq &>/dev/null; then
+        echo "jq not found. Installing jq..."
+        sudo apt-get update
+        sudo apt-get install -y jq
+    fi
+}
+
+# Function to merge JSON strings into one JSON array
+merge_json_strings() {
+    local merged_json_array=$(jq -s '.' <<< "$@")
+    echo "$merged_json_array"
+}
+
+# Check and install jq if needed
+check_and_install_jq
+
+# Get the output file path from the first argument
+output_file="${1}"
+shift
+
+# Merge JSON strings into one JSON array
+merged_json=$(merge_json_strings "$@")
+
+# Store the merged JSON array in the output file
+echo "$merged_json" > "$output_file"
