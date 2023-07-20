41e75c93b28497836207c4a79dbb8bdd40e0330a
Meer Sawood <msawood@redhat.com>
2023-07-20 21:03:05 +0530

    do an actual run

--- tests/load-tests/run-stage.sh
+++ tests/load-tests/run-stage.sh
@@ -1,9 +1,9 @@
 go run loadtest.go \
         --component-repo "${COMPONENT_REPO:-https://github.com/devfile-samples/devfile-sample-code-with-quarkus}" \
-        --users "${USERS_PER_THREAD:-3}" \
+        --users "${USERS_PER_THREAD:-1}" \
         -s \
         -w \
         -l \
-        -t "${THREADS:-2}" \
+        -t "${THREADS:-1}" \
         --disable-metrics \
         --pipeline-skip-initial-checks="${PIPELINE_SKIP_INITIAL_CHECKS:-true}"
