df8d15423f59d9314b6d1f0d26e8bb0bb1eaae62
Johnny Bieren <jbieren@redhat.com>
2023-07-20 15:28:14 -0400

    feat(HACBS-2290): bump release pipeline versions (#634)
    HACBS-2290 is about passing the snapshot object reference instead of as
    a json string due to character size limits. This commit updates the
    release pipelines to versions to accept the object reference instead of
    json strings.
    
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- tests/release/e2e-test-push-image-to-pyxis.go
+++ tests/release/e2e-test-push-image-to-pyxis.go
@@ -162,7 +162,7 @@ var _ = framework.ReleaseSuiteDescribe("[HACBS-1571]test-release-e2e-push-image-
 		_, err = fw.AsKubeAdmin.CommonController.Github.CreateFile("strategy-configs", scPath, string(scYaml), scGitRevision)
 		Expect(err).ShouldNot(HaveOccurred())
 
-		_, err = fw.AsKubeAdmin.ReleaseController.CreateReleaseStrategy("mvp-push-to-external-registry-strategy", managedNamespace, "push-to-external-registry", "quay.io/hacbs-release/pipeline-push-to-external-registry:0.11", releaseStrategyPolicyDefault, releaseStrategyServiceAccountDefault, []releaseApi.Params{
+		_, err = fw.AsKubeAdmin.ReleaseController.CreateReleaseStrategy("mvp-push-to-external-registry-strategy", managedNamespace, "push-to-external-registry", "quay.io/hacbs-release/pipeline-push-to-external-registry:0.12", releaseStrategyPolicyDefault, releaseStrategyServiceAccountDefault, []releaseApi.Params{
 			{Name: "extraConfigGitUrl", Value: fmt.Sprintf("https://github.com/%s/strategy-configs.git", utils.GetEnv(constants.GITHUB_E2E_ORGANIZATION_ENV, "redhat-appstudio-qe"))},
 			{Name: "extraConfigPath", Value: scPath},
 			{Name: "extraConfigGitRevision", Value: scGitRevision},
