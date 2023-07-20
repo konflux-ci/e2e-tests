78d8393e66f08f20edb20dfa96963b306a3d4145
Meer Sawood <msawood@redhat.com>
2023-07-20 17:35:12 +0530

    chore: Add Functions that enable connection to stage  (#635)
    * added stage functions
    
    * fix: add the client function
    
    * remove duplicate code
    
    * remove more duplicated code
    
    * fix panic while creating framework

--- pkg/sandbox/keycloak.go
+++ pkg/sandbox/keycloak.go
@@ -30,6 +30,49 @@ type KeycloakAuth struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
+//Make Request
+func (k *SandboxController) MakeRequestKeyCloak(req *http.Request, userName string) (keycloakAuth *KeycloakAuth, err error){
+
+	resp, err := k.HttpClient.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		var statusCode string
+		if resp == nil {
+			statusCode = "nil"
+		} else {
+			statusCode = fmt.Sprintf("%d", resp.StatusCode)
+		}
+		return nil, fmt.Errorf("failed to get keycloak token, userName: %s, statusCode: %s", userName, statusCode)
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&keycloakAuth)
+
+	return keycloakAuth, err
+}
+
+//Get Stage KeyCloak Token
+func (k *SandboxController) GetKeycloakTokenStage(userName, tokenURL, refreshToken string) (keycloakAuth *KeycloakAuth, err error) {
+
+
+	// Prepare the form data
+	formData := url.Values{}
+	formData.Set("grant_type", "refresh_token")
+	formData.Set("client_id", "cloud-services")
+	formData.Set("refresh_token", refreshToken)
+
+	req, err := http.NewRequest("POST", tokenURL, strings.NewReader(formData.Encode()))
+	if err != nil {
+		fmt.Println("Failed to create Access Token request:", err)
+		return nil, err
+	}
+
+	// Set the headers
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	return k.MakeRequestKeyCloak(req, userName)
+}
+
 // GetKeycloakToken return a token for admins
 func (k *SandboxController) GetKeycloakToken(clientID string, userName string, password string, realm string) (keycloakAuth *KeycloakAuth, err error) {
 	data := url.Values{
@@ -47,22 +90,7 @@ func (k *SandboxController) GetKeycloakToken(clientID string, userName string, p
 	}
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	response, err := k.HttpClient.Do(request)
-
-	if err != nil || response.StatusCode != 200 {
-		var statusCode string
-		if response == nil {
-			statusCode = "nil"
-		} else {
-			statusCode = fmt.Sprintf("%d", response.StatusCode)
-		}
-		return nil, fmt.Errorf("failed to get keycloak token, realm: %s, userName: %s, client-id: %s statusCode: %s, url: %s", realm, userName, clientID, statusCode, k.KeycloakUrl)
-	}
-	defer response.Body.Close()
-
-	err = json.NewDecoder(response.Body).Decode(&keycloakAuth)
-
-	return keycloakAuth, err
+	return k.MakeRequestKeyCloak(request, userName)
 }
 
 /*
