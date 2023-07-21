e0b080fb13266ba536c3a49545a2506f89d61152
jsmid1 <jsmid@redhat.com>
2023-07-21 09:59:34 +0200

    [RHTAP-1078] File separation

---
+++ pkg/utils/integration/snapshots.go
@@ -0,0 +1,136 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/devfile/library/v2/pkg/util"
+	appstudioApi "github.com/redhat-appstudio/application-api/api/v1alpha1"
+	"github.com/redhat-appstudio/e2e-tests/pkg/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// CreateSnapshotWithImage creates a snapshot using an image.
+func (i *IntegrationController) CreateSnapshotWithImage(applicationName, namespace, componentName, containerImage string) (*appstudioApi.Snapshot, error) {
+	hasSnapshot := &appstudioApi.Snapshot{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "snapshot-sample-" + util.GenerateRandomString(4),
+			Namespace: namespace,
+			Labels: map[string]string{
+				"test.appstudio.openshift.io/type":           "component",
+				"appstudio.openshift.io/component":           componentName,
+				"pac.test.appstudio.openshift.io/event-type": "push",
+			},
+		},
+		Spec: appstudioApi.SnapshotSpec{
+			Application: applicationName,
+			Components: []appstudioApi.SnapshotComponent{
+				{
+					Name:           componentName,
+					ContainerImage: containerImage,
+				},
+			},
+		},
+	}
+	err := i.KubeRest().Create(context.TODO(), hasSnapshot)
+	if err != nil {
+		return nil, err
+	}
+	return hasSnapshot, err
+}
+
+// CreateSnapshotWithCompoents creates a Snapshot using the given parameters.
+func (i *IntegrationController) CreateSnapshotWithComponents(name string, namespace string, applicationName string, snapshotComponents []appstudioApi.SnapshotComponent) (*appstudioApi.Snapshot, error) {
+	snapshot := &appstudioApi.Snapshot{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: appstudioApi.SnapshotSpec{
+			Application: applicationName,
+			Components:  snapshotComponents,
+		},
+	}
+	return snapshot, i.KubeRest().Create(context.TODO(), snapshot)
+}
+
+// GetSnapshotByComponent returns the first snapshot in namespace if exist, else will return nil
+func (i *IntegrationController) GetSnapshotByComponent(namespace string) (*appstudioApi.Snapshot, error) {
+	snapshot := &appstudioApi.SnapshotList{}
+	opts := []client.ListOption{
+		client.MatchingLabels{
+			"test.appstudio.openshift.io/type": "component",
+		},
+		client.InNamespace(namespace),
+	}
+	err := i.KubeRest().List(context.TODO(), snapshot, opts...)
+
+	if err == nil && len(snapshot.Items) > 0 {
+		return &snapshot.Items[0], nil
+	}
+	return nil, err
+}
+
+// GetSnapshot returns the Snapshot in the namespace and nil if it's not found
+// It will search for the Snapshot based on the Snapshot name, associated PipelineRun name or Component name
+// In the case the List operation fails, an error will be returned.
+func (i *IntegrationController) GetSnapshot(snapshotName, pipelineRunName, componentName, namespace string) (*appstudioApi.Snapshot, error) {
+	ctx := context.Background()
+	// If Snapshot name is provided, try to get the resource directly
+	if len(snapshotName) > 0 {
+		snapshot := &appstudioApi.Snapshot{}
+		if err := i.KubeRest().Get(ctx, types.NamespacedName{Name: snapshotName, Namespace: namespace}, snapshot); err != nil {
+			return nil, fmt.Errorf("couldn't find Snapshot with name '%s' in '%s' namespace", snapshotName, namespace)
+		}
+		return snapshot, nil
+	}
+	// Search for the Snapshot in the namespace based on the associated Component or PipelineRun
+	snapshots := &appstudioApi.SnapshotList{}
+	opts := []client.ListOption{
+		client.InNamespace(namespace),
+	}
+	err := i.KubeRest().List(ctx, snapshots, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("error when listing Snapshots in '%s' namespace", namespace)
+	}
+	for _, snapshot := range snapshots.Items {
+		if snapshot.Name == snapshotName {
+			return &snapshot, nil
+		}
+		// find snapshot by pipelinerun name
+		if len(pipelineRunName) > 0 && snapshot.Labels["appstudio.openshift.io/build-pipelinerun"] == pipelineRunName {
+			return &snapshot, nil
+
+		}
+		// find snapshot by component name
+		if len(componentName) > 0 && snapshot.Labels["appstudio.openshift.io/component"] == componentName {
+			return &snapshot, nil
+
+		}
+	}
+	return nil, fmt.Errorf("no snapshot found for component '%s', pipelineRun '%s' in '%s' namespace", componentName, pipelineRunName, namespace)
+}
+
+// DeleteSnapshot removes given snapshot from specified namespace.
+func (i *IntegrationController) DeleteSnapshot(hasSnapshot *appstudioApi.Snapshot, namespace string) error {
+	err := i.KubeRest().Delete(context.TODO(), hasSnapshot)
+	return err
+}
+
+// DeleteAllSnapshotsInASpecificNamespace removes all snapshots from a specific namespace. Useful when creating a lot of resources and want to remove all of them
+func (i *IntegrationController) DeleteAllSnapshotsInASpecificNamespace(namespace string, timeout time.Duration) error {
+	if err := i.KubeRest().DeleteAllOf(context.TODO(), &appstudioApi.Snapshot{}, client.InNamespace(namespace)); err != nil {
+		return fmt.Errorf("error deleting snapshots from the namespace %s: %+v", namespace, err)
+	}
+
+	snapshotList := &appstudioApi.SnapshotList{}
+	return utils.WaitUntil(func() (done bool, err error) {
+		if err := i.KubeRest().List(context.Background(), snapshotList, &client.ListOptions{Namespace: namespace}); err != nil {
+			return false, nil
+		}
+		return len(snapshotList.Items) == 0, nil
+	}, timeout)
+}
---
+++ pkg/utils/integration/space_requests.go
@@ -0,0 +1,26 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+
+	codereadytoolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// GetSpaceRequests returns a list of spaceRequests in given namespace.
+func (i *IntegrationController) GetSpaceRequests(namespace string) (*codereadytoolchainv1alpha1.SpaceRequestList, error) {
+	spaceRequestList := &codereadytoolchainv1alpha1.SpaceRequestList{}
+
+	opts := []client.ListOption{
+		client.InNamespace(namespace),
+	}
+
+	err := i.KubeRest().List(context.Background(), spaceRequestList, opts...)
+	if err != nil && !k8sErrors.IsNotFound(err) {
+		return nil, fmt.Errorf("error occurred while trying to list spaceRequests in %s namespace: %v", namespace, err)
+	}
+
+	return spaceRequestList, nil
+}
