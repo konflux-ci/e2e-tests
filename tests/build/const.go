55d2404115f1992c8582902251631b854bf49394
Jan Smid <124383262+jsmid1@users.noreply.github.com>
2023-07-27 14:48:39 +0200

    chore: Remove reference to `main` in GH and tags in `quay.io` for fixed tags (#615)
    * [RHTAP-1030] Exchanged 'main' and 'latest' for fixed references, adjusted CreateRef
    
    * [RHTAP-1030] Fixed blank sha scenario
    
    * [RHTAP-1030] Removed unused constant
    
    * [RHTAP-1030] CreateRef change + tag unpin
    
    * [RHTAP-1030] Reverted changes in pipelines
    
    ---------
    
    Co-authored-by: Flavius Lacatusu <flacatus@redhat.com>

--- tests/build/const.go
+++ tests/build/const.go
@@ -11,14 +11,15 @@ import (
 const (
 	COMPONENT_REPO_URLS_ENV string = "COMPONENT_REPO_URLS"
 
-	containerImageSource        = "quay.io/redhat-appstudio-qe/busybox-loop:latest"
+	containerImageSource        = "quay.io/redhat-appstudio-qe/busybox-loop@sha256:f698f1f2cf641fe9176d2a277c9052d872f6b1c39e56248a1dd259b96281dda9"
 	pythonComponentGitSourceURL = "https://github.com/redhat-appstudio-qe/devfile-sample-python-basic.git"
-	dummyPipelineBundleRef      = "quay.io/redhat-appstudio-qe/dummy-pipeline-bundle:latest"
+	dummyPipelineBundleRef      = "quay.io/redhat-appstudio-qe/dummy-pipeline-bundle@sha256:9805fc3f309af8f838622e49d3e7705d8364eb5c8287043d5725f3ef12232f24"
 	buildTemplatesTestLabel     = "build-templates-e2e"
 	buildTemplatesKcpTestLabel  = "build-templates-kcp-e2e"
 
 	helloWorldComponentGitSourceRepoName = "devfile-sample-hello-world"
 	helloWorldComponentDefaultBranch     = "default"
+	helloWorldComponentRevision          = "e17b15c4ea1fd4a2553e17ab24561bbe40d0e2f4"
 )
 
 var (
