7a341acec08a420067d4223a9dd4346b05142bf3
Pavel Macík <pavel.macik@gmail.com>
2023-09-21 09:18:05 +0200

    fix(load-test): replace scenarioContent string with a key=value set in the output JSON (#777)
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- tests/load-tests/run-max-concurrency.sh
+++ tests/load-tests/run-max-concurrency.sh
@@ -39,7 +39,7 @@ if [ ${#USER_PREFIX} -gt 10 ]; then
     exit 1
 else
     output="$output_dir/load-tests.max-concurrency.json"
-    IFS=" " read -r -a maxConcurrencySteps <<<"${MAX_CONCURRENCY_STEPS:-1 5 10 25 50 100 150 200}"
+    IFS="," read -r -a maxConcurrencySteps <<<"$(echo "${MAX_CONCURRENCY_STEPS:-1\ 5\ 10\ 25\ 50\ 100\ 150\ 200}" | sed 's/ /,/g')"
     maxThreads=${MAX_THREADS:-10}
     threshold=${THRESHOLD:-300}
     echo '{"startTimestamp":"'$(date +%FT%T%:z)'", "maxThreads": '"$maxThreads"', "maxConcurrencySteps": "'"${maxConcurrencySteps[*]}"'", "threshold": '"$threshold"', "maxConcurrencyReached": 0, "endTimestamp": ""}' | jq >"$output"
