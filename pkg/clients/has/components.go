e120c6cbe6e9bc7dba90ed66034ed91792bdd634
Flavius Lacatusu <flacatus@redhat.com>
2023-11-29 13:35:28 +0100

    fix: RHTAPBUGS-985: retry pipelineruns always when error occurs (#917)
    retry pipelineruns always

--- pkg/clients/has/components.go
+++ pkg/clients/has/components.go
@@ -148,9 +148,7 @@ func (h *HasController) WaitForComponentPipelineToBeFinished(component *appservi
 
 		if err != nil {
 			GinkgoWriter.Printf("attempt %d/%d: PipelineRun %q failed: %+v", attempts, maxRetries+1, pr.GetName(), err)
-			//CouldntGetTask: Retry the PipelineRun only in case we hit the known issue https://issues.redhat.com/browse/SRVKP-2749
-			// TaskRunImagePullFailed: Retry in case of https://issues.redhat.com/browse/RHTAPBUGS-985 and https://github.com/tektoncd/pipeline/issues/7184
-			if attempts == maxRetries+1 || (pr.GetStatusCondition().GetCondition(apis.ConditionSucceeded).GetReason() != "CouldntGetTask" && pr.GetStatusCondition().GetCondition(apis.ConditionSucceeded).GetReason() != "TaskRunImagePullFailed") {
+			if attempts == maxRetries+1 {
 				return err
 			}
 			if sha, err = h.RetriggerComponentPipelineRun(component, pr); err != nil {
