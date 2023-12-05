0f891f05145993ac6db1cc7c035214acac4995cc
Rafaela Soares <119665479+rsoaresd@users.noreply.github.com>
2023-12-05 09:23:07 +0000

    test: RHTAPBUGS-978 increase timeout of AwaitAttestationAndSignature (#931)
    * increase timeout
    
    * revert
    
    * increase timeout of pipelineRunTimeout
    
    * revert

--- tests/build/chains.go
+++ tests/build/chains.go
@@ -108,7 +108,7 @@ var _ = framework.ChainsSuiteDescribe("Tekton Chains E2E tests", Label("ec", "HA
 			Expect(err).ToNot(HaveOccurred())
 
 			pipelineRunTimeout = int(time.Duration(20) * time.Minute)
-			attestationTimeout = time.Duration(60) * time.Second
+			attestationTimeout = time.Duration(5) * time.Minute
 
 			defaultECP, err = fwk.AsKubeAdmin.TektonController.GetEnterpriseContractPolicy("default", "enterprise-contract-service")
 			Expect(err).NotTo(HaveOccurred())
