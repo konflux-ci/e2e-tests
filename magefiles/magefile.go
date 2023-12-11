b095e665b79af54199429512dfb93416ac722256
Flavius Lacatusu <flacatus@redhat.com>
2023-12-11 10:25:19 +0100

    fix: RHTAPBUGS-991: Do not run flaky tests in periodic jobs (#949)
    fix:RHTAPBUGS-991: Do not run flaky tests in periodic jobs

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -477,7 +477,7 @@ func (ci CI) setRequiredEnvVars() error {
 
 			os.Setenv("E2E_TEST_SUITE_LABEL", testSuiteLabel)
 
-		} else if openshiftJobSpec.Refs.Repo == "infra-deployments" {
+		} else if openshiftJobSpec.Refs.Repo == "infra-deployments" || jobType == "periodic" {
 			requiresSprayProxyRegistering = true
 			os.Setenv("INFRA_DEPLOYMENTS_ORG", pr.RemoteName)
 			os.Setenv("INFRA_DEPLOYMENTS_BRANCH", pr.BranchName)
