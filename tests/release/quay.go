e5c98cdaab7416fc88c0266353ae59968101d43b
Jing Qi <jinqi@redhat.com>
2024-02-26 02:57:41 -0300

    fix: image digest check may meet race condition (#1051)
    Signed-off-by: Jing Qi <jinqi@redhat.com>

--- tests/release/quay.go
+++ tests/release/quay.go
@@ -11,30 +11,29 @@ import (
 
 var (
 	quayApiUrl = "https://quay.io/api/v1"
-	quayOrg    = utils.GetEnv("IMAGE_CONTROLLER_QUAY_ORG", "hacbs-release-tests")
+	// quayOrg    = utils.GetEnv("IMAGE_CONTROLLER_QUAY_ORG", "hacbs-release-tests")
 	quayToken  = utils.GetEnv("IMAGE_CONTROLLER_QUAY_ORG_TOKEN", "")
 	quayClient = quay.NewQuayClient(&http.Client{Transport: &http.Transport{}}, quayToken, quayApiUrl)
 )
 
-// imageURL format example: quay.io/redhat-appstudio-qe/devfile-go-rhtap-uvv7:latest
-func GetDigestWithTagInQuay(imageURL string) (string, error) {
-	urlParts := strings.Split(imageURL, ":")
-	if len(urlParts) != 2 {
-		return "", fmt.Errorf("image URL %s has incorrect format", imageURL)
-	}
-	repoParts := strings.Split(urlParts[0], "/")
+// repoURL format example: quay.io/redhat-appstudio-qe/dcmetromap
+func DoesDigestExistInQuay(repoURL string, digest string) (bool, error) {
+	repoParts := strings.Split(repoURL, "/")
 	if len(repoParts) <= 2 {
-		return "", fmt.Errorf("image URL %s is not complete", imageURL)
+		return false, fmt.Errorf("repo URL %s is not complete", repoURL)
 	}
+
 	repoName := strings.Join(repoParts[2:], "/")
-	tagList, _, err := quayClient.GetTagsFromPage(quayOrg, repoName, 0)
+	tagList, _, err := quayClient.GetTagsFromPage(repoParts[1], repoName, 0)
 	if err != nil {
-		return "", err
+		return false, err
 	}
+
 	for _, tag := range tagList {
-		if tag.Name == urlParts[1] {
-			return tag.ManifestDigest, nil
+		if tag.ManifestDigest == digest {
+			return true, nil
 		}
 	}
-        return "", fmt.Errorf("no image is found")
+
+	return false, fmt.Errorf("no image is found")
 }
