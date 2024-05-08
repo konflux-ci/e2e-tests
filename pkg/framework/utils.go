c06a467c83e8d299946af3dff0b59bbefbc00d15
Michael Valdron <mvaldron@redhat.com>
2024-05-08 16:06:52 -0400

    remove environment api
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- pkg/framework/utils.go
+++ pkg/framework/utils.go
@@ -10,11 +10,9 @@ func (c *ControllerHub) StoreAllArtifactsForNamespace(namespace string) error {
 	finalError = appendErrorToString(finalError, c.IntegrationController.StoreAllSnapshots(namespace))
 	finalError = appendErrorToString(finalError, c.TektonController.StoreAllPipelineRuns(namespace))
 	finalError = appendErrorToString(finalError, c.CommonController.StoreAllPods(namespace))
-	finalError = appendErrorToString(finalError, c.CommonController.StoreAllSnapshotEnvironmentBindings(namespace))
 	finalError = appendErrorToString(finalError, c.GitOpsController.StoreAllDeploymentTargetClaims(namespace))
 	finalError = appendErrorToString(finalError, c.GitOpsController.StoreAllDeploymentTargetClasses(namespace))
 	finalError = appendErrorToString(finalError, c.GitOpsController.StoreAllDeploymentTargets(namespace))
-	finalError = appendErrorToString(finalError, c.GitOpsController.StoreAllEnvironments(namespace))
 	if len(finalError) > 0 {
 		return fmt.Errorf(finalError)
 	}
