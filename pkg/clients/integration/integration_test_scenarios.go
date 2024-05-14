7c1a9409b4307b04ec37cd3bbb4e8d3f12f7bc79
Michael Valdron <michael.valdron@gmail.com>
2024-05-14 11:35:39 -0400

    chore(DEVHAS-662): Remove Environment API from e2e-tests (#1158)
    remove environment api
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- pkg/clients/integration/integration_test_scenarios.go
+++ pkg/clients/integration/integration_test_scenarios.go
@@ -2,65 +2,13 @@ package integration
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/devfile/library/v2/pkg/util"
-	appservice "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	"github.com/redhat-appstudio/e2e-tests/pkg/constants"
 	integrationv1beta1 "github.com/konflux-ci/integration-service/api/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-// CreateIntegrationTestScenarioWithEnvironment will create an IntegrationTestScenario with a
-// user-supplied environment embedded in its Spec.Environment
-func (i *IntegrationController) CreateIntegrationTestScenarioWithEnvironment(applicationName, namespace, gitURL, revision, pathInRepo string, environment *appservice.Environment) (*integrationv1beta1.IntegrationTestScenario, error) {
-	integrationTestScenario := &integrationv1beta1.IntegrationTestScenario{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "example-pass-with-env-" + util.GenerateRandomString(4),
-			Namespace: namespace,
-			Labels:    constants.IntegrationTestScenarioDefaultLabels,
-		},
-		Spec: integrationv1beta1.IntegrationTestScenarioSpec{
-			Application: applicationName,
-			ResolverRef: integrationv1beta1.ResolverRef{
-				Resolver: "git",
-				Params: []integrationv1beta1.ResolverParameter{
-					{
-						Name:  "url",
-						Value: gitURL,
-					},
-					{
-						Name:  "revision",
-						Value: revision,
-					},
-					{
-						Name:  "pathInRepo",
-						Value: pathInRepo,
-					},
-				},
-			},
-			Environment: integrationv1beta1.TestEnvironment{
-				Name: environment.Name,
-				Type: environment.Spec.Type,
-				Configuration: &appservice.EnvironmentConfiguration{
-					Env: []appservice.EnvVarPair{
-						{
-							Name:  environment.Spec.Configuration.Env[0].Name,
-							Value: environment.Spec.Configuration.Env[0].Value,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	err := i.KubeRest().Create(context.Background(), integrationTestScenario)
-	if err != nil {
-		return nil, fmt.Errorf("error occurred when creating the IntegrationTestScenario: %+v", err)
-	}
-	return integrationTestScenario, nil
-}
 
 // CreateIntegrationTestScenario creates beta1 version integrationTestScenario.
 func (i *IntegrationController) CreateIntegrationTestScenario(itsName, applicationName, namespace, gitURL, revision, pathInRepo string) (*integrationv1beta1.IntegrationTestScenario, error) {
