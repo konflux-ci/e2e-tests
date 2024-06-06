4f9bfd0c4300291818325d134bbc8728c5d0a3be
Jan Hutar <jhutar@redhat.com>
2024-06-06 08:52:07 +0200

    feat: Fixing Stage GH action

--- tests/load-tests/ci-scripts/merge-json.sh
+++ tests/load-tests/ci-scripts/merge-json.sh
@@ -1,53 +1,26 @@
 #!/bin/bash
 
-# Function to check if jq is installed and install it if not (for Ubuntu-based systems)
-check_and_install_jq() {
-    if ! command -v jq &>/dev/null; then
-        echo "jq not found. Please Install it"
-        exit 1
-    fi
-}
+set -o nounset
+set -o errexit
+set -o pipefail
 
-# make a dummy kube config
-mkdir -p ~/.kube
-cat <<EOF > ~/.kube/config
-apiVersion: v1
-clusters:
-- cluster:
-    server: YOUR_KUBE_API_SERVER_URL
-  name: YOUR_CLUSTER_NAME
-contexts:
-- context:
-    cluster: YOUR_CLUSTER_NAME
-    user: YOUR_USER_NAME
-  name: YOUR_CONTEXT_NAME
-current-context: YOUR_CONTEXT_NAME
-kind: Config
-preferences: {}
-users:
-- name: YOUR_USER_NAME
-  user:
-    token: YOUR_KUBE_API_TOKEN
-EOF
+# Check if jq is installed
+if ! command -v jq &>/dev/null; then
+    echo "jq not found. Please Install it"
+    exit 1
+fi
 
-
-
-# Function to decode and save JSON data from variable to pre_N.json
-decode_and_save_json() {
-    local file_name="pre_${1}.json"
-    cat "$f" | base64 --decode > "$file_name"
-}
-
-# Loop through all the variables
-for f in $@; do
-    decode_and_save_json "$f"
+# Loop through all the variables and save them to files
+n=1
+for var in $@; do
+    echo "$var" | base64 --decode >"pre_${n}.json"
+    let n++
 done
 
 # Merge all the pre_N.json files into users.json
 jq -s '.[]' pre_*.json | jq -s 'add' > users.json
 
-# Optionally, you can remove the temporary pre_N.json files
-# Uncomment the next line to delete the files
+# Remove the temporary pre_N.json files
 rm pre_*.json
 
 echo "Decoded JSON data merged and stored in users.json."
--- tests/load-tests/run-stage.sh
+++ tests/load-tests/run-stage.sh
@@ -1,18 +1,23 @@
 go run loadtest.go \
+    --applications-count "${APPLICATIONS_COUNT:-1}" \
+    --build-pipeline-selector-bundle "${BUILD_PIPELINE_SELECTOR_BUNDLE:-}" \
     --component-repo "${COMPONENT_REPO:-https://github.com/devfile-samples/devfile-sample-code-with-quarkus}" \
-    --username "$USER_PREFIX" \
-    --users "${USERS_PER_THREAD:-2}" \
+    --component-repo-revision "${COMPONENT_REPO_REVISION:-main}" \
+    --components-count "${COMPONENTS_COUNT:-1}" \
+    --concurrency "${CONCURRENCY:-1}" \
+    --journey-duration "${JOURNEY_DURATION:-1h}" \
+    --journey-repeats "${JOURNEY_REPEATS:-1}" \
+    --log-trace \
+    --multiarch-workflow="${MULTIARCH_WORKFLOW:-false}" \
+    --output-dir "${OUTPUT_DIR:-.}" \
+    --pipeline-request-configure-pac="${PIPELINE_REQUEST_CONFIGURE_PAC:-false}" \
+    --pipeline-skip-initial-checks="${PIPELINE_SKIP_INITIAL_CHECKS:-true}" \
+    --purge="${PURGE:-true}" \
+    --quay-repo "${QUAY_REPO:-redhat-user-workloads-stage}" \
     --test-scenario-git-url "${TEST_SCENARIO_GIT_URL:-https://github.com/konflux-ci/integration-examples.git}" \
-    --test-scenario-revision "${TEST_SCENARIO_REVISION:-main}" \
     --test-scenario-path-in-repo "${TEST_SCENARIO_PATH_IN_REPO:-pipelines/integration_resolver_pipeline_pass.yaml}" \
-    -s \
-    -w="${WAIT_PIPELINES:-true}" \
-    -i="${WAIT_INTEGRATION_TESTS:-true}" \
-    -d="${WAIT_DEPLOYMENTS:-true}" \
-    -l \
-    -o "${OUTPUT_DIR:-.}" \
-    -t "${THREADS:-2}" \
-    --disable-metrics="${DISABLE_METRICS:-false}" \
-    --pushgateway-url "${PUSHGATEWAY_URL:-rhtapqe.com}" \
-    --enable-progress-bars="${ENABLE_PROGRESS_BARS:-false}" \
-    --pipeline-skip-initial-checks="${PIPELINE_SKIP_INITIAL_CHECKS:-true}"
+    --test-scenario-revision "${TEST_SCENARIO_REVISION:-main}" \
+    --username "$USER_PREFIX" \
+    --waitintegrationtestspipelines="${WAIT_INTEGRATION_TESTS:-true}" \
+    --waitpipelines="${WAIT_PIPELINES:-true}" \
+    --stage
