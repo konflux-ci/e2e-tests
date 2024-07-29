5f195e64a21de4fd201a7a250a0707ac2453d073
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2024-07-29 13:00:49 +0530

    refactor(KONFLUX-3654): move crowd of constants to new file
    Signed-off-by: Dheeraj<djodha@redhat.com>

--- tests/konflux-demo/konflux-demo.go
+++ tests/konflux-demo/konflux-demo.go
@@ -40,35 +40,6 @@ import (
 	e2eConfig "github.com/konflux-ci/e2e-tests/tests/konflux-demo/config"
 )
 
-const (
-	// Timeouts
-	appDeployTimeout            = time.Minute * 20
-	appRouteAvailableTimeout    = time.Minute * 5
-	customResourceUpdateTimeout = time.Minute * 10
-	jvmRebuildTimeout           = time.Minute * 40
-	mergePRTimeout              = time.Minute * 1
-	pipelineRunStartedTimeout   = time.Minute * 5
-	pullRequestCreationTimeout  = time.Minute * 5
-	releasePipelineTimeout      = time.Minute * 15
-	snapshotTimeout             = time.Minute * 4
-	releaseTimeout              = time.Minute * 4
-	testPipelineTimeout         = time.Minute * 15
-	branchCreateTimeout         = time.Minute * 1
-
-	// Intervals
-	defaultPollingInterval    = time.Second * 2
-	jvmRebuildPollingInterval = time.Second * 10
-	snapshotPollingInterval   = time.Second * 1
-	releasePollingInterval    = time.Second * 1
-
-	// test metadata
-	devEnvTestLabel = "konflux"
-
-	// stage env test related env vars
-	stageTimeout      = time.Minute * 5
-	stageEnvTestLabel = "verify-stage"
-)
-
 var _ = framework.KonfluxDemoSuiteDescribe(func() {
 	defer GinkgoRecover()
 
