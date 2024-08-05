0af25afa25587c0fa3608b83dc87a68446f26606
Sushanta Das <sushanta.das.ju@gmail.com>
2024-08-05 15:35:20 +0530

    update renovate test

--- tests/build/build.go
+++ tests/build/build.go
@@ -1410,10 +1410,8 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build-ser
 					//make the parent repo nudge the child repo
 					if comp.repoName == componentDependenciesParentRepoName {
 						componentObj.BuildNudgesRef = []string{ChildComponentDef.componentName}
-						comp.component, err = f.AsKubeAdmin.HasController.CreateComponent(componentObj, testNamespace, "", "", applicationName, true, utils.MergeMaps(utils.MergeMaps(constants.ComponentPaCRequestAnnotation, constants.ImageControllerAnnotationRequestPublicRepo), constants.DefaultDockerBuildPipelineBundle))
-					} else {
-						comp.component, err = f.AsKubeAdmin.HasController.CreateComponent(componentObj, testNamespace, "", "", applicationName, true, utils.MergeMaps(constants.ImageControllerAnnotationRequestPublicRepo, constants.DefaultDockerBuildPipelineBundle))
 					}
+					comp.component, err = f.AsKubeAdmin.HasController.CreateComponent(componentObj, testNamespace, "", "", applicationName, true, utils.MergeMaps(utils.MergeMaps(constants.ComponentPaCRequestAnnotation, constants.ImageControllerAnnotationRequestPublicRepo), constants.DefaultDockerBuildPipelineBundle))
 					Expect(err).ShouldNot(HaveOccurred())
 				}
 			})
@@ -1443,6 +1441,38 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build-ser
 					}
 				}
 				Expect(parentFirstDigest).ShouldNot(BeEmpty())
+			})
+
+			It(fmt.Sprintf("the PipelineRun should eventually finish successfully for child component %s", ChildComponentDef.componentName), func() {
+				Expect(f.AsKubeAdmin.HasController.WaitForComponentPipelineToBeFinished(ChildComponentDef.component, "", f.AsKubeAdmin.TektonController, &has.RetryOptions{Always: true, Retries: 2}, nil)).To(Succeed())
+			})
+
+			It(fmt.Sprintf("should lead to a PaC PR creation for child component %s", ChildComponentDef.componentName), func() {
+				timeout = time.Second * 300
+				interval := time.Second * 1
+
+				Eventually(func() bool {
+					prs, err := f.AsKubeAdmin.CommonController.Github.ListPullRequests(ChildComponentDef.repoName)
+					Expect(err).ShouldNot(HaveOccurred())
+
+					for _, pr := range prs {
+						if pr.Head.GetRef() == ChildComponentDef.pacBranchName {
+							prNumber = pr.GetNumber()
+							return true
+						}
+					}
+					return false
+				}, timeout, interval).Should(BeTrue(), fmt.Sprintf("timed out when waiting for PaC PR (branch name '%s') to be created in %s repository", ChildComponentDef.pacBranchName, ChildComponentDef.repoName))
+			})
+
+			It(fmt.Sprintf("Merging the PaC PR should be successful for child component %s", ChildComponentDef.componentName), func() {
+				Eventually(func() error {
+					mergeResult, err = f.AsKubeAdmin.CommonController.Github.MergePullRequest(ChildComponentDef.repoName, prNumber)
+					return err
+				}, time.Minute).Should(BeNil(), fmt.Sprintf("error when merging PaC pull request #%d in repo %s", prNumber, ChildComponentDef.repoName))
+
+				mergeResultSha = mergeResult.GetSHA()
+				GinkgoWriter.Printf("merged result sha: %s for PR #%d\n", mergeResultSha, prNumber)
 			})
 			// Now we have an initial image we create a dockerfile in the child that references this new image
 			// This is the file that will be updated by the nudge
@@ -1503,7 +1533,6 @@ var _ = framework.BuildSuiteDescribe("Build service E2E tests", Label("build-ser
 
 				mergeResultSha = mergeResult.GetSHA()
 				GinkgoWriter.Printf("merged result sha: %s for PR #%d\n", mergeResultSha, prNumber)
-
 			})
 			// Now the PR is merged this will kick off another build. The result of this build is what we want to update in dockerfile we created
 			It(fmt.Sprintf("PR merge triggers PAC PipelineRun for parent component %s", ParentComponentDef.componentName), func() {
