2b289f376f5ad4ac74baa3d665a52d8b131700fe
Flavius Lacatusu <flacatus@redhat.com>
2024-09-04 10:51:08 +0200

    chore: Add new task to create OCI artifacts and improve e2e runner script (#1363)
    * chore: leverage all oras container stuffs in a new task
    
    * chore: improve oci artifacts
    
    * chore: improve install script
    
    * chore: fix cluster operator
    
    * chore: fix oras push
    
    ---------
    
    Co-authored-by: redhat-appstudio-qe-bot <redhat-appstudio-qe-bot@redhat.com>
    Co-authored-by: Flavius Lacatusu <flacatus@fedora.redhat.com>

--- integration-tests/scripts/konflux-e2e-runner.sh
+++ integration-tests/scripts/konflux-e2e-runner.sh
@@ -1,92 +1,124 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
+set -euo pipefail
 
-function postActions() {
-    local EXIT_CODE=$?
-
-    # Unregister PaC Server from SprayProxy
-    if [ "$UNREGISTER_PAC" == "true" ]; then
-        make ci/sprayproxy/unregister || true
-    fi
-
-    # Save artifacts
-    cd /workspace || exit 1
-    oras login -u "$ORAS_USERNAME" -p "$ORAS_PASSWORD" quay.io
-    echo '{"doc": "README.md"}' > config.json
-
-    oras push "$ORAS_CONTAINER" --config config.json:application/vnd.acme.rocket.config.v1+json \
-        ./test-artifacts/:application/vnd.acme.rocket.docs.layer.v1+tar
-
-    [[ "${EXIT_CODE}" != "0" ]] && echo "[ERROR] Job failed." || echo "[INFO] Job completed successfully."
-
-    exit "$EXIT_CODE"
+log() {
+    echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] [$1] $2"
 }
 
-trap postActions EXIT
+load_envs() {
+    local konflux_ci_secrets_file="/usr/local/konflux-ci-secrets"
+    local konflux_infra_secrets_file="/usr/local/konflux-test-infra"
 
-# Save the current kubeconfig and unset the default
+    declare -A config_envs=(
+        [ENABLE_SCHEDULING_ON_MASTER_NODES]="false"
+        [UNREGISTER_PAC]="true"
+        [EC_DISABLE_DOWNLOAD_SERVICE]="true"
+        [ARTIFACT_DIR]="$(mktemp -d)"
+        [GITHUB_USER]=""
+        [GITHUB_TOKEN]=""
+        [DEFAULT_QUAY_ORG]="redhat-appstudio-qe"
+        [OCI_STORAGE_USERNAME]="$(jq -r '."quay-username"' ${konflux_infra_secrets_file}/oci-storage)"
+        [OCI_STORAGE_TOKEN]="$(jq -r '."quay-token"' ${konflux_infra_secrets_file}/oci-storage)"
+    )
+
+    declare -A load_envs_from_file=(
+        [DEFAULT_QUAY_ORG_TOKEN]="${konflux_ci_secrets_file}/default-quay-org-token"
+        [GITHUB_TOKENS_LIST]="${konflux_ci_secrets_file}/github_accounts"
+        [QUAY_TOKEN]="${konflux_ci_secrets_file}/quay-token"
+        [QUAY_OAUTH_USER]="${konflux_ci_secrets_file}/quay-oauth-user"
+        [QUAY_OAUTH_TOKEN]="${konflux_ci_secrets_file}/quay-oauth-token"
+        [PYXIS_STAGE_KEY]="${konflux_ci_secrets_file}/pyxis-stage-key"
+        [PYXIS_STAGE_CERT]="${konflux_ci_secrets_file}/pyxis-stage-cert"
+        [OFFLINE_TOKEN]="${konflux_ci_secrets_file}/stage_offline_token"
+        [TOOLCHAIN_API_URL]="${konflux_ci_secrets_file}/stage_toolchain_api_url"
+        [KEYLOAK_URL]="${konflux_ci_secrets_file}/stage_keyloak_url"
+        [EXODUS_PROD_KEY]="${konflux_ci_secrets_file}/exodus_prod_key"
+        [EXODUS_PROD_CERT]="${konflux_ci_secrets_file}/exodus_prod_cert"
+        [CGW_USERNAME]="${konflux_ci_secrets_file}/cgw_username"
+        [CGW_TOKEN]="${konflux_ci_secrets_file}/cgw_token"
+        [REL_IMAGE_CONTROLLER_QUAY_ORG]="${konflux_ci_secrets_file}/release_image_controller_quay_org"
+        [REL_IMAGE_CONTROLLER_QUAY_TOKEN]="${konflux_ci_secrets_file}/release_image_controller_quay_token"
+        [QE_SPRAYPROXY_HOST]="${konflux_ci_secrets_file}/qe-sprayproxy-host"
+        [QE_SPRAYPROXY_TOKEN]="${konflux_ci_secrets_file}/qe-sprayproxy-token"
+        [E2E_PAC_GITHUB_APP_ID]="${konflux_ci_secrets_file}/pac-github-app-id"
+        [E2E_PAC_GITHUB_APP_PRIVATE_KEY]="${konflux_ci_secrets_file}/pac-github-app-private-key"
+        [PAC_GITHUB_APP_WEBHOOK_SECRET]="${konflux_ci_secrets_file}/pac-github-app-webhook-secret"
+        [SLACK_BOT_TOKEN]="${konflux_ci_secrets_file}/slack-bot-token"
+        [MULTI_PLATFORM_AWS_ACCESS_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-access-key"
+        [MULTI_PLATFORM_AWS_SECRET_ACCESS_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-secret-access-key"
+        [MULTI_PLATFORM_AWS_SSH_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-ssh-key"
+        [MULTI_PLATFORM_IBM_API_KEY]="${konflux_ci_secrets_file}/multi-platform-ibm-api-key"
+        [DOCKER_IO_AUTH]="${konflux_ci_secrets_file}/docker_io"
+        [GITLAB_BOT_TOKEN]="${konflux_ci_secrets_file}/gitlab-bot-token"
+    )
+
+    for var in "${!config_envs[@]}"; do
+        export "$var"="${config_envs[$var]}"
+    done
+
+    for var in "${!load_envs_from_file[@]}"; do
+        local file="${load_envs_from_file[$var]}"
+        if [[ -f "$file" ]]; then
+            export "$var"="$(<"$file")"
+        else
+            log "ERROR" "Secret file for $var not found at $file"
+        fi
+    done
+}
+
+post_actions() {
+    local exit_code=$?
+    local temp_annotation_file="$(mktemp)"
+
+    if [[ "${UNREGISTER_PAC}" == "true" ]]; then
+        make ci/sprayproxy/unregister
+    fi
+
+    cd "$ARTIFACT_DIR"
+
+    # Fetch the manifest annotations for the container
+    if ! MANIFESTS=$(oras manifest fetch "${ORAS_CONTAINER}" | jq .annotations); then
+        log "ERROR" "Failed to fetch manifest from ${OCI_STORAGE_CONTAINER}"
+        exit 1
+    fi
+
+    jq -n --argjson manifest "$MANIFESTS" '{ "$manifest": $manifest }' > "${temp_annotation_file}"
+
+    oras pull "${ORAS_CONTAINER}"
+
+    local attempt=1
+    while ! oras push "$ORAS_CONTAINER" --username="${OCI_STORAGE_USERNAME}" --password="${OCI_STORAGE_TOKEN}" --annotation-file "${temp_annotation_file}" ./:application/vnd.acme.rocket.docs.layer.v1+tar; do
+        if [[ $attempt -ge 5 ]]; then
+            log "ERROR" "oras push failed after $attempt attempts."
+            exit 1
+        fi
+        log "WARNING" "oras push failed (attempt $attempt). Retrying in 5 seconds..."
+        sleep 5
+        ((attempt++))
+    done
+
+    exit "$exit_code"
+}
+
+trap post_actions EXIT
+
+load_envs
+
 oc config view --minify --raw > /workspace/kubeconfig
-unset KUBECONFIG
 export KUBECONFIG=/workspace/kubeconfig
 
-# Create artifact directory if it doesn't exist
-if [ ! -d "$ARTIFACT_DIR" ]; then
-    mkdir -p "$ARTIFACT_DIR"
-fi
-
-# Export environment variables from secrets
-export DEFAULT_QUAY_ORG=redhat-appstudio-qe
-export DEFAULT_QUAY_ORG_TOKEN=$(cat /usr/local/konflux-ci-secrets/default-quay-org-token)
-export GITHUB_USER=""
-export GITHUB_TOKEN=""
-export GITHUB_TOKENS_LIST=$(cat /usr/local/konflux-ci-secrets/github_accounts)
-export QUAY_TOKEN=$(cat /usr/local/konflux-ci-secrets/quay-token)
-export QUAY_OAUTH_USER=$(cat /usr/local/konflux-ci-secrets/quay-oauth-user)
-export QUAY_OAUTH_TOKEN=$(cat /usr/local/konflux-ci-secrets/quay-oauth-token)
-export PYXIS_STAGE_KEY=$(cat /usr/local/konflux-ci-secrets/pyxis-stage-key)
-export PYXIS_STAGE_CERT=$(cat /usr/local/konflux-ci-secrets/pyxis-stage-cert)
-export OFFLINE_TOKEN=$(cat /usr/local/konflux-ci-secrets/stage_offline_token)
-export TOOLCHAIN_API_URL=$(cat /usr/local/konflux-ci-secrets/stage_toolchain_api_url)
-export KEYLOAK_URL=$(cat /usr/local/konflux-ci-secrets/stage_keyloak_url)
-export EXODUS_PROD_KEY=$(cat /usr/local/konflux-ci-secrets/exodus_prod_key)
-export EXODUS_PROD_CERT=$(cat /usr/local/konflux-ci-secrets/exodus_prod_cert)
-export CGW_USERNAME=$(cat /usr/local/konflux-ci-secrets/cgw_username)
-export CGW_TOKEN=$(cat /usr/local/konflux-ci-secrets/cgw_token)
-export REL_IMAGE_CONTROLLER_QUAY_ORG=$(cat /usr/local/konflux-ci-secrets/release_image_controller_quay_org)
-export REL_IMAGE_CONTROLLER_QUAY_TOKEN=$(cat /usr/local/konflux-ci-secrets/release_image_controller_quay_token)
-export OPENSHIFT_API=$(yq e '.clusters[0].cluster.server' "$KUBECONFIG")
-export OPENSHIFT_USERNAME="kubeadmin"
 export PREVIOUS_RATE_REMAINING=0
-export QE_SPRAYPROXY_HOST=$(cat /usr/local/konflux-ci-secrets/qe-sprayproxy-host)
-export QE_SPRAYPROXY_TOKEN=$(cat /usr/local/konflux-ci-secrets/qe-sprayproxy-token)
-export E2E_PAC_GITHUB_APP_ID=$(cat /usr/local/konflux-ci-secrets/pac-github-app-id)
-export E2E_PAC_GITHUB_APP_PRIVATE_KEY=$(cat /usr/local/konflux-ci-secrets/pac-github-app-private-key)
-export PAC_GITHUB_APP_WEBHOOK_SECRET=$(cat /usr/local/konflux-ci-secrets/pac-github-app-webhook-secret)
-export SLACK_BOT_TOKEN=$(cat /usr/local/konflux-ci-secrets/slack-bot-token)
-export MULTI_PLATFORM_AWS_ACCESS_KEY=$(cat /usr/local/konflux-ci-secrets/multi-platform-aws-access-key)
-export MULTI_PLATFORM_AWS_SECRET_ACCESS_KEY=$(cat /usr/local/konflux-ci-secrets/multi-platform-aws-secret-access-key)
-export MULTI_PLATFORM_AWS_SSH_KEY=$(cat /usr/local/konflux-ci-secrets/multi-platform-aws-ssh-key)
-export MULTI_PLATFORM_IBM_API_KEY=$(cat /usr/local/konflux-ci-secrets/multi-platform-ibm-api-key)
-export ORAS_USERNAME=$(cat /usr/local/konflux-ci-secrets/oras-username)
-export ORAS_PASSWORD=$(cat /usr/local/konflux-ci-secrets/oras-password)
-export DOCKER_IO_AUTH=$(cat /usr/local/konflux-ci-secrets/docker_io)
-export GITLAB_BOT_TOKEN=$(cat /usr/local/konflux-ci-secrets/gitlab-bot-token)
-
-export ENABLE_SCHEDULING_ON_MASTER_NODES=false
-
-# Process GitHub accounts
 IFS=',' read -r -a GITHUB_ACCOUNTS_ARRAY <<< "$(cat /usr/local/konflux-ci-secrets/github_accounts)"
 for account in "${GITHUB_ACCOUNTS_ARRAY[@]}"; do
     IFS=':' read -r -a GITHUB_USERNAME_ARRAY <<< "$account"
-    
+
     GH_RATE_REMAINING=$(curl -s \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_USERNAME_ARRAY[1]}" \
         https://api.github.com/rate_limit | jq ".rate.remaining")
 
-    echo -e "[INFO ] user: ${GITHUB_USERNAME_ARRAY[0]} with rate limit remaining $GH_RATE_REMAINING"
+    log "INFO" "user: ${GITHUB_USERNAME_ARRAY[0]} with rate limit remaining $GH_RATE_REMAINING"
     if [[ "$GH_RATE_REMAINING" -ge "$PREVIOUS_RATE_REMAINING" ]]; then
         GITHUB_USER="${GITHUB_USERNAME_ARRAY[0]}"
         GITHUB_TOKEN="${GITHUB_USERNAME_ARRAY[1]}"
@@ -94,15 +126,14 @@ for account in "${GITHUB_ACCOUNTS_ARRAY[@]}"; do
     PREVIOUS_RATE_REMAINING="$GH_RATE_REMAINING"
 done
 
-echo -e "[INFO] Start tests with user: ${GITHUB_USER}"
+log "INFO" "running tests with github user: ${GITHUB_USER}"
 
-# ROSA HCP workaround for docker limits in konflux-otel
+# ROSA HCP workaround for Docker limits
 oc create namespace konflux-otel
 oc create sa open-telemetry-opentelemetry-collector -n konflux-otel
 
 oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > ./global-pull-secret.json
 oc get secret -n openshift-config -o yaml pull-secret > global-pull-secret.yaml
-
 yq -i '.metadata.namespace = "konflux-otel"' global-pull-secret.yaml
 
 oc registry login --registry=docker.io --auth-basic="$DOCKER_IO_AUTH" --to=./global-pull-secret.json
@@ -110,21 +141,17 @@ oc apply -f global-pull-secret.yaml -n konflux-otel
 oc set data secret/pull-secret -n konflux-otel --from-file=.dockerconfigjson=./global-pull-secret.json
 oc secrets link open-telemetry-opentelemetry-collector pull-secret --for=pull -n konflux-otel
 
-# Prepare and run tests
-cd "$(mktemp -d)" || exit 1
+# Prepare git, pair branch if necessary, Install Konflux and run e2e tests
+cd "$(mktemp -d)"
 
 git config --global user.name "redhat-appstudio-qe-bot"
 git config --global user.email redhat-appstudio-qe-bot@redhat.com
 
 mkdir -p "${HOME}/creds"
-GIT_CREDS_PATH="${HOME}/creds/file"
-git config --global credential.helper "store --file ${GIT_CREDS_PATH}"
-
-echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > "${GIT_CREDS_PATH}"
+git_creds_path="${HOME}/creds/file"
+git config --global credential.helper "store --file $git_creds_path"
+echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > "$git_creds_path"
 
 git clone --origin upstream --branch main "https://${GITHUB_TOKEN}@github.com/konflux-ci/e2e-tests.git" .
-
-make ci/prepare/e2e-branch 2>&1 | tee /workspace/test-artifacts/e2e-branch.log
-
-UNREGISTER_PAC=true
-make ci/test/e2e 2>&1 | tee /workspace/test-artifacts/e2e-tests.log
+make ci/prepare/e2e-branch 2>&1 | tee "${ARTIFACT_DIR}"/e2e-branch.log
+make ci/test/e2e 2>&1 | tee "${ARTIFACT_DIR}"/e2e-tests.log
