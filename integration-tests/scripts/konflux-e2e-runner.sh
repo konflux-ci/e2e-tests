0642c32cffd6160459d9ee42d28b614198047405
Flavius Lacatusu <flacatus@redhat.com>
2024-09-25 19:57:34 +0200

    feat: Add workaround to avoid rate limits in ROSA HCP clusters

--- integration-tests/scripts/konflux-e2e-runner.sh
+++ integration-tests/scripts/konflux-e2e-runner.sh
@@ -1,145 +1,33 @@
 #!/bin/bash
 
-set -euo pipefail
+# Function to apply Docker rate limit workaround for a given namespace and service account in ROSA HCP clusters.
+docker_io_workaround() {
+    local namespace=$1
+    local service_account=$2
 
-log() {
-    echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] [$1] $2"
+    oc create namespace $namespace --dry-run=client -o yaml | oc apply -f -
+    oc create sa $service_account -n $namespace --dry-run=client -o yaml | oc apply -f -
+
+    oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > ./global-pull-secret.json
+    oc get secret -n openshift-config -o yaml pull-secret > ./global-pull-secret-$namespace.yaml
+    yq -i ".metadata.namespace = \"$namespace\"" ./global-pull-secret-$namespace.yaml
+
+    oc registry login --registry=docker.io --auth-basic="$DOCKER_IO_AUTH" --to=./global-pull-secret.json
+    oc apply -f ./global-pull-secret-$namespace.yaml -n $namespace
+    oc set data secret/pull-secret -n $namespace --from-file=.dockerconfigjson=./global-pull-secret.json
+    oc secrets link $service_account pull-secret --for=pull -n $namespace
 }
 
-load_envs() {
-    local konflux_ci_secrets_file="/usr/local/konflux-ci-secrets"
-    local konflux_infra_secrets_file="/usr/local/konflux-test-infra"
+# List of namespaces and corresponding service accounts
+declare -A namespace_service_account=(
+    ["konflux-otel"]="open-telemetry-opentelemetry-collector"
+    ["minio-operator"]="minio-operator"
+)
 
-    declare -A config_envs=(
-        [ENABLE_SCHEDULING_ON_MASTER_NODES]="false"
-        [UNREGISTER_PAC]="true"
-        [EC_DISABLE_DOWNLOAD_SERVICE]="true"
-        [ARTIFACT_DIR]="$(mktemp -d)"
-        [GITHUB_USER]=""
-        [GITHUB_TOKEN]=""
-        [DEFAULT_QUAY_ORG]="redhat-appstudio-qe"
-        [OCI_STORAGE_USERNAME]="$(jq -r '."quay-username"' ${konflux_infra_secrets_file}/oci-storage)"
-        [OCI_STORAGE_TOKEN]="$(jq -r '."quay-token"' ${konflux_infra_secrets_file}/oci-storage)"
-    )
-
-    declare -A load_envs_from_file=(
-        [DEFAULT_QUAY_ORG_TOKEN]="${konflux_ci_secrets_file}/default-quay-org-token"
-        [GITHUB_TOKENS_LIST]="${konflux_ci_secrets_file}/github_accounts"
-        [QUAY_TOKEN]="${konflux_ci_secrets_file}/quay-token"
-        [QUAY_OAUTH_USER]="${konflux_ci_secrets_file}/quay-oauth-user"
-        [QUAY_OAUTH_TOKEN]="${konflux_ci_secrets_file}/quay-oauth-token"
-        [PYXIS_STAGE_KEY]="${konflux_ci_secrets_file}/pyxis-stage-key"
-        [PYXIS_STAGE_CERT]="${konflux_ci_secrets_file}/pyxis-stage-cert"
-        [OFFLINE_TOKEN]="${konflux_ci_secrets_file}/stage_offline_token"
-        [TOOLCHAIN_API_URL]="${konflux_ci_secrets_file}/stage_toolchain_api_url"
-        [KEYLOAK_URL]="${konflux_ci_secrets_file}/stage_keyloak_url"
-        [EXODUS_PROD_KEY]="${konflux_ci_secrets_file}/exodus_prod_key"
-        [EXODUS_PROD_CERT]="${konflux_ci_secrets_file}/exodus_prod_cert"
-        [CGW_USERNAME]="${konflux_ci_secrets_file}/cgw_username"
-        [CGW_TOKEN]="${konflux_ci_secrets_file}/cgw_token"
-        [REL_IMAGE_CONTROLLER_QUAY_ORG]="${konflux_ci_secrets_file}/release_image_controller_quay_org"
-        [REL_IMAGE_CONTROLLER_QUAY_TOKEN]="${konflux_ci_secrets_file}/release_image_controller_quay_token"
-        [QE_SPRAYPROXY_HOST]="${konflux_ci_secrets_file}/qe-sprayproxy-host"
-        [QE_SPRAYPROXY_TOKEN]="${konflux_ci_secrets_file}/qe-sprayproxy-token"
-        [E2E_PAC_GITHUB_APP_ID]="${konflux_ci_secrets_file}/pac-github-app-id"
-        [E2E_PAC_GITHUB_APP_PRIVATE_KEY]="${konflux_ci_secrets_file}/pac-github-app-private-key"
-        [PAC_GITHUB_APP_WEBHOOK_SECRET]="${konflux_ci_secrets_file}/pac-github-app-webhook-secret"
-        [SLACK_BOT_TOKEN]="${konflux_ci_secrets_file}/slack-bot-token"
-        [MULTI_PLATFORM_AWS_ACCESS_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-access-key"
-        [MULTI_PLATFORM_AWS_SECRET_ACCESS_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-secret-access-key"
-        [MULTI_PLATFORM_AWS_SSH_KEY]="${konflux_ci_secrets_file}/multi-platform-aws-ssh-key"
-        [MULTI_PLATFORM_IBM_API_KEY]="${konflux_ci_secrets_file}/multi-platform-ibm-api-key"
-        [DOCKER_IO_AUTH]="${konflux_ci_secrets_file}/docker_io"
-        [GITLAB_BOT_TOKEN]="${konflux_ci_secrets_file}/gitlab-bot-token"
-    )
-
-    for var in "${!config_envs[@]}"; do
-        export "$var"="${config_envs[$var]}"
-    done
-
-    for var in "${!load_envs_from_file[@]}"; do
-        local file="${load_envs_from_file[$var]}"
-        if [[ -f "$file" ]]; then
-            export "$var"="$(<"$file")"
-        else
-            log "ERROR" "Secret file for $var not found at $file"
-        fi
-    done
-}
-
-post_actions() {
-    local exit_code=$?
-    local temp_annotation_file="$(mktemp)"
-
-    if [[ "${UNREGISTER_PAC}" == "true" ]]; then
-        make ci/sprayproxy/unregister
-    fi
-
-    cd "$ARTIFACT_DIR"
-
-    # Fetch the manifest annotations for the container
-    if ! MANIFESTS=$(oras manifest fetch "${ORAS_CONTAINER}" | jq .annotations); then
-        log "ERROR" "Failed to fetch manifest from ${OCI_STORAGE_CONTAINER}"
-        exit 1
-    fi
-
-    jq -n --argjson manifest "$MANIFESTS" '{ "$manifest": $manifest }' > "${temp_annotation_file}"
-
-    oras pull "${ORAS_CONTAINER}"
-
-    local attempt=1
-    while ! oras push "$ORAS_CONTAINER" --username="${OCI_STORAGE_USERNAME}" --password="${OCI_STORAGE_TOKEN}" --annotation-file "${temp_annotation_file}" ./:application/vnd.acme.rocket.docs.layer.v1+tar; do
-        if [[ $attempt -ge 5 ]]; then
-            log "ERROR" "oras push failed after $attempt attempts."
-            exit 1
-        fi
-        log "WARNING" "oras push failed (attempt $attempt). Retrying in 5 seconds..."
-        sleep 5
-        ((attempt++))
-    done
-
-    exit "$exit_code"
-}
-
-trap post_actions EXIT
-
-load_envs
-
-oc config view --minify --raw > /workspace/kubeconfig
-export KUBECONFIG=/workspace/kubeconfig
-
-export PREVIOUS_RATE_REMAINING=0
-IFS=',' read -r -a GITHUB_ACCOUNTS_ARRAY <<< "$(cat /usr/local/konflux-ci-secrets/github_accounts)"
-for account in "${GITHUB_ACCOUNTS_ARRAY[@]}"; do
-    IFS=':' read -r -a GITHUB_USERNAME_ARRAY <<< "$account"
-
-    GH_RATE_REMAINING=$(curl -s \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GITHUB_USERNAME_ARRAY[1]}" \
-        https://api.github.com/rate_limit | jq ".rate.remaining")
-
-    log "INFO" "user: ${GITHUB_USERNAME_ARRAY[0]} with rate limit remaining $GH_RATE_REMAINING"
-    if [[ "$GH_RATE_REMAINING" -ge "$PREVIOUS_RATE_REMAINING" ]]; then
-        GITHUB_USER="${GITHUB_USERNAME_ARRAY[0]}"
-        GITHUB_TOKEN="${GITHUB_USERNAME_ARRAY[1]}"
-    fi
-    PREVIOUS_RATE_REMAINING="$GH_RATE_REMAINING"
+# Apply workaround for each namespace and service account
+for namespace in "${!namespace_service_account[@]}"; do
+    docker_io_workaround $namespace ${namespace_service_account[$namespace]}
 done
-
-log "INFO" "running tests with github user: ${GITHUB_USER}"
-
-# ROSA HCP workaround for Docker limits
-oc create namespace konflux-otel
-oc create sa open-telemetry-opentelemetry-collector -n konflux-otel
-
-oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > ./global-pull-secret.json
-oc get secret -n openshift-config -o yaml pull-secret > global-pull-secret.yaml
-yq -i '.metadata.namespace = "konflux-otel"' global-pull-secret.yaml
-
-oc registry login --registry=docker.io --auth-basic="$DOCKER_IO_AUTH" --to=./global-pull-secret.json
-oc apply -f global-pull-secret.yaml -n konflux-otel
-oc set data secret/pull-secret -n konflux-otel --from-file=.dockerconfigjson=./global-pull-secret.json
-oc secrets link open-telemetry-opentelemetry-collector pull-secret --for=pull -n konflux-otel
 
 # Prepare git, pair branch if necessary, Install Konflux and run e2e tests
 cd "$(mktemp -d)"
