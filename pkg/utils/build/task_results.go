ccf34b73de8df02763907811c2693f2759183bc5
Luiz Carvalho <lucarval@redhat.com>
2024-10-07 16:48:31 -0400

    feat: verify REPORTS result from clair-scan Task
    Ref: https://issues.redhat.com/browse/EC-839
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- pkg/utils/build/task_results.go
+++ pkg/utils/build/task_results.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
 
+	"github.com/konflux-ci/e2e-tests/pkg/clients/oras"
 	"github.com/konflux-ci/e2e-tests/pkg/constants"
 	pipeline "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -28,6 +32,8 @@ type ClairScanResult struct {
 	Vulnerabilities Vulnerabilities `json:"vulnerabilities"`
 }
 
+type ClairScanReports map[string]string
+
 type Vulnerabilities struct {
 	Critical int `json:"critical"`
 	High     int `json:"high"`
@@ -36,6 +42,17 @@ type Vulnerabilities struct {
 }
 
 func ValidateBuildPipelineTestResults(pipelineRun *pipeline.PipelineRun, c crclient.Client, isFBCBuild bool) error {
+	var imageURL string
+	for _, result := range pipelineRun.Status.Results {
+		if result.Name == "IMAGE_URL" {
+			imageURL = strings.TrimSpace(result.Value.StringVal)
+			break
+		}
+	}
+	if imageURL == "" {
+		return fmt.Errorf("unable to find IMAGE_URL result from PipelineRun %s", pipelineRun.Name)
+	}
+
 	for _, taskName := range taskNames {
 		// The inspect-image task is only required for FBC pipelines which we can infer by the component name
 
@@ -55,13 +72,14 @@ func ValidateBuildPipelineTestResults(pipelineRun *pipeline.PipelineRun, c crcli
 		switch taskName {
 		case "clair-scan":
 			resultsToValidate = append(resultsToValidate, "SCAN_OUTPUT")
+			resultsToValidate = append(resultsToValidate, "REPORTS")
 		case "deprecated-image-check":
 			resultsToValidate = append(resultsToValidate, "PYXIS_HTTP_CODE")
 		case "inspect-image":
 			resultsToValidate = append(resultsToValidate, "BASE_IMAGE", "BASE_IMAGE_REPOSITORY")
 		}
 
-		if err := validateTaskRunResult(results, resultsToValidate, taskName); err != nil {
+		if err := validateTaskRunResult(imageURL, results, resultsToValidate, taskName); err != nil {
 			return err
 		}
 
@@ -85,7 +103,7 @@ func fetchTaskRunResults(c crclient.Client, pr *pipeline.PipelineRun, pipelineTa
 		"pipelineTaskName %q not found in PipelineRun %s/%s", pipelineTaskName, pr.GetName(), pr.GetNamespace())
 }
 
-func validateTaskRunResult(trResults []pipeline.TaskRunResult, expectedResultNames []string, taskName string) error {
+func validateTaskRunResult(imageURL string, trResults []pipeline.TaskRunResult, expectedResultNames []string, taskName string) error {
 	for _, rn := range expectedResultNames {
 		found := false
 		for _, r := range trResults {
@@ -103,6 +121,41 @@ func validateTaskRunResult(trResults []pipeline.TaskRunResult, expectedResultNam
 					err := json.Unmarshal([]byte(r.Value.StringVal), &testOutput)
 					if err != nil {
 						return fmt.Errorf("cannot parse SCAN_OUTPUT result: %+v", err)
+					}
+				case "REPORTS":
+					var reports = ClairScanReports{}
+					err := json.Unmarshal([]byte(r.Value.StringVal), &reports)
+					if err != nil {
+						return fmt.Errorf("cannot parse REPORTS result: %w", err)
+					}
+					for _, reportDigest := range reports {
+						reportRef := fmt.Sprintf("%s@%s", imageURL, reportDigest)
+
+						imageDir, err := oras.PullArtifacts(reportRef)
+						if err != nil {
+							return fmt.Errorf("cannot fetch report from ref %s: %w", reportRef, err)
+						}
+
+						var hasNonEmptyReport bool
+						if err := filepath.Walk(imageDir, func(p string, info fs.FileInfo, err error) error {
+							if err != nil {
+								return err
+							}
+							if info.IsDir() {
+								return nil
+							}
+							if info.Size() == 0 {
+								return fmt.Errorf("report %s from %s is empty", p, reportRef)
+							}
+							hasNonEmptyReport = true
+							return nil
+						}); err != nil {
+							return err
+						}
+
+						if !hasNonEmptyReport {
+							return fmt.Errorf("no report files were found in %s", reportRef)
+						}
 					}
 				case "PYXIS_HTTP_CODE", "BASE_IMAGE", "BASE_IMAGE_REPOSITORY":
 					if len(r.Value.StringVal) < 1 {
