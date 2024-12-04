2efc444be23a3439fedb2643c023b2d5d62a8d3b
Pavel Sturc <psturc@redhat.com>
2024-12-04 12:03:21 +0100

    fix: PR pairing

--- integration-tests/scripts/konflux-e2e-runner.sh
+++ integration-tests/scripts/konflux-e2e-runner.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-log() {
-    echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] [$1] $2"
-}
-
 load_envs() {
     local konflux_ci_secrets_file="/usr/local/konflux-ci-secrets"
     local konflux_infra_secrets_file="/usr/local/konflux-test-infra"
@@ -14,9 +10,6 @@ load_envs() {
         [ENABLE_SCHEDULING_ON_MASTER_NODES]="false"
         [UNREGISTER_PAC]="true"
         [EC_DISABLE_DOWNLOAD_SERVICE]="true"
-        [ARTIFACT_DIR]="$(mktemp -d)"
-        [GITHUB_USER]=""
-        [GITHUB_TOKEN]=""
         [DEFAULT_QUAY_ORG]="redhat-appstudio-qe"
         [OCI_STORAGE_USERNAME]="$(jq -r '."quay-username"' ${konflux_infra_secrets_file}/oci-storage)"
         [OCI_STORAGE_TOKEN]="$(jq -r '."quay-token"' ${konflux_infra_secrets_file}/oci-storage)"
@@ -24,7 +17,6 @@ load_envs() {
 
     declare -A load_envs_from_file=(
         [DEFAULT_QUAY_ORG_TOKEN]="${konflux_ci_secrets_file}/default-quay-org-token"
-        [GITHUB_TOKENS_LIST]="${konflux_ci_secrets_file}/github_accounts"
         [QUAY_TOKEN]="${konflux_ci_secrets_file}/quay-token"
         [QUAY_OAUTH_USER]="${konflux_ci_secrets_file}/quay-oauth-user"
         [QUAY_OAUTH_TOKEN]="${konflux_ci_secrets_file}/quay-oauth-token"
@@ -71,7 +63,9 @@ load_envs() {
 
 post_actions() {
     local exit_code=$?
-    local temp_annotation_file="$(mktemp)"
+    local temp_annotation_file
+
+    temp_annotation_file="$(mktemp)"
 
     if [[ "${UNREGISTER_PAC}" == "true" ]]; then
         make ci/sprayproxy/unregister
@@ -81,7 +75,7 @@ post_actions() {
 
     # Fetch the manifest annotations for the container
     if ! MANIFESTS=$(oras manifest fetch "${ORAS_CONTAINER}" | jq .annotations); then
-        log "ERROR" "Failed to fetch manifest from ${OCI_STORAGE_CONTAINER}"
+        log "ERROR" "Failed to fetch manifest from ${ORAS_CONTAINER}"
         exit 1
     fi
 
@@ -110,26 +104,6 @@ load_envs
 oc config view --minify --raw > /workspace/kubeconfig
 export KUBECONFIG=/workspace/kubeconfig
 
-export PREVIOUS_RATE_REMAINING=0
-IFS=',' read -r -a GITHUB_ACCOUNTS_ARRAY <<< "$(cat /usr/local/konflux-ci-secrets/github_accounts)"
-for account in "${GITHUB_ACCOUNTS_ARRAY[@]}"; do
-    IFS=':' read -r -a GITHUB_USERNAME_ARRAY <<< "$account"
-
-    GH_RATE_REMAINING=$(curl -s \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GITHUB_USERNAME_ARRAY[1]}" \
-        https://api.github.com/rate_limit | jq ".rate.remaining")
-
-    log "INFO" "user: ${GITHUB_USERNAME_ARRAY[0]} with rate limit remaining $GH_RATE_REMAINING"
-    if [[ "$GH_RATE_REMAINING" -ge "$PREVIOUS_RATE_REMAINING" ]]; then
-        GITHUB_USER="${GITHUB_USERNAME_ARRAY[0]}"
-        GITHUB_TOKEN="${GITHUB_USERNAME_ARRAY[1]}"
-    fi
-    PREVIOUS_RATE_REMAINING="$GH_RATE_REMAINING"
-done
-
-log "INFO" "running tests with github user: ${GITHUB_USER}"
-
 # ROSA HCP workaround for Docker limits
 # for namespaces 'minio-operator' and 'tekton-results'
 oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > ./global-pull-secret.json
@@ -154,18 +128,4 @@ while IFS='|' read -r ns sa_name; do
     oc secrets link "$sa_name" pull-secret --for=pull -n "$ns"
 done <<< "$namespace_sa_names"
 
-
-# Prepare git, pair branch if necessary, Install Konflux and run e2e tests
-cd "$(mktemp -d)"
-
-git config --global user.name "redhat-appstudio-qe-bot"
-git config --global user.email redhat-appstudio-qe-bot@redhat.com
-
-mkdir -p "${HOME}/creds"
-git_creds_path="${HOME}/creds/file"
-git config --global credential.helper "store --file $git_creds_path"
-echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > "$git_creds_path"
-
-git clone --origin upstream --branch main "https://${GITHUB_TOKEN}@github.com/konflux-ci/e2e-tests.git" .
-make ci/prepare/e2e-branch 2>&1 | tee "${ARTIFACT_DIR}"/e2e-branch.log
 make ci/test/e2e 2>&1 | tee "${ARTIFACT_DIR}"/e2e-tests.log
