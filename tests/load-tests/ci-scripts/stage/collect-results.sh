63f487494a5ce1c1e90902df892ad9eee836714e
Jan Hutar <jhutar@redhat.com>
2025-01-29 08:06:01 +0100

    feat: Remove unneeded loop as reason for it was removed in 766cb58dab7674a475b965a5329a728e4f9394d5

--- tests/load-tests/ci-scripts/stage/collect-results.sh
+++ tests/load-tests/ci-scripts/stage/collect-results.sh
@@ -73,39 +73,4 @@ status_data.py \
 
 deactivate
 
-echo "[$(date --utc -Ins)] Collecting additional info"
-if ! [ -r users.json ]; then
-    echo "ERROR: Missing file with user creds"
-else
-    mkdir -p $ARTIFACT_DIR/collected-data
-    login_log_stub="${ARTIFACT_DIR}/collected-data/collected-oc_login"
-    application_stub="${ARTIFACT_DIR}/collected-data/collected-applications.appstudio.redhat.com"
-    component_stub="${ARTIFACT_DIR}/collected-data/collected-components.appstudio.redhat.com"
-
-    for uid in $( seq 1 $CONCURRENCY ); do
-        username="test-rhtap-$uid"
-        offline_token=$( cat users.json | jq --raw-output '.[] | select(.username == "'$username'").token' )
-        api_server=$( cat users.json | jq --raw-output '.[] | select(.username == "'$username'").apiurl' )
-        sso_server=$( cat users.json | jq --raw-output '.[] | select(.username == "'$username'").ssourl' )
-        access_token=$( curl \
-                          --silent \
-                          --header "Accept: application/json" \
-                          --header "Content-Type: application/x-www-form-urlencoded" \
-                          --data-urlencode "grant_type=refresh_token" \
-                          --data-urlencode "client_id=cloud-services" \
-                          --data-urlencode "refresh_token=${offline_token}" \
-                          "${sso_server}" \
-                        | jq --raw-output ".access_token" )
-        login_log="${login_log_stub}-${username}.log"
-        echo "Logging in as $username..."
-        if ! oc login --token="$access_token" --server="$api_server" &>$login_log; then
-            echo "ERROR: Login as $username failed:"
-            cat "$login_log"
-            continue
-        fi
-        tenant="${username}-tenant"
-
-    done
-fi
-
 popd
