1a72c4aec4b028d945de723c0e862cdc12ab8e69
Krunoslav Pavic <kpavic@redhat.com>
2025-03-16 07:37:04 +0100

    fix(KONFLUX-6572): fix retries for WaitForComponentPipelineToBeFinished (#1536)
    * The previous iteration of the WaitForComponentPipelineToBeFinished
      function was always using the originally supplied `sha` parameter
      on every attempt
    * This fix ensres that the `shaToUpdate` variable is used instead
      and correctly updated on every new attempt
    
    Signed-off-by: dirgim <kpavic@redhat.com>
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- pkg/clients/has/components.go
+++ pkg/clients/has/components.go
@@ -165,10 +165,11 @@ func (h *HasController) WaitForComponentPipelineToBeFinished(component *appservi
 	attempts := 1
 	app := component.Spec.Application
 	pr := &pipeline.PipelineRun{}
+	var shaToUpdate = &sha
 
 	for {
 		err := wait.PollUntilContextTimeout(context.Background(), constants.PipelineRunPollingInterval, 30*time.Minute, true, func(ctx context.Context) (done bool, err error) {
-			pr, err = h.GetComponentPipelineRun(component.GetName(), app, component.GetNamespace(), sha)
+			pr, err = h.GetComponentPipelineRun(component.GetName(), app, component.GetNamespace(), *shaToUpdate)
 
 			if err != nil {
 				GinkgoWriter.Printf("PipelineRun has not been created yet for the Component %s/%s\n", component.GetNamespace(), component.GetName())
@@ -211,7 +212,7 @@ func (h *HasController) WaitForComponentPipelineToBeFinished(component *appservi
 			if err = h.PipelineClient().TektonV1().PipelineRuns(pr.GetNamespace()).Delete(context.Background(), pr.GetName(), metav1.DeleteOptions{}); err != nil {
 				return fmt.Errorf("failed to delete PipelineRun %q from %q namespace with error: %v", pr.GetName(), pr.GetNamespace(), err)
 			}
-			if sha, err = h.RetriggerComponentPipelineRun(component, pr); err != nil {
+			if *shaToUpdate, err = h.RetriggerComponentPipelineRun(component, pr); err != nil {
 				return fmt.Errorf("unable to retrigger pipelinerun for component %s:%s: %+v", component.GetNamespace(), component.GetName(), err)
 			}
 			attempts++
