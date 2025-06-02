b8e6a4d5be65406c2c07b3d6b8eb0bcc0bf673a7
Jan Hutar <jhutar@redhat.com>
2025-06-02 06:56:08 +0200

    feat: Adding error message about component build creation timeout I have seen once today

--- tests/load-tests/errors.py
+++ tests/load-tests/errors.py
@@ -33,6 +33,7 @@ ERRORS = {
     "Repo forking failed as the target is still being deleted": r"Repo forking failed: Error forking project .* POST https://gitlab.cee.redhat.com.* 409 .*Project namespace name has already been taken, The project is still being deleted",
     "Repo forking failed when deleting target repo on gitlab.com (not CEE!) due unathorized": r"Repo forking failed: Error deleting project .* DELETE https://gitlab.com/.* 401 .* Unauthorized",
     "Timeout forking the repo before the actual test": r"Repo forking failed: Error forking project .*: context deadline exceeded",
+    "Timeout getting build service account": r"Component build SA failed creation: Component build SA .* not created: context deadline exceeded",
     "Timeout getting pipeline": r"Message:.*resolver failed to get Pipeline.*resolution took longer than global timeout of .*",
     "Timeout getting task via git resolver from gitlab.cee": r"Message:.*Couldn't retrieve Task .*resolver type git.*https://gitlab.cee.redhat.com/.* resolution took longer than global timeout of .*",
 }
