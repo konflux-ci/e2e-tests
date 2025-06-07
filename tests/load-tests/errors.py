b0ca18b75372469f45b9bd74d2f409e630a0e62f
Jan Hutar <jhutar@redhat.com>
2025-06-07 16:01:14 +0200

    feat: New error: Test Pipeline Run failed run: context deadline exceeded

--- tests/load-tests/errors.py
+++ tests/load-tests/errors.py
@@ -37,7 +37,8 @@ ERRORS = {
     "Timeout getting build service account": r"Component build SA failed creation: Component build SA .* not created: context deadline exceeded",
     "Timeout getting pipeline": r"Message:.*resolver failed to get Pipeline.*resolution took longer than global timeout of .*",
     "Timeout getting task via git resolver from gitlab.cee": r"Message:.*Couldn't retrieve Task .*resolver type git.*https://gitlab.cee.redhat.com/.* resolution took longer than global timeout of .*",
-    "Timeout waiting for integration test scenario to finish": r"Integration test scenario failed validation: context deadline exceeded",
+    "Timeout waiting for integration test scenario to validate": r"Integration test scenario failed validation: context deadline exceeded",
+    "Timeout waiting for test pipeline to finish": r"Test Pipeline Run failed run: context deadline exceeded",
 }
 
 
