11fd2624b88f398175a6ec19d9f7a8df9baca1d6
Larry Rios <lrios@redhat.com>
2025-06-18 08:06:29 -0400

    adding enhacements for playwright update token script and lib
    (cherry picked from commit c47ba8839627630596b553c12acc8556d8ff362e)

--- tests/load-tests/ci-scripts/utility_scripts/playwright_lib.py
+++ tests/load-tests/ci-scripts/utility_scripts/playwright_lib.py
@@ -9,12 +9,15 @@ def goto_login_and_accept_cookies(page):
     page.goto("https://console.redhat.com")
     page.wait_for_url("https://sso.redhat.com/**")
 
-    #### Accept cookies
-    ###cookies_iframe = page.frame_locator('iframe[name="trustarc_cm"]')
-    ###cookies_button = cookies_iframe.get_by_role(
-    ###    "button", name="Agree and proceed with standard settings"
-    ###)
-    ###cookies_button.click()
+    # Accept cookies
+    cookies_iframe = page.frame_locator('iframe[name="trustarc_cm"]')
+    cookies_button = cookies_iframe.get_by_role(
+        "button", name="Agree and proceed with standard settings"
+    )
+    if cookies_button.is_visible():
+        cookies_button.click()
+    else:
+        print("Cookies button not found or already clicked.")
 
 
 def form_login(page, username, password):
@@ -32,3 +35,4 @@ def form_login(page, username, password):
     page.locator('//button[@id="rh-password-verification-submit-button"]').click()
     page.wait_for_url("https://console.redhat.com/**")
     page.wait_for_selector('//h2[text()="Welcome to your Hybrid Cloud Console."]')
+
