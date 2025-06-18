1dfa9bb02273a013016ebd8e4a18e74bb84a4656
Jan Hutar <jhutar@redhat.com>
2025-06-18 09:50:27 +0200

    feat: New error: Repo forking failed: Error checking repository rhtap-perf-test/nodejs-devfile-sample2-undef-jhutar: GET https://api.github.com/repos/rhtap-perf-test/nodejs-devfile-sample2-undef-jhutar: 503 No server is currently available to service your request. Sorry about that. Please try resubmitting your request and contact us if the problem persists. []

--- tests/load-tests/ci-scripts/utility_scripts/playwright-update-tokens.py
+++ tests/load-tests/ci-scripts/utility_scripts/playwright-update-tokens.py
@@ -33,7 +33,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import playwright_lib
 
-PLAYWRIGHT_HEADLESS = False
+PLAYWRIGHT_HEADLESS = True
 PLAYWRIGHT_VIDEO_DIR = "videos/"
 
 
