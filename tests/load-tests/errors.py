647939175c2ad274a36b8c96243fe89891d14e05
Jan Hutar <jhutar@redhat.com>
2025-07-01 12:26:27 +0200

    feat: New error (and sort): ERROR Command returned error: Failed to download metadata (baseurl: "https://kojipkgs.fedoraproject.org/repos/f43-build/6585390/x86_64") for repository "build": Usable URL not found

--- tests/load-tests/errors.py
+++ tests/load-tests/errors.py
@@ -56,10 +56,11 @@ ERRORS = {
 
 FAILED_PLR_ERRORS = {
     "SKIP": r"Skipping step because a previous step failed",
+    "DNF failed to download repodata from Koji": r"ERROR Command returned error: Failed to download metadata (baseurl: \"https://kojipkgs.fedoraproject.org/repos/[^ ]*\") for repository \"build\": Usable URL not found",
     "Error allocating host as provision TR already exists": r"Error allocating host: taskruns.tekton.dev \".*provision\" already exists",
-    "RPM build failed: bool cannot be defined via typedef": r"error: .bool. cannot be defined via .typedef..*error: Bad exit status from /var/tmp/rpm-tmp..* ..build.",
-    "Failed because registry.access.redhat.com returned 503 when reading manifest": r"source-build:ERROR:command execution failure, status: 1, stderr: time=.* level=fatal msg=.Error parsing image name .* reading manifest .* in registry.access.redhat.com/.* received unexpected HTTP status: 503 Service Unavailable",
     "Failed because of quay.io returned 502": r"level=fatal msg=.Error parsing image name .*docker://quay.io/.* Requesting bearer token: invalid status code from registry 502 .Bad Gateway.",
+    "Failed because registry.access.redhat.com returned 503 when reading manifest": r"source-build:ERROR:command execution failure, status: 1, stderr: time=.* level=fatal msg=.Error parsing image name .* reading manifest .* in registry.access.redhat.com/.* received unexpected HTTP status: 503 Service Unavailable",
+    "RPM build failed: bool cannot be defined via typedef": r"error: .bool. cannot be defined via .typedef..*error: Bad exit status from /var/tmp/rpm-tmp..* ..build.",
 }
 
 
