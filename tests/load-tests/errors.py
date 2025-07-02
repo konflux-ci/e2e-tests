d1b6a878fdddeda7ffaacef1ee7e504f200906ba
Jan Hutar <jhutar@redhat.com>
2025-07-02 07:27:38 +0200

    feat: New error: Application failed creation: Unable to create the Application jhutar-app-wpyjx: failed to get API group resources: unable to retrieve the complete list of server APIs: appstudio.redhat.com/v1alpha1: Get https://api.stone-prod-p02.hjvn.p1.openshiftapps.com:6443/apis/appstudio.redhat.com/v1alpha1: net/http: TLS handshake timeout

--- tests/load-tests/errors.py
+++ tests/load-tests/errors.py
@@ -17,6 +17,7 @@ COLUMN_MESSAGE = 2
 
 # Errors patterns we recognize (when newlines were removed)
 ERRORS = {
+    "Application creation failed because of TLS handshake timeout": r"Application failed creation: Unable to create the Application .*: failed to get API group resources: unable to retrieve the complete list of server APIs: appstudio.redhat.com/v1alpha1: Get .*: net/http: TLS handshake timeout",
     "Application creation timed out waiting for quota evaluation": r"Application failed creation: Unable to create the Application .*: Internal error occurred: resource quota evaluation timed out",
     "Build Pipeline Run was cancelled" : r"Build Pipeline Run failed run: PipelineRun for component .* in namespace .* failed: .* Reason:Cancelled .* Message:PipelineRun .* was cancelled",
     "Component creation timed out waiting for image-controller annotations": r"Component failed creation: Unable to create the Component .* timed out when waiting for image-controller annotations to be updated on component",
