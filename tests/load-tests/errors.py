41299ca7c6e1936eecbbdce3959ea6e6be10ffbd
Jan Hutar <jhutar@redhat.com>
2025-07-03 10:45:29 +0200

    feat: New error: Release failed: Release jhutar-app-okeck-krc9c-4f2976e-dgznx in namespace jhutar-tenant failed: {Type:Released Status:False ObservedGeneration:0 LastTransitionTime:2025-07-02 06:44:05 +0000 UTC Reason:Progressing Message:}

--- tests/load-tests/errors.py
+++ tests/load-tests/errors.py
@@ -40,6 +40,7 @@ ERRORS = {
     "GitLab token used by test expired": r"Repo forking failed: Error deleting project .*: DELETE https://gitlab.cee.redhat.com/.*: 401 .*error: invalid_token.*error_description: Token is expired. You can either do re-authorization or token refresh",
     "Pipeline failed": r"Message:Tasks Completed: [0-9]+ \(Failed: [1-9]+,",
     "Post-test data collection failed": r"Failed to collect pipeline run JSONs",
+    "Release failed in progress without error given": r"Release failed: Release .* in namespace .* failed: .Type:Released Status:False .* Reason:Progressing Message:.$",
     "Release failure: PipelineRun not created": r"couldn't find PipelineRun in managed namespace '%s' for a release '%s' in '%s' namespace",
     "Repo forking failed as GitLab CEE says 401 Unauthorized": r"Repo forking failed: Error deleting project .*: DELETE https://gitlab.cee.redhat.com/.*: 401 .*message: 401 Unauthorized.*",
     "Repo forking failed as the target is still being deleted": r"Repo forking failed: Error forking project .* POST https://gitlab.cee.redhat.com.* 409 .*Project namespace name has already been taken, The project is still being deleted",
