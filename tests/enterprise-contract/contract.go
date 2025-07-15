6c92ed5bbd24631d0f3ea9baaf9942f9ec9fade5
Stefano Pentassuglia <spentass@redhat.com>
2025-07-15 13:29:36 +0200

    style: rename Enterprise Contract to Conforma (#1639)
    Update some of the Enterprise Contract references to Conforma, only in
    some docs and labels.
    Many objects and names still refer to it as EnterpriseContract, but it
    seems overkill to perform a full rename with the risk of breaking
    things, just for the sake of tidyness.
    
    Ref: https://issues.redhat.com/browse/EC-1128

--- tests/enterprise-contract/contract.go
+++ tests/enterprise-contract/contract.go
@@ -23,7 +23,7 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-var _ = framework.EnterpriseContractSuiteDescribe("Enterprise Contract E2E tests", Label("ec"), func() {
+var _ = framework.EnterpriseContractSuiteDescribe("Conforma E2E tests", Label("ec"), func() {
 
 	defer GinkgoRecover()
 
