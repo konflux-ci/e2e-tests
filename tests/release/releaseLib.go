aaed04ad6dd61d0eb4c438c655908c637fee20f6
Martin Jediny <jedinym@proton.me>
2025-08-04 11:55:20 +0200

    fixup! fix(ISV-6032): use suggested gingo patterns
    Signed-off-by: Martin Jediny <jedinym@proton.me>

--- tests/release/releaseLib.go
+++ tests/release/releaseLib.go
@@ -350,5 +350,5 @@ func VerifyAtlasURL(url string) {
 	uuidStr := strings.TrimPrefix(url, prefix)
 	u, err := uuid.Parse(uuidStr)
 	Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Could not parse UUID %s uuidStr", uuidStr))
-	Expect(u.Version()).To(Equal(4), fmt.Sprintf("UUID %s is not version 4", uuidStr))
+	Expect(u.Version()).To(BeEquivalentTo(4), fmt.Sprintf("UUID %s is not version 4", uuidStr))
 }
