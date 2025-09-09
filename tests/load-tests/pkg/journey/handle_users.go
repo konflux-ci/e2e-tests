90360db1f9c1700d680d4f73f34436a2ea991f8d
Jan Hutar <jhutar@redhat.com>
2025-09-09 08:59:10 +0200

    refactor: Simplify user frameworks creation

--- tests/load-tests/pkg/journey/handle_users.go
+++ tests/load-tests/pkg/journey/handle_users.go
@@ -5,57 +5,53 @@ import "time"
 import "strings"
 
 import logging "github.com/konflux-ci/e2e-tests/tests/load-tests/pkg/logging"
+import loadtestutils "github.com/konflux-ci/e2e-tests/tests/load-tests/pkg/loadtestutils"
 
 import "github.com/konflux-ci/e2e-tests/pkg/framework"
 import "github.com/konflux-ci/e2e-tests/pkg/utils"
 
+// Returns framework, namespace (and error)
+func provisionFramework(stageUsers []loadtestutils.User, threadIndex int, username string, isStage bool) (*framework.Framework, string, error) {
+	var f *framework.Framework
+	var err error
+
+	if isStage {
+		user := stageUsers[threadIndex]
+		f, err = framework.NewFrameworkWithTimeout(
+			username,
+			time.Minute*60,
+			utils.Options{
+				ApiUrl: user.APIURL,
+				Token:  user.Token,
+			})
+	} else {
+		f, err = framework.NewFrameworkWithTimeout(username, time.Minute*60)
+	}
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	return f, f.UserNamespace, nil
+}
+
 func HandleUser(ctx *MainContext) error {
 	var err error
 
-	// TODO E.g. when token is incorrect, timeout does not work as expected
 	if ctx.Opts.Stage {
-		user := (*ctx.StageUsers)[ctx.ThreadIndex]
-		ctx.Username = strings.TrimSuffix(user.Namespace, "-tenant")
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(
-			ctx.Username,
-			time.Minute*60,
-			utils.Options{
-				ApiUrl: user.APIURL,
-				Token:  user.Token,
-			})
+		ctx.Username = strings.TrimSuffix((*ctx.StageUsers)[ctx.ThreadIndex].Namespace, "-tenant")
 	} else {
 		ctx.Username = fmt.Sprintf("%s-%04d", ctx.Opts.RunPrefix, ctx.ThreadIndex)
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(ctx.Username, time.Minute*60)
 	}
 
+	ctx.Framework, ctx.Namespace, err = provisionFramework(
+		*ctx.StageUsers,
+		ctx.ThreadIndex,
+		ctx.Username,
+		ctx.Opts.Stage,
+	)
 	if err != nil {
 		return logging.Logger.Fail(10, "Unable to provision user %s: %v", ctx.Username, err)
-	}
-
-	ctx.Namespace = ctx.Framework.UserNamespace
-
-	return nil
-}
-
-func HandleNewFrameworkForComp(ctx *PerComponentContext) error {
-	var err error
-
-	// TODO This framework generation code is duplicate to above
-	if ctx.ParentContext.ParentContext.Opts.Stage {
-		user := (*ctx.ParentContext.ParentContext.StageUsers)[ctx.ParentContext.ParentContext.ThreadIndex]
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(
-			ctx.ParentContext.ParentContext.Username,
-			time.Minute*60,
-			utils.Options{
-				ApiUrl: user.APIURL,
-				Token:  user.Token,
-			})
-	} else {
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(ctx.ParentContext.ParentContext.Username, time.Minute*60)
-	}
-
-	if err != nil {
-		return logging.Logger.Fail(11, "Unable to provision framework for user %s: %v", ctx.ParentContext.ParentContext.Username, err)
 	}
 
 	return nil
@@ -64,22 +60,30 @@ func HandleNewFrameworkForComp(ctx *PerComponentContext) error {
 func HandleNewFrameworkForApp(ctx *PerApplicationContext) error {
 	var err error
 
-	// TODO This framework generation code is duplicate to above
-	if ctx.ParentContext.Opts.Stage {
-		user := (*ctx.ParentContext.StageUsers)[ctx.ParentContext.ThreadIndex]
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(
-			ctx.ParentContext.Username,
-			time.Minute*60,
-			utils.Options{
-				ApiUrl: user.APIURL,
-				Token:  user.Token,
-			})
-	} else {
-		ctx.Framework, err = framework.NewFrameworkWithTimeout(ctx.ParentContext.Username, time.Minute*60)
+	ctx.Framework, _, err = provisionFramework(
+		*ctx.ParentContext.StageUsers,
+		ctx.ParentContext.ThreadIndex,
+		ctx.ParentContext.Username,
+		ctx.ParentContext.Opts.Stage,
+	)
+	if err != nil {
+		return logging.Logger.Fail(11, "Unable to provision framework for user %s: %v", ctx.ParentContext.Username, err)
 	}
 
+	return nil
+}
+
+func HandleNewFrameworkForComp(ctx *PerComponentContext) error {
+	var err error
+
+	ctx.Framework, _, err = provisionFramework(
+		*ctx.ParentContext.ParentContext.StageUsers,
+		ctx.ParentContext.ParentContext.ThreadIndex,
+		ctx.ParentContext.ParentContext.Username,
+		ctx.ParentContext.ParentContext.Opts.Stage,
+	)
 	if err != nil {
-		return logging.Logger.Fail(12, "Unable to provision framework for user %s: %v", ctx.ParentContext.Username, err)
+		return logging.Logger.Fail(12, "Unable to provision framework for user %s: %v", ctx.ParentContext.ParentContext.Username, err)
 	}
 
 	return nil
