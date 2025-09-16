bc8ceeeb0fd1f1f5d4b8c7707e1e0c0ecd6c9bda
Jan Hutar <jhutar@redhat.com>
2025-09-16 12:14:19 +0200

    refactor(KONFLUX-10143): Compute KPI stats without guessing with the new data we have in timings csv

--- tests/load-tests/evaluate.py
+++ tests/load-tests/evaluate.py
@@ -11,10 +11,14 @@ import sys
 
 # Column indexes in input data
 COLUMN_WHEN = 0
-COLUMN_METRIC = 1
-COLUMN_DURATION = 2
-COLUMN_PARAMS = 3
-COLUMN_ERROR = 4
+COLUMN_PER_USER_T = 1
+COLUMN_PER_APP_T = 2
+COLUMN_PER_COMP_T = 3
+COLUMN_REPEATS_COUNTER = 4
+COLUMN_METRIC = 5
+COLUMN_DURATION = 6
+COLUMN_PARAMS = 7
+COLUMN_ERROR = 8
 
 # Metrics we care about that together form KPI metric duration
 METRICS = [
@@ -64,6 +68,42 @@ METRICS_RELEASE = [
     "validateReleasePipelineRunCondition",
     "validateReleaseCondition",
 ]
+
+
+class SinglePass:
+    """Structure to record data about one specific pass through loadtest workload, identified by an identier (touple with loadtest's per user, per application and per component thread index and repeats counter."""
+
+    def __init__(self):
+        self._metrics = {}
+
+    def add(self, metric, duration):
+        """Adds given metric to data about this pass."""
+        assert metric not in self._metrics
+        self._metrics[metric] = duration
+
+    def complete(self, expected_metrics):
+        """Checks if we have all expected metrics."""
+        current = set(self._metrics.keys())
+        return current == expected_metrics
+
+    def total(self):
+        """Return total duration."""
+        return sum(self._metrics.values())
+
+    @staticmethod
+    def i_matches(identifier1, identifier2):
+        """Check if first provided identifier matches second one. When we have -1 instead of some value(s) in the first identifier, it acts as a wildcard."""
+        if identifier1[3] == -1 or identifier1[3] == identifier2[3]:
+            if identifier1[2] == -1 or identifier1[2] == identifier2[2]:
+                if identifier1[1] == -1 or identifier1[1] == identifier2[1]:
+                    if identifier1[0] == -1 or identifier1[0] == identifier2[0]:
+                        return True
+        return False
+
+    @staticmethod
+    def i_complete(identifier):
+        """Check this is complete identifier (does not contain wildcards)."""
+        return -1 not in identifier
 
 
 def str2date(date_str):
@@ -122,18 +162,31 @@ def main():
         options = json.load(fp)
 
     # Determine what metrics we need to skip based on options
-    METRICS_to_skip = []
+    to_skip = []
     if options["Stage"]:
         print("NOTE: Ignoring CI cluster related metrics because running against non-CI cluster")
-        METRICS_to_skip += METRICS_CI
+        to_skip += METRICS_CI
     if options["TestScenarioGitURL"] == "":
         print("NOTE: Ignoring ITS related metrics because they were disabled at test run")
-        METRICS_to_skip += METRICS_ITS
+        to_skip += METRICS_ITS
     if options["ReleasePolicy"] == "":
         print("NOTE: Ignoring Release related metrics because they were disabled at test run")
-        METRICS_to_skip += METRICS_RELEASE
+        to_skip += METRICS_RELEASE
+
+    # When processing, only consider these metrics
+    expected_metrics = set(METRICS) - set(to_skip)
 
     stats_raw = {}
+    stats_passes = {}
+
+    rows_incomplete = []
+
+    # Prepopulate stats_raw data structure
+    for m in expected_metrics:
+        stats_raw[m] = {
+            "pass": {"duration": [], "when": []},
+            "fail": {"duration": [], "when": []},
+        }
 
     with open(input_file, "r") as fp:
         csvreader = csv.reader(fp)
@@ -142,30 +195,50 @@ def main():
                 continue
 
             when = str2date(row[COLUMN_WHEN])
-            metric = row[COLUMN_METRIC]
+            per_user_t = int(row[COLUMN_PER_USER_T])
+            per_app_t = int(row[COLUMN_PER_APP_T])
+            per_comp_t = int(row[COLUMN_PER_COMP_T])
+            repeats_counter = int(row[COLUMN_REPEATS_COUNTER])
+            metric = row[COLUMN_METRIC].split(".")[-1]
             duration = float(row[COLUMN_DURATION])
             error = row[COLUMN_ERROR] != "<nil>"
 
-            for m in METRICS:
-                if m not in stats_raw:
-                    stats_raw[m] = {
-                        "pass": {"duration": [], "when": []},
-                        "fail": {"duration": [], "when": []},
-                    }
+            if metric not in expected_metrics:
+                continue
 
-                if metric.endswith("." + m):
-                    stats_raw[m]["fail" if error else "pass"]["duration"].append(duration)
-                    stats_raw[m]["fail" if error else "pass"]["when"].append(when)
+            # First add this record to stats_raw that allows us to track stats per metric
+            stats_raw[metric]["fail" if error else "pass"]["duration"].append(duration)
+            stats_raw[metric]["fail" if error else "pass"]["when"].append(when)
+
+            # Second add this record to stats_passes that allows us to track full completed passes
+            if not error:
+                identifier = (per_user_t, per_app_t, per_comp_t, repeats_counter)
+
+                if SinglePass.i_complete(identifier):
+                    if identifier not in stats_passes:
+                        stats_passes[identifier] = SinglePass()
+                    stats_passes[identifier].add(metric, duration)
+                else:
+                    # Safe this metric for later once we have all passes
+                    rows_incomplete.append((identifier, metric, duration))
+
+    # Now when we have data about all passes, add metrics that had incomplete identifiers (with wildcards)
+    for incomplete in rows_incomplete:
+        identifier, metric, duration = incomplete
+        found = [v for k, v in stats_passes.items() if SinglePass.i_matches(identifier, k)]
+        for i in found:
+            i.add(metric, duration)
 
     #print("Raw stats:")
     #print(json.dumps(stats_raw, indent=4, default=lambda o: '<' + str(o) + '>'))
+    #print(json.dumps({str(k): v for k, v in stats_passes.items()}, indent=4, default=lambda o: '<' + str(o._metrics) + '>'))
 
     stats = {}
-    kpi_mean = 0.0
-    kpi_successes = sys.maxsize
+    kpi_mean_data = []
+    kpi_successes = 0
     kpi_errors = 0
 
-    for m in [m for m in METRICS if m not in METRICS_to_skip]:
+    for m in expected_metrics:
         stats[m] = {"pass": {"duration": {"samples": 0}, "when": {}}, "fail": {"duration": {"samples": 0}, "when": {}}}
         if m in stats_raw:
             stats[m]["pass"]["duration"] = count_stats(stats_raw[m]["pass"]["duration"])
@@ -173,32 +246,15 @@ def main():
             stats[m]["pass"]["when"] = count_stats_when(stats_raw[m]["pass"]["when"])
             stats[m]["fail"]["when"] = count_stats_when(stats_raw[m]["fail"]["when"])
 
-        if kpi_mean != -1:
-            # If we had 0 measurements in some metric, that means not a single
-            # build made it through this step, so kpi_mean metric does not make
-            # sense as it would not cover this part of the journey
-            if stats[m]["pass"]["duration"]["samples"] == 0:
-                kpi_mean = -1
-            else:
-                kpi_mean += stats[m]["pass"]["duration"]["mean"]
-
-        if stats[m]["pass"]["duration"]["samples"] < kpi_successes:
-            kpi_successes = stats[m]["pass"]["duration"]["samples"]
-
-        if stats[m]["pass"]["duration"]["samples"] == 0:
-            if kpi_errors == 0:
-                kpi_errors += 1
+    for k, v in stats_passes.items():
+        if v.complete(expected_metrics):
+            kpi_successes += 1
+            kpi_mean_data.append(v.total())
         else:
-            kpi_errors += stats[m]["fail"]["duration"]["samples"]
-
-        runs = stats[m]["pass"]["duration"]["samples"] + stats[m]["fail"]["duration"]["samples"]
-        if runs == 0:
-            stats[m]["error_rate"] = None
-        else:
-            stats[m]["error_rate"] = stats[m]["fail"]["duration"]["samples"] / runs
+            kpi_errors += 1
 
     stats["KPI"] = {}
-    stats["KPI"]["mean"] = kpi_mean
+    stats["KPI"]["mean"] = sum(kpi_mean_data) / kpi_successes if kpi_successes > 0 else -1
     stats["KPI"]["successes"] = kpi_successes
     stats["KPI"]["errors"] = kpi_errors
 
