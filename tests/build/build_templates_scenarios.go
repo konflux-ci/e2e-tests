ed49afb041c51834fce2aedbd9d8ef44490f0b59
Sushanta Das <sushanta.das.ju@gmail.com>
2025-12-22 15:15:22 +0530

    feat: selective execution of tests

--- tests/build/build_templates_scenarios.go
+++ tests/build/build_templates_scenarios.go
@@ -1,6 +1,7 @@
 package build
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/konflux-ci/e2e-tests/pkg/constants"
@@ -302,4 +303,32 @@ func GetComponentScenarioDetailsFromGitUrl(gitUrl string) ComponentScenarioSpec 
 		}
 	}
 	return ComponentScenarioSpec{}
+}
+
+// this function returns true if hermeto related files changed, otherwise false
+func DoesHermetoChanged(changedFilesStr string) bool {
+	isHermetoChanged := false
+	changedFiles := strings.Split(changedFilesStr, " ")
+	for _, filePath := range changedFiles {
+		if strings.HasPrefix(filePath, "task/buildah") || strings.HasPrefix(filePath, "task/prefetch-dependencies") {
+			isHermetoChanged = true
+			break
+		}
+	}
+	return isHermetoChanged
+}
+
+// this function returns which scenarios to execute based on changed_files in PR
+func GetScenarios() []string {
+	changedFiles := utils.GetEnv(PR_CHANGED_FILES_ENV, "")
+	if changedFiles == "" {
+		fmt.Println("ChangedFiles is empty")
+		return componentUrls
+	} else if DoesHermetoChanged(changedFiles) {
+		fmt.Println("Hermeto related files changed, running hermetic scenarios as well")
+		return append(basicScenarioUrls, hermeticScenarioUrls...)
+	} else {
+		fmt.Println("Files changed are not hermeto related, running basic scenarios")
+		return basicScenarioUrls
+	}
 }
