f196c79753f79ccd28fe88247222f73dbcc37c5a
Troy Taylor <tbarrett@redhat.com>
2026-04-30 14:35:24 +0100

    feat: remove dead code

--- pkg/framework/describe.go
+++ pkg/framework/describe.go
@@ -21,28 +21,12 @@ func MultiPlatformBuildSuiteDescribe(text string, args ...interface{}) bool {
 	return ginkgo.Describe("[multi-platform-build-service-suite "+text+"]", args, ginkgo.Ordered)
 }
 
-func IntegrationServiceSuiteDescribe(text string, args ...interface{}) bool {
-	return ginkgo.Describe("[integration-service-suite "+text+"]", args, ginkgo.Ordered)
-}
-
-func KonfluxDemoSuiteDescribe(args ...interface{}) bool {
-	return ginkgo.Describe("[konflux-demo-suite]", args)
-}
-
 func EnterpriseContractSuiteDescribe(text string, args ...interface{}) bool {
 	return ginkgo.Describe("[enterprise-contract-suite "+text+"]", args, ginkgo.Ordered)
 }
 
 func UpgradeSuiteDescribe(text string, args ...interface{}) bool {
 	return ginkgo.Describe("[upgrade-suite "+text+"]", args, ginkgo.Ordered)
-}
-
-func ReleasePipelinesSuiteDescribe(text string, args ...interface{}) bool {
-	return ginkgo.Describe("[release-pipelines-suite "+text+"]", args, ginkgo.Ordered)
-}
-
-func ReleaseServiceSuiteDescribe(text string, args ...interface{}) bool {
-	return ginkgo.Describe("[release-service-suite "+text+"]", args, ginkgo.Ordered)
 }
 
 func TknBundleSuiteDescribe(text string, args ...interface{}) bool {
--- tests/README.md
+++ tests/README.md
@@ -5,8 +5,6 @@ Tests folder contains E2E related to several components divided in folders:
 | Folder | Component repository | README | Note |
 |---|---|---|---|
 | `build` | [build-service](https://github.com/konflux-ci/build-service), [jvm-build-service](https://github.com/redhat-appstudio/jvm-build-service) | [README.md](/tests/build/README.md) | |
-| `conforma` | N/A | [README.md](/tests/enterprise-contract/README.md) | |
-| `integration-service` | [integration-service](https://github.com/konflux-ci/integration-service) | [README.md](/tests/integration-service/README.md) |  |
-| `release` | [release-service](https://github.com/konflux-ci/release-service) | [README.md](/tests/release/README.md) |  |
-| `konflux-demo` | N/A | [README.md](/tests/konflux-demo/README.md) | |
+| `disaster-recovery` | N/A | | |
+| `enterprise-contract` | N/A | [README.md](/tests/enterprise-contract/README.md) | |
 | `upgrade` | N/A | [README.md](/tests/upgrade/README.md) | |
