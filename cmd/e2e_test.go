a4636c46b1f29c72e28c238ef3e756fa2f07f317
Troy Taylor <tbarrett@redhat.com>
2026-04-30 14:10:43 +0100

    feat: remove migrated tests

--- cmd/e2e_test.go
+++ cmd/e2e_test.go
@@ -14,10 +14,6 @@ import (
 	_ "github.com/konflux-ci/e2e-tests/tests/build"
 	_ "github.com/konflux-ci/e2e-tests/tests/disaster-recovery"
 	_ "github.com/konflux-ci/e2e-tests/tests/enterprise-contract"
-	_ "github.com/konflux-ci/e2e-tests/tests/integration-service"
-	_ "github.com/konflux-ci/e2e-tests/tests/konflux-demo"
-	_ "github.com/konflux-ci/e2e-tests/tests/release/pipelines"
-	_ "github.com/konflux-ci/e2e-tests/tests/release/service"
 	_ "github.com/konflux-ci/e2e-tests/tests/upgrade"
 
 	"flag"
