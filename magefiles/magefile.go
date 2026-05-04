78fcc6e4001cf9e354fbe93d6cac05deabc64f4c
Meirav Rath <84704676+meyrevived@users.noreply.github.com>
2026-05-04 12:22:44 +0300

    fix: KFLUXINFRA-3633 increase DR workflow timeout to 1080m (#1859)

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1342,7 +1342,7 @@ func DisasterRecoveryWorkflow() error {
 		return err
 	}
 
-	return runTestsWithTimeout("disaster-recovery", "disaster-recovery-report.xml", "360m")
+	return runTestsWithTimeout("disaster-recovery", "disaster-recovery-report.xml", "1080m")
 }
 
 func runTests(labelsToRun string, junitReportFile string) error {
